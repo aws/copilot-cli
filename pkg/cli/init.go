2884abce028b13b29dd44ecbf0606bbbd1a558c7
David Killmon <killmond+gh@amazon.com>
2019-09-12 15:52:07 -0700

    Add project creation / manifest writing back into init
    This change adds:
    1. Project listing, if there are existing projects
    2. Project creating, if the project is new
    3. Adding the manfiest back
    4. Stub for deploying an env after everything is complete
    
    Caveat with this change, since we'll be re-working the manifest
    reader/writer - I haven't covered that under our unit tests.

--- pkg/cli/init.go
+++ pkg/cli/init.go
@@ -7,6 +7,9 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/aws/PRIVATE-amazon-ecs-archer/cmd/archer/template"
+	"github.com/aws/PRIVATE-amazon-ecs-archer/pkg/archer"
+	"github.com/aws/PRIVATE-amazon-ecs-archer/pkg/manifest"
+	"github.com/aws/PRIVATE-amazon-ecs-archer/pkg/store/ssm"
 	"github.com/spf13/cobra"
 )
 
@@ -15,6 +18,12 @@ type InitAppOpts struct {
 	Project string `survey:"project"` // namespace that this application belongs to.
 	Name    string `survey:"name"`    // unique identifier to logically group AWS resources together.
 	Type    string `survey:"Type"`    // The type of application you're trying to build (LoadBalanced, Backend, etc.)
+
+	projStore archer.ProjectStore
+	envStore  archer.EnvironmentStore
+
+	existingProjects []string // existing project names
+
 	// Prompt holds the interfaces to receive and output app configuration data to the terminal.
 	Prompt terminal.Stdio
 }
@@ -23,14 +32,7 @@ type InitAppOpts struct {
 func (opts *InitAppOpts) Ask() error {
 	var qs []*survey.Question
 	if opts.Project == "" {
-		qs = append(qs, &survey.Question{
-			Name: "project",
-			Prompt: &survey.Input{
-				Message: "What is your project's name?",
-				Help:    "Applications under the same project share the same VPC and ECS Cluster and are discoverable via service discovery.",
-			},
-			Validate: validateProjectName,
-		})
+		qs = append(qs, projectQuestion(opts))
 	}
 	if opts.Name == "" {
 		qs = append(qs, &survey.Question{
@@ -42,7 +44,45 @@ func (opts *InitAppOpts) Ask() error {
 			Validate: validateApplicationName,
 		})
 	}
+	if opts.Type == "" {
+		qs = append(qs, manifestQuestion(opts))
+	}
 	return survey.Ask(qs, opts, survey.WithStdio(opts.Prompt.In, opts.Prompt.Out, opts.Prompt.Err))
+}
+
+func manifestQuestion(opts *InitAppOpts) *survey.Question {
+	return &survey.Question{
+		Prompt: &survey.Select{
+			Message: "Which template would you like to use?",
+			Help:    "Pre-defined infrastructure templates.",
+			Options: manifest.TemplateNames,
+			Default: manifest.TemplateNames[0],
+		},
+		Name: "Type",
+	}
+}
+
+func projectQuestion(opts *InitAppOpts) *survey.Question {
+	if len(opts.existingProjects) > 0 {
+		return &survey.Question{
+			Name: "project",
+			Prompt: &survey.Select{
+				Message: "Which project should we use?",
+				Help:    "Choose a project to create a new application in. Applications in the same project share the same VPC, ECS Cluster and are discoverable via service discovery",
+				Options: opts.existingProjects,
+			},
+		}
+	}
+
+	return &survey.Question{
+		Name: "project",
+		Prompt: &survey.Input{
+			Message: "What is your project's name?",
+			Help:    "Applications under the same project share the same VPC and ECS Cluster and are discoverable via service discovery.",
+		},
+		Validate: validateProjectName,
+	}
+
 }
 
 // Validate returns an error if a command line flag provided value is invalid
@@ -58,27 +98,95 @@ func (opts *InitAppOpts) Validate() error {
 	return nil
 }
 
-// InitApp creates the project and application structure
-func (a *InitAppOpts) InitApp() error {
-	//TODO fill in
+// Prepare loads contextual data such as any existing projects, the current workspace, etc
+func (opts *InitAppOpts) Prepare() {
+	// Load existing projects (this is a UI convenience, so we'll ignore errors)
+	existingProjects, _ := opts.projStore.ListProjects()
+	var projectNames []string
+	for _, p := range existingProjects {
+		projectNames = append(projectNames, p.Name)
+	}
+	opts.existingProjects = projectNames
+}
+
+// InitApp creates a project and initializes the workspace
+func (opts *InitAppOpts) InitApp() error {
+	shouldCreateNewProject := true
+	// If the project already exists, skip creating it
+	for _, project := range opts.existingProjects {
+		if opts.Project == project {
+			shouldCreateNewProject = false
+		}
+	}
+	if shouldCreateNewProject {
+		err := opts.projStore.CreateProject(&archer.Project{
+			Name: opts.Project,
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	m, err := manifest.New(opts.Type)
+	if err != nil {
+		return err
+	}
+	m.Render(opts.Name, opts)
+	return nil
+}
+
+// DeployEnv optionally deploys an environment
+func (opts *InitAppOpts) DeployEnv() error {
+	existingEnvs, _ := opts.envStore.ListEnvironments(opts.Project)
+	if len(existingEnvs) > 0 {
+		return nil
+	}
+	deployEnv := false
+	prompt := &survey.Confirm{
+		Message: "Would you like to set up a test environment?",
+		Help:    "You can deploy your app into your test environment.",
+	}
+
+	survey.AskOne(prompt, &deployEnv, survey.WithStdio(opts.Prompt.In, opts.Prompt.Out, opts.Prompt.Err))
+
+	if deployEnv {
+		//TODO deploy env
+		fmt.Println("Deploying env...")
+	}
 	return nil
 }
 
 // BuildInitCmd builds the command to build an application
 func BuildInitCmd() *cobra.Command {
-	opts := InitAppOpts{Prompt: terminal.Stdio{
-		In:  os.Stdin,
-		Out: os.Stderr,
-		Err: os.Stderr,
-	}}
+	opts := InitAppOpts{
+		Prompt: terminal.Stdio{
+			In:  os.Stdin,
+			Out: os.Stderr,
+			Err: os.Stderr,
+		},
+	}
+
 	cmd := &cobra.Command{
 		Use:   "init",
 		Short: "Create a new ECS application",
 		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			ssm, err := ssm.NewStore()
+			if err != nil {
+				return err
+			}
+
+			opts.projStore = ssm
+			opts.envStore = ssm
+
+			opts.Prepare()
 			return opts.Ask()
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return opts.InitApp()
+			err := opts.InitApp()
+			if err != nil {
+				return err
+			}
+			return opts.DeployEnv()
 		},
 	}
 	cmd.Flags().StringVarP(&opts.Project, "project", "p", "", "Name of the project (required).")
