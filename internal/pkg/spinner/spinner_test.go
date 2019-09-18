ac1d91aeac02966d3a40ad793150e16e079a284e
Nick Fischer <nicfisch@amazon.com>
2019-09-18 10:53:02 -0700

    Use testify/mock in spinner package tests

--- internal/pkg/spinner/spinner_test.go
+++ internal/pkg/spinner/spinner_test.go
@@ -6,32 +6,41 @@ package spinner
 import (
 	"testing"
 
-	"github.com/aws/PRIVATE-amazon-ecs-archer/internal/pkg/spinner/mocks"
-	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestStart(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockSpinner := mocks.NewMockspinner(ctrl)
+type mockSpinner struct {
+	mock.Mock
+}
 
+func (m mockSpinner) Start() {
+	m.Called()
+}
+
+func (m mockSpinner) Stop() {
+	m.Called()
+}
+
+func TestStart(t *testing.T) {
+	mockSpinner := new(mockSpinner)
+	mockSpinner.On("Start")
 	s := &Spinner{
 		internal: mockSpinner,
 	}
 
-	mockSpinner.EXPECT().Start()
+	s.Start("doing stuff")
 
-	s.Start("start")
+	mockSpinner.AssertExpectations(t)
 }
 
 func TestStop(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockSpinner := mocks.NewMockspinner(ctrl)
-
+	mockSpinner := new(mockSpinner)
+	mockSpinner.On("Stop")
 	s := &Spinner{
 		internal: mockSpinner,
 	}
 
-	mockSpinner.EXPECT().Stop()
+	s.Stop("done")
 
-	s.Stop("stop")
+	mockSpinner.AssertExpectations(t)
 }
