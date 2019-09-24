c663318cebca0e599181f0405082c473e909699a
Nick Fischer <nicfisch@amazon.com>
2019-09-24 16:05:10 -0700

    Change spinner charset

---
+++ internal/pkg/spinner/charset.go
@@ -0,0 +1,9 @@
+// +build !windows
+
+package spinner
+
+import (
+	spin "github.com/briandowns/spinner"
+)
+
+var charset = spin.CharSets[14]
---
+++ internal/pkg/spinner/charset_windows.go
@@ -0,0 +1,3 @@
+package spinner
+
+var charset = []string{"/", "-", "\\"}
