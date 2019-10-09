fc9e70cab79c5edd64853a5bc3b2893c9be4fec2
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2019-10-09 12:04:44 -0700

    Removed redundant package doc

--- cmd/archer/template/template.go
+++ cmd/archer/template/template.go
@@ -3,7 +3,6 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package template provides usage templates to render help menus.
 package template
 
 import (
--- cmd/archer/template/template_windows.go
+++ cmd/archer/template/template_windows.go
@@ -1,7 +1,6 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package template provides usage templates to render help menus.
 package template
 
 import (
