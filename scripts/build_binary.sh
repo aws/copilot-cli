7530a6fc9f3613db5d3b0af90fd8135ed963b060
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-10-17 16:47:30 -0700

    Refactor Makefile

---
+++ scripts/build_binary.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Normalize to working directory being build root (up one level from ./scripts)
+ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )
+cd "${ROOT}"
+
+go run ./version/gen/generate-version.go
+
+# GIT_DIRTY=`git diff --quiet || echo '*'`
+# GIT_SHORT_HASH="$GIT_DIRTY"`git rev-parse --short=7 HEAD`
+
+echo "Building archer to ${DESTINATION}"
+
+# TODO: Inject version and git short hash into build
+GOOS=$TARGET_GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o ${DESTINATION} ./cmd/archer
+	# -ldflags "-X github.com/aws/amazon-ecs-cli-v2/version.GitHash=$GIT_SHORT_HASH" \
