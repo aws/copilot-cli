bd150a90de362c7fce9f9d3dbd4bda84d0a0c987
Nick Fischer <nicfisch@amazon.com>
2019-10-28 14:03:03 -0700

    Add newline to version outputs

--- cmd/archer/template/version.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// Package template provides usage templates to render help menus.
-package template
-
-const VersionFlag = `Archer version: {{.Version}}`
