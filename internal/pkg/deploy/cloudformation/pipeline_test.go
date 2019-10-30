3272739414d05574cdf5519b4c42455a884505aa
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2019-10-30 13:02:22 -0700

    Updated pipeline.go to take all information in as `CreatePipelineInput`

--- internal/pkg/deploy/cloudformation/pipeline_test.go
+++ internal/pkg/deploy/cloudformation/pipeline_test.go
@@ -8,42 +8,29 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
-	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 
-	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
-	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/manifest"
-	"github.com/aws/amazon-ecs-cli-v2/mocks"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy"
 )
 
-const projectName = "chickenProject"
+const (
+	projectName = "chickenProject"
+	pipelineName = "wingspipeline"
+)
 
 func TestPipelineParameters(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	pipelineManifest := mockPipelineManifest()
-	mockEnvG := validMockEnvironment(ctrl)
-	mockWs := validMockWorkspace(ctrl)
-	pipeline, err := newPipelineStackConfig(
-		mockEnvG, mockWs, pipelineManifest,
+	pipeline := newPipelineStackConfig(
+		mockDeployPipelineInput(),
 	)
-	require.NoError(t, err, "newPipelineStackConfig fails")
 
 	require.Nil(t, pipeline.Parameters(), "pipeline cloudformation template should not expose any parameters")
 }
 
 func TestPipelineTags(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	pipelineManifest := mockPipelineManifest()
-	mockEnvG := validMockEnvironment(ctrl)
-	mockWs := validMockWorkspace(ctrl)
-	pipeline, err := newPipelineStackConfig(
-		mockEnvG, mockWs, pipelineManifest,
+	pipeline := newPipelineStackConfig(
+		mockDeployPipelineInput(),
 	)
-	require.NoError(t, err, "newPipelineStackConfig fails")
+
 	expectedTags := []*cloudformation.Tag{
 		{
 			Key:   aws.String(projectTagKey),
@@ -54,65 +41,43 @@ func TestPipelineTags(t *testing.T) {
 }
 
 func TestPipelineStackName(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	pipelineManifest := mockPipelineManifest()
-	mockEnvG := validMockEnvironment(ctrl)
-	mockWs := validMockWorkspace(ctrl)
-	pipeline, err := newPipelineStackConfig(
-		mockEnvG, mockWs, pipelineManifest,
+	pipeline := newPipelineStackConfig(
+		mockDeployPipelineInput(),
 	)
-	require.NoError(t, err, "newPipelineStackConfig fails")
 
-	require.Equal(t, projectName+"-"+pipelineManifest.Name,
+	require.Equal(t, projectName+"-"+pipelineName,
 		pipeline.StackName(), "unexpected StackName")
 }
 
-func validMockEnvironment(ctrl *gomock.Controller) archer.EnvironmentGetter {
-	mockEnvG := mocks.NewMockEnvironmentGetter(ctrl)
-	mockEnvG.EXPECT().
-		GetEnvironment(gomock.Eq(projectName), gomock.Any()).
-		Return(mockEnvironment("wingsEnv", "us-west-2", false), nil)
-
-	return mockEnvG
-}
-
-func validMockWorkspace(ctrl *gomock.Controller) archer.Workspace {
-	mockWs := mocks.NewMockWorkspace(ctrl)
-	mockWs.EXPECT().Summary().Return(&archer.WorkspaceSummary{
-		ProjectName: projectName,
-	}, nil)
-
-	mockWs.EXPECT().AppNames().Return([]string{"frontend", "backend"}, nil)
-	return mockWs
-}
-
-func mockEnvironment(envName string, region string, isProd bool) *archer.Environment {
-	return &archer.Environment{
-		Project:     projectName,
-		Name:        envName,
-		Region:      region,
-		AccountID:   "012345678910",
-		Prod:        isProd,
-		RegistryURL: "dontCare",
+func mockAssociatedEnv(envName, region string, isProd bool) *deploy.AssociatedEnvironment {
+	return &deploy.AssociatedEnvironment{
+		Name: envName,
+		Region: region,
+		AccountId: "012345678910",
+		Prod: isProd,
 	}
 }
 
-func mockPipelineManifest() *manifest.PipelineManifest {
-	return &manifest.PipelineManifest{
-		Name:    "pipeline",
-		Version: manifest.Ver1,
-		Source: &manifest.Source{
+func mockDeployPipelineInput() *deploy.CreatePipelineInput {	
+	return &deploy.CreatePipelineInput{
+		ProjectName: projectName,
+		Name:            pipelineName         ,
+		Source: &deploy.Source{
 			ProviderName: "GitHub",
 			Properties: map[string]interface{}{
 				"repository": "aws/somethingCool",
 				"branch":     "master",
 			},
+
 		},
-		Stages: []manifest.PipelineStage{
+		Stages: []deploy.PipelineStage{
 			{
-				Name: "chicken",
+				AssociatedEnvironment: mockAssociatedEnv("test", "us-west-2", false),
+				LocalApplications: []string{"frontend", "backend"},
+			},
+			{
+				AssociatedEnvironment: mockAssociatedEnv("prod", "us-east-1", true),
+				LocalApplications: []string{"frontend", "backend"},
 			},
 		},
 	}
