7f0f5dcfc2672a48e0c827e3cf5943d27c3799dc
David Killmon <killmond+gh@amazon.com>
2019-11-06 16:33:12 -0800

    Add ProjectResourceStore interface
    This interface describes the methods to fetch resources for a particular
    project.

--- internal/pkg/archer/project.go
+++ internal/pkg/archer/project.go
@@ -3,31 +3,47 @@
 
 package archer
 
-// Project is a named collection of environments.
+// Project is a named collection of environments and apps.
 type Project struct {
-	Name      string `json:"name"`    // Name of a project. Must be unique amongst other projects in the same account
-	AccountID string `json:"account"` // AccountID this project is mastered in
-	Version   string `json:"version"` // The version of the project layout in the underyling datastore (e.g. SSM)
+	Name      string `json:"name"`    // Name of a project. Must be unique amongst other projects in the same account.
+	AccountID string `json:"account"` // AccountID this project is mastered in.
+	Version   string `json:"version"` // The version of the project layout in the underlying datastore (e.g. SSM).
 }
 
-// ProjectStore is an interface for creating and listing projects
+// ProjectRegionalResources represent project resources that are regional.
+type ProjectRegionalResources struct {
+	Region         string            // The region these resources are in.
+	KMSKeyARN      string            // A KMS Key ARN for encrypting Pipeline artifacts.
+	S3Bucket       string            // S3 bucket for Pipeline artifacts.
+	RepositoryURLs map[string]string // The image repository URLs by app name.
+}
+
+// ProjectStore is an interface for creating and listing projects.
 type ProjectStore interface {
 	ProjectLister
 	ProjectGetter
 	ProjectCreator
 }
 
-// ProjectLister lists all the projects in the underlying project manager
+// ProjectLister lists all the projects in the underlying project manager.
 type ProjectLister interface {
 	ListProjects() ([]*Project, error)
 }
 
-// ProjectCreator creates a project in the underlying project manager
+// ProjectCreator creates a project in the underlying project manager.
 type ProjectCreator interface {
 	CreateProject(project *Project) error
 }
 
-// ProjectGetter fetches an individual project from the underlying project manager
+// ProjectGetter fetches an individual project from the underlying project manager.
 type ProjectGetter interface {
 	GetProject(projectName string) (*Project, error)
+}
+
+// ProjectResourceStore fetches resources related to the project.
+type ProjectResourceStore interface {
+	// Fetch all regional resources in a project.
+	GetRegionalProjectResources(project *Project) ([]*ProjectRegionalResources, error)
+	// Fetch the project regional resources for a particular region.
+	GetProjectResourcesByRegion(project *Project, region string) (*ProjectRegionalResources, error)
 }
