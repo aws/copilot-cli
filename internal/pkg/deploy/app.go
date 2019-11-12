6995419c4ba066c98527ac5e7ab1e4ae55a67330
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-11-11 17:08:36 -0800

    Extract deploy/app.go

---
+++ internal/pkg/deploy/app.go
@@ -0,0 +1,19 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// Package deploy holds the structures to deploy infrastructure resources.
+// This file defines app deployment resources.
+package deploy
+
+import (
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/manifest"
+)
+
+// CreateLBFargateAppInput holds the fields required to deploy a load-balanced AWS Fargate application.
+type CreateLBFargateAppInput struct {
+	App          *manifest.LBFargateManifest
+	Env          *archer.Environment
+	ImageRepoURL string
+	ImageTag     string
+}
--- internal/pkg/deploy/deploy.go
+++ internal/pkg/deploy/deploy.go
@@ -4,19 +4,6 @@
 // Package deploy holds the structures to deploy infrastructure resources.
 package deploy
 
-import (
-	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
-	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/manifest"
-)
-
-// CreateLBFargateAppInput holds the fields required to deploy a load-balanced AWS Fargate application.
-type CreateLBFargateAppInput struct {
-	App          *manifest.LBFargateManifest
-	Env          *archer.Environment
-	ImageRepoURL string
-	ImageTag     string
-}
-
 // Resource represents an AWS resource.
 type Resource struct {
 	LogicalName string
