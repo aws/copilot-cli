90497c9950d2fc2841d45b1af24b81705973ff6e
Hsing-Hui Hsu <hsinghui@amazon.com>
2019-11-15 11:29:06 -0800

    Move comment
    So efe doesn't get confused

--- internal/pkg/cli/deploy.go
+++ internal/pkg/cli/deploy.go
@@ -1,7 +1,6 @@
 // Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Interfaces for deploying resources through CloudFormation. Facilitates mocking.
 package cli
 
 import (
@@ -9,6 +8,7 @@ import (
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy"
 )
 
+// Interfaces for deploying resources through CloudFormation. Facilitates mocking.
 type environmentDeployer interface {
 	DeployEnvironment(env *deploy.CreateEnvironmentInput) error
 	StreamEnvironmentCreation(env *deploy.CreateEnvironmentInput) (<-chan []deploy.ResourceEvent, <-chan deploy.CreateEnvironmentResponse)
