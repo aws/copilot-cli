35b651fd30cea7509c13edc5cc105e8bdb0dc5ea
Yenlin Chen <3822365+hencrice@users.noreply.github.com>
2019-11-19 17:19:51 -0800

    Conditionally create or update the pipeline and wait til succeess
    The PR fixes 2 issues:
    1. We did not properly wait for pipeline stack creation/update
    2. We could not update the pipeline stack because the stack already exists
    
    Fixes #414. And fixes #413.

--- internal/pkg/deploy/cloudformation/pipeline.go
+++ internal/pkg/deploy/cloudformation/pipeline.go
@@ -6,8 +6,14 @@
 package cloudformation
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy/cloudformation/stack"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 )
 
 // DeployPipeline sets up a CodePipeline for deploying applications.
@@ -17,5 +23,27 @@ import (
 func (cf CloudFormation) DeployPipeline(in *deploy.CreatePipelineInput) error {
 	pipelineConfig := stack.NewPipelineStackConfig(in)
 
-	return cf.create(pipelineConfig)
+	// First attempt to create the pipeline stack
+	err := cf.create(pipelineConfig)
+	if err == nil {
+		_, err := cf.waitForStackCreation(pipelineConfig)
+		if err != nil {
+			return err
+		}
+	}
+
+	// If the stack already exists - we update it
+	var alreadyExists *ErrStackAlreadyExists
+	if !errors.As(err, &alreadyExists) {
+		return err
+	}
+
+	if err := cf.update(pipelineConfig); err != nil {
+		return fmt.Errorf("updating pipeline: %w", err)
+	}
+
+	return cf.client.WaitUntilStackUpdateCompleteWithContext(context.Background(),
+		&cloudformation.DescribeStacksInput{
+			StackName: aws.String(pipelineConfig.StackName()),
+		}, cf.waiters...)
 }
