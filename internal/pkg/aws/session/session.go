b1d648af8fcfd2d175579c5b3508fd789ac841db
Piradeep Kandasamy <piradeep@amazon.com>
2019-11-20 15:39:45 -0800

    Add user agent

--- internal/pkg/aws/session/session.go
+++ internal/pkg/aws/session/session.go
@@ -7,6 +7,7 @@ package session
 import (
 	"fmt"
 
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/aws/handlers"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -14,30 +15,39 @@ import (
 
 // Default returns a session configured against the "default" AWS profile.
 func Default() (*session.Session, error) {
-	return session.NewSessionWithOptions(session.Options{
+	sess, err := session.NewSessionWithOptions(session.Options{
 		Config: aws.Config{
 			CredentialsChainVerboseErrors: aws.Bool(true),
 		},
 		SharedConfigState: session.SharedConfigEnable,
 	})
+	sess.Handlers.Build.PushBackNamed(handlers.UserAgentHandler())
+
+	return sess, err
 }
 
 // DefaultWithRegion returns a session configured against the "default" AWS profile and the input region.
 func DefaultWithRegion(region string) (*session.Session, error) {
-	return session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	})
+	sess.Handlers.Build.PushBackNamed(handlers.UserAgentHandler())
+
+	return sess, err
 }
 
 // FromProfile returns a session configured against the input profile name.
 func FromProfile(name string) (*session.Session, error) {
-	return session.NewSessionWithOptions(session.Options{
+	sess, err := session.NewSessionWithOptions(session.Options{
 		Config: aws.Config{
 			CredentialsChainVerboseErrors: aws.Bool(true),
 		},
 		SharedConfigState: session.SharedConfigEnable,
 		Profile:           name,
 	})
+	sess.Handlers.Build.PushBackNamed(handlers.UserAgentHandler())
+
+	return sess, err
 }
 
 // FromRole returns a session configured against the input role and region.
@@ -49,9 +59,12 @@ func FromRole(roleARN string, region string) (*session.Session, error) {
 	}
 
 	creds := stscreds.NewCredentials(defaultSession, roleARN)
-	return session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 		CredentialsChainVerboseErrors: aws.Bool(true),
 		Credentials:                   creds,
 		Region:                        &region,
 	})
+	sess.Handlers.Build.PushBackNamed(handlers.UserAgentHandler())
+
+	return sess, err
 }
