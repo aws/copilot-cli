3493b36996f965e19c2e4ed441575736ad9d0c98
penghaoh <penghaoh@amazon.com>
2019-12-12 13:28:56 -0800

    Switch sanity check and existence check

--- internal/pkg/cli/app_init.go
+++ internal/pkg/cli/app_init.go
@@ -83,11 +83,11 @@ func (opts *InitAppOpts) Validate() error {
 		}
 	}
 	if opts.DockerfilePath != "" {
-		if _, err := opts.fs.Stat(opts.DockerfilePath); err != nil {
-			return err
-		}
 		if !strings.HasSuffix(opts.DockerfilePath, "/Dockerfile") {
 			return fmt.Errorf("a valid Dockerfile path is required")
+		}
+		if _, err := opts.fs.Stat(opts.DockerfilePath); err != nil {
+			return err
 		}
 	}
 	if opts.ProjectName() == "" {
