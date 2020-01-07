d2f05da2071c364ae83b84137420f6fb7e0d6e03
Efe Karakus <karakuse@amazon.com>
2020-01-07 10:23:41 -0800

    refactor(env init): change method receiver name to "o" from "opts"

--- internal/pkg/cli/env_init.go
+++ internal/pkg/cli/env_init.go
@@ -62,107 +62,107 @@ type InitEnvOpts struct {
 }
 
 // Validate returns an error if the values passed by the user are invalid.
-func (opts *InitEnvOpts) Validate() error {
-	if opts.EnvName != "" {
-		if err := validateEnvironmentName(opts.EnvName); err != nil {
+func (o *InitEnvOpts) Validate() error {
+	if o.EnvName != "" {
+		if err := validateEnvironmentName(o.EnvName); err != nil {
 			return err
 		}
 	}
-	if opts.ProjectName() == "" {
+	if o.ProjectName() == "" {
 		return errors.New("no project found, run `project init` first please")
 	}
 	return nil
 }
 
 // Ask asks for fields that are required but not passed in.
-func (opts *InitEnvOpts) Ask() error {
-	if opts.EnvName == "" {
-		envName, err := opts.prompt.Get(envInitNamePrompt, envInitNameHelpPrompt, validateEnvironmentName)
+func (o *InitEnvOpts) Ask() error {
+	if o.EnvName == "" {
+		envName, err := o.prompt.Get(envInitNamePrompt, envInitNameHelpPrompt, validateEnvironmentName)
 		if err != nil {
 			return fmt.Errorf("prompt to get environment name: %w", err)
 		}
-		opts.EnvName = envName
+		o.EnvName = envName
 	}
-	if opts.EnvProfile == "" {
-		profile, err := opts.prompt.Get(
-			fmt.Sprintf(fmtEnvInitProfilePrompt, color.HighlightUserInput(opts.EnvName)),
+	if o.EnvProfile == "" {
+		profile, err := o.prompt.Get(
+			fmt.Sprintf(fmtEnvInitProfilePrompt, color.HighlightUserInput(o.EnvName)),
 			envInitProfileHelpPrompt,
 			nil, // no validation needed
 			prompt.WithDefaultInput("default"))
 		if err != nil {
 			return fmt.Errorf("prompt to get the profile name: %w", err)
 		}
-		opts.EnvProfile = profile
+		o.EnvProfile = profile
 	}
 	return nil
 }
 
 // Execute deploys a new environment with CloudFormation and adds it to SSM.
-func (opts *InitEnvOpts) Execute() error {
-	project, err := opts.projectGetter.GetProject(opts.ProjectName())
+func (o *InitEnvOpts) Execute() error {
+	project, err := o.projectGetter.GetProject(o.ProjectName())
 	if err != nil {
 		// Ensure the project actually exists before we do a deployment.
 		return err
 	}
-	caller, err := opts.identity.Get()
+	caller, err := o.identity.Get()
 	if err != nil {
 		return fmt.Errorf("get identity: %w", err)
 	}
 
 	// 1. Start creating the CloudFormation stack for the environment.
 	deployEnvInput := &deploy.CreateEnvironmentInput{
-		Name:                     opts.EnvName,
-		Project:                  opts.ProjectName(),
-		Prod:                     opts.IsProduction,
+		Name:                     o.EnvName,
+		Project:                  o.ProjectName(),
+		Prod:                     o.IsProduction,
 		PublicLoadBalancer:       true, // TODO: configure this based on user input or application Type needs?
 		ToolsAccountPrincipalARN: caller.RootUserARN,
 		ProjectDNSName:           project.Domain,
 	}
 
 	if project.RequiresDNSDelegation() {
-		if err := opts.delegateDNSFromProject(project); err != nil {
+		if err := o.delegateDNSFromProject(project); err != nil {
 			return fmt.Errorf("granting DNS permissions: %w", err)
 		}
 	}
 
-	opts.prog.Start(fmt.Sprintf(fmtDeployEnvStart, color.HighlightUserInput(opts.EnvName)))
+	o.prog.Start(fmt.Sprintf(fmtDeployEnvStart, color.HighlightUserInput(o.EnvName)))
 
-	if err := opts.envDeployer.DeployEnvironment(deployEnvInput); err != nil {
+	if err := o.envDeployer.DeployEnvironment(deployEnvInput); err != nil {
 		var existsErr *cloudformation.ErrStackAlreadyExists
 		if errors.As(err, &existsErr) {
 			// Do nothing if the stack already exists.
-			opts.prog.Stop("")
+			o.prog.Stop("")
 			log.Successf("Environment %s already exists under project %s! Do nothing.\n",
-				color.HighlightUserInput(opts.EnvName), color.HighlightResource(opts.ProjectName()))
+				color.HighlightUserInput(o.EnvName), color.HighlightResource(o.ProjectName()))
 			return nil
 		}
-		opts.prog.Stop(log.Serrorf(fmtDeployEnvFailed, color.HighlightUserInput(opts.EnvName)))
+		o.prog.Stop(log.Serrorf(fmtDeployEnvFailed, color.HighlightUserInput(o.EnvName)))
 		return err
 	}
 
 	// 2. Display updates while the deployment is happening.
-	opts.prog.Start(fmt.Sprintf(fmtStreamEnvStart, color.HighlightUserInput(opts.EnvName)))
-	stackEvents, responses := opts.envDeployer.StreamEnvironmentCreation(deployEnvInput)
+	o.prog.Start(fmt.Sprintf(fmtStreamEnvStart, color.HighlightUserInput(o.EnvName)))
+	stackEvents, responses := o.envDeployer.StreamEnvironmentCreation(deployEnvInput)
 	for stackEvent := range stackEvents {
-		opts.prog.Events(opts.humanizeEnvironmentEvents(stackEvent))
+		o.prog.Events(o.humanizeEnvironmentEvents(stackEvent))
 	}
 	resp := <-responses
 	if resp.Err != nil {
-		opts.prog.Stop(log.Serrorf(fmtStreamEnvFailed, color.HighlightUserInput(opts.EnvName)))
+		o.prog.Stop(log.Serrorf(fmtStreamEnvFailed, color.HighlightUserInput(o.EnvName)))
 		return resp.Err
 	}
-	opts.prog.Stop(log.Ssuccessf(fmtStreamEnvComplete, color.HighlightUserInput(opts.EnvName)))
+	o.prog.Stop(log.Ssuccessf(fmtStreamEnvComplete, color.HighlightUserInput(o.EnvName)))
 
 	// 3. Add the stack set instance to the project stackset.
-	opts.prog.Start(fmt.Sprintf(fmtAddEnvToProjectStart, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(opts.ProjectName())))
-	if err := opts.projDeployer.AddEnvToProject(project, resp.Env); err != nil {
-		opts.prog.Stop(log.Serrorf(fmtAddEnvToProjectFailed, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(opts.ProjectName())))
+	o.prog.Start(fmt.Sprintf(fmtAddEnvToProjectStart, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(o.ProjectName())))
+	if err := o.projDeployer.AddEnvToProject(project, resp.Env); err != nil {
+		o.prog.Stop(log.Serrorf(fmtAddEnvToProjectFailed, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(o.ProjectName())))
 		return fmt.Errorf("deploy env %s to project %s: %w", resp.Env.Name, project.Name, err)
 	}
-	opts.prog.Stop(log.Ssuccessf(fmtAddEnvToProjectComplete, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(opts.ProjectName())))
+	o.prog.Stop(log.Ssuccessf(fmtAddEnvToProjectComplete, color.HighlightResource(resp.Env.AccountID), color.HighlightResource(resp.Env.Region), color.HighlightUserInput(o.ProjectName())))
 
 	// 4. Store the environment in SSM.
-	if err := opts.envCreator.CreateEnvironment(resp.Env); err != nil {
+	if err := o.envCreator.CreateEnvironment(resp.Env); err != nil {
 		return fmt.Errorf("store environment: %w", err)
 	}
 	log.Successf("Created environment %s in region %s under project %s.\n",
@@ -170,8 +170,8 @@ func (opts *InitEnvOpts) Execute() error {
 	return nil
 }
 
-func (opts *InitEnvOpts) delegateDNSFromProject(project *archer.Project) error {
-	envAccount, err := opts.envIdentity.Get()
+func (o *InitEnvOpts) delegateDNSFromProject(project *archer.Project) error {
+	envAccount, err := o.envIdentity.Get()
 	if err != nil {
 		return fmt.Errorf("getting environment account ID for DNS Delegation: %w", err)
 	}
@@ -181,16 +181,16 @@ func (opts *InitEnvOpts) delegateDNSFromProject(project *archer.Project) error {
 		return nil
 	}
 
-	opts.prog.Start(fmt.Sprintf(fmtDNSDelegationStart, color.HighlightUserInput(envAccount.Account)))
+	o.prog.Start(fmt.Sprintf(fmtDNSDelegationStart, color.HighlightUserInput(envAccount.Account)))
 
-	if err := opts.projDeployer.DelegateDNSPermissions(project, envAccount.Account); err != nil {
-		opts.prog.Stop(log.Serrorf(fmtDNSDelegationFailed, color.HighlightUserInput(envAccount.Account)))
+	if err := o.projDeployer.DelegateDNSPermissions(project, envAccount.Account); err != nil {
+		o.prog.Stop(log.Serrorf(fmtDNSDelegationFailed, color.HighlightUserInput(envAccount.Account)))
 		return err
 	}
 	return nil
 }
 
-func (opts *InitEnvOpts) humanizeEnvironmentEvents(resourceEvents []deploy.ResourceEvent) []termprogress.TabRow {
+func (o *InitEnvOpts) humanizeEnvironmentEvents(resourceEvents []deploy.ResourceEvent) []termprogress.TabRow {
 	matcher := map[termprogress.Text]termprogress.ResourceMatcher{
 		textVPC: func(event deploy.Resource) bool {
 			return event.Type == "AWS::EC2::VPC"
@@ -231,7 +231,7 @@ func (opts *InitEnvOpts) humanizeEnvironmentEvents(resourceEvents []deploy.Resou
 }
 
 // RecommendedActions returns follow-up actions the user can take after successfully executing the command.
-func (opts *InitEnvOpts) RecommendedActions() []string {
+func (o *InitEnvOpts) RecommendedActions() []string {
 	return nil
 }
 
