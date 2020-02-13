5fdd850c05ee2c7dd8cecc27bc96e2db293f6e4f
David Killmon <killmond+gh@amazon.com>
2020-02-12 22:10:25 -0800

    test: First Pass at E2E tests
    This is my first pass at real end to end tests. There are currently
    3 suites:
    
    1. Init Flow
      This is our simple, one command and have a service up command test,
      and ensures that an app can start up, logs, ls, delete work and
      the public endpoint resolves.
    2. Multi Env Project
       This sets up a project with two environments, in different accounts
       and regions. It deploys an app to both environments, ensures that
       the app is reachable, and the following commands work:
          app [show, logs, ls, deploy, init],
          project [show, ls, init, delete],
          env [init, ls]
    3. Multi App Project (single ws)
       This tests a similar set of commands, but ensures that we can deploy
       two apps to one environment. The app reachability test (calling the http
       endpoint) currently fails because that feature is broken without updating
       the manifest.
    
    There are lots of todos, but wanted to get this out as it's a good start:
    * App Package
    * Pipeline init, update, delete
    * Pipeline release (git push and wait for something to go out)
    * Multi workspace, multi app project
    
    Another important note - this change doesn't currently wire
    the e2e tests into our release process. That'll be a different
    PR. These tests can run in a Docker image so that we don't have
    to worry about dependencies, filesystem cleanup, etc.

---
+++ e2e/internal/client/cli.go
@@ -0,0 +1,338 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/onsi/ginkgo"
+	"github.com/onsi/gomega/gexec"
+)
+
+// CLI is a wrapper around os.execs
+type CLI struct {
+	path string
+}
+
+// ProjectInitRequest contains the parameters for calling ecs project init
+type ProjectInitRequest struct {
+	ProjectName string
+	Domain      string
+}
+
+// InitRequest contains the parameters for calling ecs init
+type InitRequest struct {
+	ProjectName string
+	AppName     string
+	Deploy      bool
+	ImageTag    string
+	Dockerfile  string
+	AppType     string
+}
+
+// EnvInitRequest contains the parameters for calling ecs env init
+type EnvInitRequest struct {
+	ProjectName string
+	EnvName     string
+	Profile     string
+	Prod        bool
+}
+
+// AppInitRequest contains the parameters for calling ecs app init
+type AppInitRequest struct {
+	AppName    string
+	AppType    string
+	Dockerfile string
+}
+
+// AppShowRequest contains the parameters for calling ecs app show
+type AppShowRequest struct {
+	ProjectName string
+	AppName     string
+}
+
+// AppLogsRequest contains the parameters for calling ecs app logs
+type AppLogsRequest struct {
+	ProjectName string
+	AppName     string
+	EnvName     string
+	Since       string
+}
+
+// AppDeployInput contains the parameters for calling ecs app deploy
+type AppDeployInput struct {
+	AppName  string
+	EnvName  string
+	ImageTag string
+}
+
+// NewCLI returns a wrapper around CLI
+func NewCLI() (*CLI, error) {
+	cliPath := "/bin/ecs-preview"
+	if _, err := os.Stat(cliPath); err != nil {
+		return nil, err
+	}
+
+	return &CLI{
+		path: cliPath,
+	}, nil
+}
+
+/*Help runs
+ecs-preview --help
+*/
+func (cli *CLI) Help() (string, error) {
+	return cli.exec(exec.Command(cli.path, "--help"))
+}
+
+/*Version runs:
+ecs-preview --version
+*/
+func (cli *CLI) Version() (string, error) {
+	return cli.exec(exec.Command(cli.path, "--version"))
+}
+
+/*Init runs:
+ecs-preview init
+	--project $p
+	--app $a
+	--app-type $type
+	--tag $t
+	--dockerfile $d
+	--deploy (optionally)
+*/
+func (cli *CLI) Init(opts *InitRequest) (string, error) {
+	var deployOption string
+
+	if opts.Deploy {
+		deployOption = "--deploy"
+	}
+
+	return cli.exec(
+		exec.Command(cli.path, "init",
+			"--project", opts.ProjectName,
+			"--app", opts.AppName,
+			"--app-type", opts.AppType,
+			"--tag", opts.ImageTag,
+			"--dockerfile", opts.Dockerfile,
+			deployOption))
+}
+
+/*AppInit runs:
+ecs-preview app init
+	--name $n
+	--app-type $t
+	--dockerfile $d
+*/
+func (cli *CLI) AppInit(opts *AppInitRequest) (string, error) {
+	return cli.exec(
+		exec.Command(cli.path, "app", "init",
+			"--name", opts.AppName,
+			"--app-type", opts.AppType,
+			"--dockerfile", opts.Dockerfile))
+}
+
+/*AppShow runs:
+ecs-preview app show
+	--project $p
+	--app $a
+	--json
+*/
+func (cli *CLI) AppShow(opts *AppShowRequest) (*AppShowOutput, error) {
+	appJSON, appShowErr := cli.exec(
+		exec.Command(cli.path, "app", "show",
+			"--project", opts.ProjectName,
+			"--app", opts.AppName,
+			"--json"))
+
+	if appShowErr != nil {
+		return nil, appShowErr
+	}
+
+	return toAppShowOutput(appJSON)
+}
+
+/*AppDelete runs:
+ecs-preview app delete
+	--name $n
+	--yes
+*/
+func (cli *CLI) AppDelete(appName string) (string, error) {
+	return cli.exec(
+		exec.Command(cli.path, "app", "delete",
+			"--name", appName,
+			"--yes"))
+}
+
+/*AppDeploy runs:
+ecs-preview app deploy
+	--name $n
+	--env $e
+	--tag $t
+*/
+func (cli *CLI) AppDeploy(opts *AppDeployInput) (string, error) {
+	return cli.exec(
+		exec.Command(cli.path, "app", "deploy",
+			"--name", opts.AppName,
+			"--env", opts.EnvName,
+			"--tag", opts.ImageTag))
+}
+
+/*AppList runs:
+ecs-preview app ls
+	--project $p
+	--json
+*/
+func (cli *CLI) AppList(projectName string) (*AppListOutput, error) {
+	output, err := cli.exec(
+		exec.Command(cli.path, "app", "ls",
+			"--project", projectName,
+			"--json"))
+	if err != nil {
+		return nil, err
+	}
+	return toAppListOutput(output)
+}
+
+/*AppLogs runs:
+ecs-preview app logs
+	--project $p
+	--name $n
+	--since $s
+	--env $e
+	--json
+*/
+func (cli *CLI) AppLogs(opts *AppLogsRequest) ([]AppLogsOutput, error) {
+	output, err := cli.exec(
+		exec.Command(cli.path, "app", "logs",
+			"--project", opts.ProjectName,
+			"--name", opts.AppName,
+			"--since", opts.Since,
+			"--env", opts.EnvName,
+			"--json"))
+	if err != nil {
+		return nil, err
+	}
+	return toAppLogsOutput(output)
+}
+
+/*EnvDelete runs:
+ecs-preview env delete
+	--name $n
+	--profile $p
+	--yes
+*/
+func (cli *CLI) EnvDelete(envName, profile string) (string, error) {
+	return cli.exec(
+		exec.Command(cli.path, "env", "delete",
+			"--name", envName,
+			"--profile", profile,
+			"--yes"))
+}
+
+/*EnvInit runs:
+ecs-preview env init
+	--name $n
+	--project $p
+	--profile $pr
+	--prod (optionally)
+*/
+func (cli *CLI) EnvInit(opts *EnvInitRequest) (string, error) {
+	commands := []string{"env", "init",
+		"--name", opts.EnvName,
+		"--project", opts.ProjectName,
+		"--profile", opts.Profile,
+	}
+	if opts.Prod {
+		commands = append(commands, "--prod")
+	}
+	return cli.exec(exec.Command(cli.path, commands...))
+}
+
+/*EnvList runs:
+ecs-preview env ls
+	--project $p
+	--json
+*/
+func (cli *CLI) EnvList(projectName string) (*EnvListOutput, error) {
+	output, err := cli.exec(
+		exec.Command(cli.path, "env", "ls",
+			"--project", projectName,
+			"--json"))
+	if err != nil {
+		return nil, err
+	}
+	return toEnvListOutput(output)
+}
+
+/*ProjectInit runs:
+ecs-preview project init $p
+	--domain $d (optionally)
+*/
+func (cli *CLI) ProjectInit(opts *ProjectInitRequest) (string, error) {
+	commands := []string{"project", "init", opts.ProjectName}
+	if opts.Domain != "" {
+		commands = append(commands, "--domain", opts.Domain)
+	}
+	return cli.exec(exec.Command(cli.path, commands...))
+}
+
+/*ProjectShow runs:
+ecs-preview project show
+	--project $p
+	--json
+*/
+func (cli *CLI) ProjectShow(projectName string) (*ProjectShowOutput, error) {
+	output, err := cli.exec(
+		exec.Command(cli.path, "project", "show",
+			"--project", projectName,
+			"--json"))
+	if err != nil {
+		return nil, err
+	}
+	return toProjectShowOutput(output)
+}
+
+/*ProjectList runs:
+ecs-preview project ls
+*/
+func (cli *CLI) ProjectList() (string, error) {
+	return cli.exec(exec.Command(cli.path, "project", "ls"))
+}
+
+/*ProjectDelete runs:
+ecs-preview project delete
+	--env-profiles $e1=$p1,$e2=$p2
+	--yes
+*/
+func (cli *CLI) ProjectDelete(profiles map[string]string) (string, error) {
+	commands := []string{"project", "delete", "--yes"}
+
+	if len(profiles) > 0 {
+		commands = append(commands, "--env-profiles")
+		envProfiles := []string{}
+		for env, profile := range profiles {
+			envProfiles = append(envProfiles, fmt.Sprintf("%s=%s", env, profile))
+		}
+		commands = append(commands, strings.Join(envProfiles, ","))
+	}
+	return cli.exec(
+		exec.Command(cli.path, commands...))
+}
+
+func (cli *CLI) exec(command *exec.Cmd) (string, error) {
+	// Turn off colors
+	command.Env = append(os.Environ(), "COLOR=false")
+	sess, err := gexec.Start(command, ginkgo.GinkgoWriter, ginkgo.GinkgoWriter)
+	if err != nil {
+		return "", err
+	}
+
+	contents := sess.Wait(100000000).Out.Contents()
+	if exitCode := sess.ExitCode(); exitCode != 0 {
+		return string(contents), fmt.Errorf("received non 0 exit code")
+	}
+
+	return string(contents), nil
+}
---
+++ e2e/multi-env-project/multi_env_test.go
@@ -0,0 +1,215 @@
+package multi_env_app_test
+
+import (
+	"fmt"
+	"net/http"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/aws/amazon-ecs-cli-v2/e2e/internal/client"
+)
+
+var (
+	initErr error
+)
+
+var _ = Describe("Multiple Env Project", func() {
+	Context("when creating a new project", func() {
+		BeforeAll(func() {
+			_, initErr = cli.ProjectInit(&client.ProjectInitRequest{
+				ProjectName: projectName,
+			})
+		})
+
+		It("project init succeeds", func() {
+			Expect(initErr).NotTo(HaveOccurred())
+		})
+
+		It("project init creates an ecs-project directory", func() {
+			Expect("./ecs-project").Should(BeADirectory())
+		})
+
+		It("project ls includes new project", func() {
+			projects, err := cli.ProjectList()
+			Expect(err).NotTo(HaveOccurred())
+			Expect(projects).To(ContainSubstring(projectName))
+		})
+
+		It("project show includes project name", func() {
+			projectShowOutput, err := cli.ProjectShow(projectName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(projectShowOutput.Name).To(Equal(projectName))
+			Expect(projectShowOutput.URI).To(BeEmpty())
+		})
+	})
+
+	Context("when adding cross account environments", func() {
+		var (
+			testEnvInitErr error
+			prodEnvInitErr error
+		)
+		BeforeAll(func() {
+			_, testEnvInitErr = cli.EnvInit(&client.EnvInitRequest{
+				ProjectName: projectName,
+				EnvName:     "test",
+				Profile:     testEnvironmentProfile,
+				Prod:        false,
+			})
+
+			_, prodEnvInitErr = cli.EnvInit(&client.EnvInitRequest{
+				ProjectName: projectName,
+				EnvName:     "prod",
+				Profile:     prodEnvironmentProfile,
+				Prod:        true,
+			})
+
+		})
+
+		It("env init should succeed for test and prod envs", func() {
+			Expect(testEnvInitErr).NotTo(HaveOccurred())
+			Expect(prodEnvInitErr).NotTo(HaveOccurred())
+		})
+
+		It("env ls should list both envs", func() {
+			envListOutput, err := cli.EnvList(projectName)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(envListOutput.Envs)).To(Equal(2))
+			envs := map[string]client.EnvDescription{}
+			for _, env := range envListOutput.Envs {
+				envs[env.Name] = env
+				Expect(env.ExecutionRole).NotTo(BeEmpty())
+				Expect(env.ManagerRole).NotTo(BeEmpty())
+			}
+
+			Expect(envs["test"]).NotTo(BeNil())
+			Expect(envs["test"].Prod).To(BeFalse())
+
+			Expect(envs["prod"]).NotTo(BeNil())
+			Expect(envs["prod"].Prod).To(BeTrue())
+
+			// Make sure, for the sake of coverage, these are cross account,
+			// cross region environments.
+			Expect(envs["test"].Region).NotTo(Equal(envs["prod"].Region))
+			Expect(envs["test"].Account).NotTo(Equal(envs["prod"].Account))
+		})
+	})
+
+	Context("when adding an app", func() {
+		var (
+			frontEndInitErr error
+		)
+		BeforeAll(func() {
+			_, frontEndInitErr = cli.AppInit(&client.AppInitRequest{
+				AppName:    "front-end",
+				AppType:    "Load Balanced Web App",
+				Dockerfile: "./front-end/Dockerfile", //TODO FIX MEH
+			})
+		})
+
+		It("app init should succeed", func() {
+			Expect(frontEndInitErr).NotTo(HaveOccurred())
+		})
+
+		It("app init should create an app manifest", func() {
+			Expect("./ecs-project/front-end-app.yml").Should(BeAnExistingFile())
+		})
+
+		It("app ls should list the app", func() {
+			appList, appListError := cli.AppList(projectName)
+			Expect(appListError).NotTo(HaveOccurred())
+			Expect(len(appList.Apps)).To(Equal(1))
+			Expect(appList.Apps[0].AppName).To(Equal("front-end"))
+		})
+
+		It("app package should output a cloudformation template and params file", func() {
+			Skip("not implemented yet")
+		})
+	})
+
+	Context("when deploying an app to test and prod envs", func() {
+		var (
+			testDeployErr    error
+			prodEndDeployErr error
+			appName          string
+		)
+		BeforeAll(func() {
+			appName = "front-end"
+			_, testDeployErr = cli.AppDeploy(&client.AppDeployInput{
+				AppName:  appName,
+				EnvName:  "test",
+				ImageTag: "gallopinggurdey",
+			})
+
+			_, prodEndDeployErr = cli.AppDeploy(&client.AppDeployInput{
+				AppName:  appName,
+				EnvName:  "prod",
+				ImageTag: "gallopinggurdey",
+			})
+		})
+
+		It("app deploy should succeed to both environment", func() {
+			Expect(testDeployErr).NotTo(HaveOccurred())
+			Expect(prodEndDeployErr).NotTo(HaveOccurred())
+		})
+
+		It("app show should include a valid URL and description for test and prod envs", func() {
+			app, appShowErr := cli.AppShow(&client.AppShowRequest{
+				ProjectName: projectName,
+				AppName:     appName,
+			})
+			Expect(appShowErr).NotTo(HaveOccurred())
+			Expect(len(app.Routes)).To(Equal(2))
+			// Group routes by environment
+			envRoutes := map[string]client.AppShowRoutes{}
+			for _, route := range app.Routes {
+				envRoutes[route.Environment] = route
+			}
+
+			// Call each environment's endpoint and ensure it returns a 200
+			for _, env := range []string{"test", "prod"} {
+				route := envRoutes[env]
+				Expect(route.Environment).To(Equal(env))
+				Expect(route.Path).To(Equal("*"))
+				resp, fetchErr := http.Get(fmt.Sprintf("http://%s", route.URL))
+				Expect(fetchErr).NotTo(HaveOccurred())
+				Expect(resp.StatusCode).To(Equal(200))
+			}
+		})
+
+		It("app logs should display logs", func() {
+			for _, envName := range []string{"test", "prod"} {
+				appLogs, appLogsErr := cli.AppLogs(&client.AppLogsRequest{
+					ProjectName: projectName,
+					AppName:     appName,
+					EnvName:     envName,
+					Since:       "1h",
+				})
+				Expect(appLogsErr).NotTo(HaveOccurred())
+				for _, logLine := range appLogs {
+					Expect(logLine.Message).NotTo(Equal(""))
+					Expect(logLine.TaskID).NotTo(Equal(""))
+					Expect(logLine.Timestamp).NotTo(Equal(0))
+					Expect(logLine.IngestionTime).NotTo(Equal(0))
+				}
+			}
+		})
+	})
+
+	Context("when setting up a pipeline", func() {
+		It("pipeline init should create a pipeline manifest", func() {
+			Skip("not implemented yet")
+		})
+
+		It("pipeline update should create a pipeline", func() {
+			Skip("not implemented yet")
+		})
+	})
+
+	Context("when pushing a change to the pipeline", func() {
+		It("the change should be propagated to test and prod environments", func() {
+			Skip("not implemented yet")
+		})
+	})
+
+})
