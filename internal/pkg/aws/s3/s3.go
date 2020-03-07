2d948b7b431bf9bf9b5edceb61f75055a2e60de7
Penghao He <penghaoh@amazon.com>
2020-03-06 17:04:29 -0800

    fix(cli): project delete empty s3 buckets before deleting (#714)

--- internal/pkg/aws/s3/s3.go
+++ internal/pkg/aws/s3/s3.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
@@ -20,19 +21,26 @@ const (
 	artifactDirName = "manual"
 )
 
-type s3Client interface {
+type s3ManagerClient interface {
 	Upload(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error)
+}
+
+type s3Client interface {
+	ListObjectVersions(input *s3.ListObjectVersionsInput) (*s3.ListObjectVersionsOutput, error)
+	DeleteObjects(input *s3.DeleteObjectsInput) (*s3.DeleteObjectsOutput, error)
 }
 
 // Service wraps an Amazon Simple Storage Service client.
 type Service struct {
-	s3Svc s3Client
+	s3ManagerSvc s3ManagerClient
+	s3Svc        s3Client
 }
 
 // New returns a Service configured against the input session.
 func New(s *session.Session) *Service {
 	return &Service{
-		s3Svc: s3manager.NewUploader(s),
+		s3ManagerSvc: s3manager.NewUploader(s),
+		s3Svc:        s3.New(s),
 	}
 }
 
@@ -41,7 +49,7 @@ func New(s *session.Session) *Service {
 func (s *Service) PutArtifact(bucket, fileName string, data io.Reader) (string, error) {
 	id := time.Now().Unix()
 	key := path.Join(artifactDirName, strconv.FormatInt(id, 10), fileName)
-	resp, err := s.s3Svc.Upload(&s3manager.UploadInput{
+	resp, err := s.s3ManagerSvc.Upload(&s3manager.UploadInput{
 		Body:   data,
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
@@ -51,4 +59,46 @@ func (s *Service) PutArtifact(bucket, fileName string, data io.Reader) (string, 
 	}
 
 	return resp.Location, nil
+}
+
+// EmptyBucket deletes all objects within the bucket.
+func (s *Service) EmptyBucket(bucket string) error {
+	var listResp *s3.ListObjectVersionsOutput
+	var err error
+	listParams := &s3.ListObjectVersionsInput{
+		Bucket: aws.String(bucket),
+	}
+	// Remove all versions of all objects.
+	for {
+		listResp, err = s.s3Svc.ListObjectVersions(listParams)
+		if err != nil {
+			return fmt.Errorf("list objects for bucket %s: %w", bucket, err)
+		}
+		var objectsToDelete []*s3.ObjectIdentifier
+		for _, object := range listResp.Versions {
+			objectsToDelete = append(objectsToDelete, &s3.ObjectIdentifier{
+				Key:       object.Key,
+				VersionId: object.VersionId,
+			})
+		}
+		// After deleting other versions, remove delete markers version.
+		// For info on "delete marker": https://docs.aws.amazon.com/AmazonS3/latest/dev/DeleteMarker.html
+		for _, deleteMarker := range listResp.DeleteMarkers {
+			objectsToDelete = append(objectsToDelete, &s3.ObjectIdentifier{
+				Key:       deleteMarker.Key,
+				VersionId: deleteMarker.VersionId,
+			})
+		}
+		_, err = s.s3Svc.DeleteObjects(&s3.DeleteObjectsInput{
+			Bucket: aws.String(bucket),
+			Delete: &s3.Delete{
+				Objects: objectsToDelete,
+			},
+		})
+		if !aws.BoolValue(listResp.IsTruncated) {
+			return nil
+		}
+		listParams.KeyMarker = listResp.NextKeyMarker
+		listParams.VersionIdMarker = listResp.NextVersionIdMarker
+	}
 }
