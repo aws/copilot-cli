82cfa3b8ffc869a83e5a35d23936f8b716d1f7fc
David Killmon <killmond+gh@amazon.com>
2020-03-09 11:20:31 -0700

    feat: service discovery by default
    This change enables service discovery by default for
    all apps. Service discovery alows apps within an
    environment to communicate with each other without
    having to traverse the internet (traffic is routed
    internally within the VPC).
    
    To access a service via its service discovery endpoint,
    you can make a request like:
    
    ```golang
      resp, _ := http.Get(fmt.Sprintf("http://%s.%s", appName, os.Getenv("ECS_APP_DISCOVERY_ENDPOINT"))
    ```
    
    Or, with only the project name:
    ```golang
      resp, _ := http.Get(fmt.Sprintf("http://%s.%s.local", appName, projectName))
    ```
    
    Or, without the string interpolation:
    
    ```golang
      # Making a request to app `api` in project `kudos`
      resp, _ := http.Get("http://api.kudos.local")
    ```
    
    The same address works across environments, since the requests
    are scoped to services within one environment.
    
    __ Why not just .local ?__
    
    A couple of reasons to go for the `.{project}.local` scheme, rather
    than just `.local` or just `.{project}`.
    
    The reason for not using just `.local` is just to reduce the chance
    of a namespace colision if another project is using the same VPC.
    
    The reason for not just going with `.{project}` is in case the
    project name matches an existing TLD. That'd be confusing behavior.
    The `.local` TLD doesn't seem to exist.
    
    __ Why make this default for every service? __
    
    You can't add service discovery after the fact, unfortunately.
    The only sensible thing is to just enable it by default. In the
    future, we can offer a disable knob, but it's very cheap (less
    than a dollar per month per env) - so isn't a huge burden for
    customers who end up not using it.
    
    resolves #599

---
+++ e2e/multi-app-project/back-end/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// HealthCheck just returns true if the service is up.
+func HealthCheck(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	log.Println("🚑 healthcheck ok!")
+	w.WriteHeader(http.StatusOK)
+}
+
+// SimpleGet just returns true no matter what
+func SimpleGet(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	log.Println("Get Succeeded")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("back-end"))
+}
+
+// ServiceDiscoveryGet just returns true no matter what
+func ServiceDiscoveryGet(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+	log.Println("Get on ServiceDiscovery endpoint Succeeded")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("back-end-service-discovery"))
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/back-end/", SimpleGet)
+	router.GET("/service-discovery/", ServiceDiscoveryGet)
+
+	// Health Check
+	router.GET("/", HealthCheck)
+
+	log.Fatal(http.ListenAndServe(":80", router))
+}
