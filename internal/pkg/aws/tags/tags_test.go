777bc8ee711c9848a1ad59da19c8634b496245b5
Efe Karakus <karakuse@amazon.com>
2020-03-26 13:43:45 -0700

    feat: add --resource-tags flag to deploy command (#788)
    Resolves #782
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ internal/pkg/aws/tags/tags_test.go
@@ -0,0 +1,44 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package tags
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMerge(t *testing.T) {
+	// GIVEN
+	projectTags := map[string]string{
+		"owner": "boss",
+	}
+	envTags := map[string]string{
+		"owner": "boss2",
+		"stage": "test",
+	}
+	appTags := map[string]string{
+		"owner":   "boss3",
+		"stage":   "test2",
+		"service": "gateway",
+	}
+
+	// WHEN
+	out := Merge(projectTags, envTags, appTags)
+
+	// THEN
+	require.Equal(t, map[string]string{
+		"owner":   "boss3",
+		"stage":   "test2",
+		"service": "gateway",
+	}, out)
+	// Ensure original tags are not modified.
+	require.Equal(t, map[string]string{
+		"owner": "boss",
+	}, projectTags)
+	require.Equal(t, map[string]string{
+		"owner": "boss2",
+		"stage": "test",
+	}, envTags)
+}
