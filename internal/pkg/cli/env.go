5a0d44fbe58e1fbde5a02e641f0d4d6bde3aaf5f
huanjani <60631893+huanjani@users.noreply.github.com>
2020-04-03 10:44:58 -0700

    feat: scaffolding for env show (#807)
    * start adapting app_show to env_show
    
    * feat: env show wip
    
    * build: env show help
    
    * docs: env show help name flags
    
    * chore: start taking out unrelated code
    
    * fix: add note explaining ws's presence
    
    * chore: ignore all .idea directories
    
    * fix: change 'app' to 'env'
    
    * fix: comment out command in env.go and start removing extraneous code in env_show.go
    
    * fix: finish delete extraneous code for help menu
    
    * fix: delete .idea dirs
    
    * fix: delete templates
    
    * fix: delete env show test
    
    * fix: delete extra indent
    
    * fix: change commented out BuildEnvShowCmd to hidden; take out extra line

--- internal/pkg/cli/env.go
+++ internal/pkg/cli/env.go
@@ -26,6 +26,7 @@ An environment represents a deployment stage.`,
 	cmd.AddCommand(BuildEnvInitCmd())
 	cmd.AddCommand(BuildEnvListCmd())
 	cmd.AddCommand(BuildEnvDeleteCmd())
+	cmd.AddCommand(BuildEnvShowCmd())
 	cmd.SetUsageTemplate(template.Usage)
 	cmd.Annotations = map[string]string{
 		"group": group.Develop,
---
+++ internal/pkg/cli/env_show.go
@@ -0,0 +1,77 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+
+package cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+type showEnvVars struct {
+	*GlobalOpts
+	shouldOutputJSON      bool
+	shouldOutputResources bool
+	envName               string
+}
+
+type showEnvOpts struct {
+	showEnvVars
+}
+
+func newShowEnvOpts(vars showEnvVars) (*showEnvOpts, error) {
+	return &showEnvOpts{
+		showEnvVars: 	vars,
+	}, nil
+}
+
+// Validate returns an error if the values provided by the user are invalid.
+func (o *showEnvOpts) Validate() error {
+	return nil
+}
+
+// Ask asks for fields that are required but not passed in.
+func (o *showEnvOpts) Ask() error {
+	return nil
+}
+
+// Execute shows the environments through the prompt.
+func (o *showEnvOpts) Execute() error {
+	return nil
+}
+
+// BuildEnvShowCmd builds the command for showing environments in a project.
+func BuildEnvShowCmd() *cobra.Command {
+	vars := showEnvVars{
+		GlobalOpts: NewGlobalOpts(),
+	}
+	cmd := &cobra.Command{
+		Hidden: true, //TODO remove when ready for production!
+		Use:   "show",
+		Short: "Shows info about a deployed environment.",
+		Long:  "Shows info about a deployed environment, including region, account ID, and apps.",
+
+		Example: `
+  Shows info about the environment "test"
+  /code $ ecs-preview env show -n test`,
+		RunE: runCmdE(func(cmd *cobra.Command, args []string) error {
+			opts, err := newShowEnvOpts(vars)
+			if err != nil {
+				return err
+			}
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+			if err := opts.Ask(); err != nil {
+				return err
+			}
+			return opts.Execute()
+		}),
+	}
+	// The flags bound by viper are available to all sub-commands through viper.GetString({flagName})
+	cmd.Flags().StringVarP(&vars.envName, nameFlag, nameFlagShort, "", envFlagDescription)
+	cmd.Flags().BoolVar(&vars.shouldOutputJSON, jsonFlag, false, jsonFlagDescription)
+	cmd.Flags().BoolVar(&vars.shouldOutputResources, resourcesFlag, false, resourcesFlagDescription)
+	cmd.Flags().StringVarP(&vars.projectName, projectFlag, projectFlagShort, "", projectFlagDescription)
+	return cmd
+}
