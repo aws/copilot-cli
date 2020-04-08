2fa15045149f538e405f36638f8ee28c4c87e2ae
Hsing-Hui Hsu <hsinghui@amazon.com>
2020-04-08 14:22:56 -0700

    implement pipeline_show#Ask, part the first
    Move the logic of reading the pipeline manifest into Ask instead of
    Validate. Validating the pipeline name will now involve making a call to
    codePipeline (TODO)

--- internal/pkg/cli/pipeline_show.go
+++ internal/pkg/cli/pipeline_show.go
@@ -8,19 +8,26 @@ import (
 
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/manifest"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/store"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/term/color"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/term/log"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/workspace"
 	"github.com/spf13/cobra"
+)
+
+const (
+	pipelineShowProjectNamePrompt     = "Which project's pipelines would you like to show?"
+	pipelineShowProjectNameHelpPrompt = "A project groups all of your pipelines together."
 )
 
 type showPipelineVars struct {
 	*GlobalOpts
 	shouldOutputJSON      bool
 	shouldOutputResources bool
+	pipelineName          string
 }
 
 type showPipelineOpts struct {
 	showPipelineVars
-	pipelineName string
 
 	// Interfaces to dependencies
 	ws    wsPipelineReader
@@ -54,21 +61,8 @@ func (o *showPipelineOpts) Validate() error {
 			return err
 		}
 	}
-
-	data, err := o.ws.ReadPipelineManifest()
-	if err != nil {
-		return fmt.Errorf("read pipeline manifest: %w", err)
-	}
-
-	pipeline, err := manifest.UnmarshalPipeline(data)
-	if err != nil {
-		return fmt.Errorf("unmarshal pipeline manifest: %w", err)
-	}
-
-	o.pipelineName = pipeline.Name
-
-	if o.pipelineName == "" {
-		return errNoPipelineInWorkspace
+	if o.pipelineName != "" {
+		// TODO check codepipeline if pipelineName is valid
 	}
 
 	return nil
@@ -76,8 +70,84 @@ func (o *showPipelineOpts) Validate() error {
 
 // Ask prompts for fields that are required but not passed in.
 func (o *showPipelineOpts) Ask() error {
-	// TODO Placeholder
+	if err := o.askProject(); err != nil {
+		return err
+	}
+
+	return o.askPipelineName()
+}
+
+func (o *showPipelineOpts) askProject() error {
+	if o.ProjectName() != "" {
+		return nil
+	}
+	projNames, err := o.retrieveProjects()
+	if err != nil {
+		return err
+	}
+	if len(projNames) == 0 {
+		return fmt.Errorf("no project found: run %s please", color.HighlightCode("project init"))
+	}
+	proj, err := o.prompt.SelectOne(
+		pipelineShowProjectNamePrompt,
+		pipelineShowProjectNameHelpPrompt,
+		projNames,
+	)
+	if err != nil {
+		return fmt.Errorf("select projects: %w", err)
+	}
+	o.projectName = proj
+
 	return nil
+}
+
+func (o *showPipelineOpts) retrieveProjects() ([]string, error) {
+	projs, err := o.store.ListProjects()
+	if err != nil {
+		return nil, fmt.Errorf("list projects: %w", err)
+	}
+	projNames := make([]string, len(projs))
+	for ind, proj := range projs {
+		projNames[ind] = proj.Name
+	}
+	return projNames, nil
+}
+
+func (o *showPipelineOpts) askPipelineName() error {
+	// return if pipeline name is set by flag
+	if o.pipelineName != "" {
+		return nil
+	}
+
+	pipelineName, err := o.getPipelineNameFromManifest()
+	fmt.Println(pipelineName)
+	if err != nil {
+		return err
+	}
+
+	o.pipelineName = pipelineName
+	// if pipelineName == "" {
+	// get pipeline name from remote
+	// }
+
+	return nil
+}
+
+func (o *showPipelineOpts) getPipelineNameFromManifest() (string, error) {
+	// try to read pipelinename from from manifest
+
+	data, err := o.ws.ReadPipelineManifest()
+	if err != nil {
+		log.Infof("No pipeline manifest in workspace for project %s\n.", color.HighlightUserInput(o.ProjectName()))
+		return "", nil
+	}
+
+	pipeline, err := manifest.UnmarshalPipeline(data)
+	if err != nil {
+		return "", fmt.Errorf("unmarshal pipeline manifest: %w", err)
+	}
+
+	return pipeline.Name, nil
 }
 
 // Execute writes the pipeline manifest file.
@@ -113,6 +183,7 @@ func BuildPipelineShowCmd() *cobra.Command {
 			return opts.Execute()
 		}),
 	}
+	cmd.Flags().StringVarP(&vars.pipelineName, nameFlag, nameFlagShort, "", pipelineFlagDescription)
 	cmd.Flags().StringVarP(&vars.projectName, projectFlag, projectFlagShort, "", projectFlagDescription)
 	cmd.Flags().BoolVar(&vars.shouldOutputJSON, jsonFlag, false, jsonFlagDescription)
 	cmd.Flags().BoolVar(&vars.shouldOutputResources, resourcesFlag, false, resourcesFlagDescription)
--- internal/pkg/cli/pipeline_show_test.go
+++ internal/pkg/cli/pipeline_show_test.go
@@ -5,7 +5,7 @@ package cli
 
 import (
 	"errors"
-	"fmt"
+	// "fmt"
 	"testing"
 
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
@@ -15,14 +15,84 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var (
+	mockError        = errors.New("mock error")
+	mockProjectName  = "dinder"
+	mockPipelineName = "pipeline-dinder-badgoose-repo"
+)
+
 type showPipelineMocks struct {
-	store *mocks.MockstoreReader
-	ws    *mocks.MockwsPipelineReader
+	store  *mocks.MockstoreReader
+	ws     *mocks.MockwsPipelineReader
+	prompt *mocks.Mockprompter
 }
 
 func TestPipelineShow_Validate(t *testing.T) {
-	mockError := errors.New("mock error")
-	mockProjectName := "dinder"
+	testCases := map[string]struct {
+		inProjectName string
+		setupMocks    func(mocks showPipelineMocks)
+
+		expectedErr error
+	}{
+		"with valid project name and pipeline name": {
+			inProjectName: mockProjectName,
+			setupMocks: func(mocks showPipelineMocks) {
+				gomock.InOrder(
+					mocks.store.EXPECT().GetProject("dinder").Return(&archer.Project{
+						Name: "dinder",
+					}, nil),
+				)
+			},
+			expectedErr: nil,
+		},
+		"with invalid project name": {
+			inProjectName: mockProjectName,
+			setupMocks: func(mocks showPipelineMocks) {
+				gomock.InOrder(
+					mocks.store.EXPECT().GetProject("dinder").Return(nil, mockError),
+				)
+			},
+			expectedErr: mockError,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// GIVEN
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStoreReader := mocks.NewMockstoreReader(ctrl)
+
+			mocks := showPipelineMocks{
+				store: mockStoreReader,
+			}
+
+			tc.setupMocks(mocks)
+
+			opts := &showPipelineOpts{
+				showPipelineVars: showPipelineVars{
+					GlobalOpts: &GlobalOpts{
+						projectName: tc.inProjectName,
+					},
+				},
+				store: mockStoreReader,
+			}
+
+			// WHEN
+			err := opts.Validate()
+
+			// THEN
+			if tc.expectedErr != nil {
+				require.EqualError(t, err, tc.expectedErr.Error())
+			} else {
+				require.Nil(t, err)
+			}
+		})
+	}
+}
+
+func TestPipelineShow_Ask(t *testing.T) {
 	pipelineData := `
 name: pipeline-dinder-badgoose-repo
 version: 1
@@ -42,43 +112,35 @@ stages:
 `
 
 	testCases := map[string]struct {
-		inProjectName string
-		setupMocks    func(mocks showPipelineMocks)
+		inProjectName  string
+		inPipelineName string
+		setupMocks     func(mocks showPipelineMocks)
 
-		wantedError error
+		expectedProject  string
+		expectedPipeline string
+		expectedErr      error
 	}{
-		"with valid project name and pipeline name": {
+		"happy path with project and pipeline flags": {
+			inProjectName:  mockProjectName,
+			inPipelineName: mockPipelineName,
+
+			setupMocks: func(mocks showPipelineMocks) {},
+
+			expectedProject:  mockProjectName,
+			expectedPipeline: mockPipelineName,
+			expectedErr:      nil,
+		},
+
+		"reads pipeline name from manifest": {
 			inProjectName: mockProjectName,
 			setupMocks: func(mocks showPipelineMocks) {
 				gomock.InOrder(
-					mocks.store.EXPECT().GetProject("dinder").Return(&archer.Project{
-						Name: "dinder",
-					}, nil),
 					mocks.ws.EXPECT().ReadPipelineManifest().Return([]byte(pipelineData), nil),
 				)
 			},
-			wantedError: nil,
-		},
-		"with invalid project name": {
-			inProjectName: mockProjectName,
-			setupMocks: func(mocks showPipelineMocks) {
-				gomock.InOrder(
-					mocks.store.EXPECT().GetProject("dinder").Return(nil, mockError),
-				)
-			},
-			wantedError: mockError,
-		},
-		"with no pipeline in workspace": {
-			inProjectName: "dinder",
-			setupMocks: func(mocks showPipelineMocks) {
-				gomock.InOrder(
-					mocks.store.EXPECT().GetProject("dinder").Return(&archer.Project{
-						Name: "dinder",
-					}, nil),
-					mocks.ws.EXPECT().ReadPipelineManifest().Return([]byte(""), mockError),
-				)
-			},
-			wantedError: fmt.Errorf("read pipeline manifest: %w", mockError),
+			expectedProject:  mockProjectName,
+			expectedPipeline: mockPipelineName,
+			expectedErr:      nil,
 		},
 	}
 
@@ -88,43 +150,40 @@ stages:
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 
-			// NOTE: we don't want to actually make network (in the
-			// case of the store, which calls SSM) or system calls
-			// (in the case of the workspace, which reads a file on
-			// disc) so we create these mocks which we use when
-			// creating the &showPipelineOpts. The
-			// deletePipelineMocks struct is there to make the
-			// tests cases cleaner when we assert expectations on
-			// what they're supposed to do within the method we are
-			// testing.
 			mockStoreReader := mocks.NewMockstoreReader(ctrl)
 			mockWorkspace := mocks.NewMockwsPipelineReader(ctrl)
+			mockPrompt := mocks.NewMockprompter(ctrl)
 
 			mocks := showPipelineMocks{
-				store: mockStoreReader,
-				ws:    mockWorkspace,
+				store:  mockStoreReader,
+				ws:     mockWorkspace,
+				prompt: mockPrompt,
 			}
 
-			tc.setupMocks(mocks) // NOTE: this function call is where we make the actual assertions on what we expect the mock objects to do
+			tc.setupMocks(mocks)
 
 			opts := &showPipelineOpts{
 				showPipelineVars: showPipelineVars{
 					GlobalOpts: &GlobalOpts{
+						prompt:      mockPrompt,
 						projectName: tc.inProjectName,
 					},
+					pipelineName: tc.inPipelineName,
 				},
 				store: mockStoreReader,
 				ws:    mockWorkspace,
 			}
 
 			// WHEN
-			err := opts.Validate()
+			err := opts.Ask()
 
 			// THEN
-			if tc.wantedError != nil {
-				require.EqualError(t, err, tc.wantedError.Error())
+			if tc.expectedErr != nil {
+				require.EqualError(t, err, tc.expectedErr.Error())
 			} else {
 				require.Nil(t, err)
+				require.Equal(t, tc.expectedProject, opts.ProjectName(), "expected project name to match")
+				require.Equal(t, tc.expectedPipeline, opts.pipelineName, "expected pipeline name to match")
 			}
 		})
 	}
