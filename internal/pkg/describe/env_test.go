b4de504ee910c104f1174e1f9ad2ded0862b5c4b
Janice Huang <huanjani@amazon.com>
2020-05-11 15:42:27 -0700

    wip

--- internal/pkg/describe/env_test.go
+++ internal/pkg/describe/env_test.go
@@ -4,24 +4,29 @@
 package describe
 
 import (
+	"errors"
+	"fmt"
+	"testing"
+
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy/cloudformation/stack"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/describe/mocks"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 type envDescriberMocks struct {
-	mockStoreService   *mocks.MockstoreSvc
+	mockStoreService         *mocks.MockstoreSvc
 	mockResourceGroupsClient *mocks.MockresourceGroupsClient
 }
 
 func TestEnvDescriber_FilterAppsForEnv(t *testing.T) {
 	const (
-		badParsedStack  = "badMockArn"
-		testParsedStack1 = "phonetool-test-app1"
-		testParsedStack2 = "phonetool-prod-app2"
+		testARN1         = "arn:aws:cloudformation:us-west-2:123456789012:stack/testProject-testEnv1-testApp1/6d75d1g0-8b1a-11ea-b358-06c1882c17fd"
+		testARN2         = "arn:aws:cloudformation:us-west-2:123456789012:stack/testProject-testEnv1-testApp3/7d75d1f0-8c1a-11ea-b358-06c1882c17fc"
+		testParsedStack1 = "testProject-test-app1"
+		testParsedStack2 = "testProject-prod-app2"
+		testParsedStack3 = "testProject-test-app3"
 	)
 	testProject := &archer.Project{
 		Name: "testProject",
@@ -38,43 +43,47 @@ func TestEnvDescriber_FilterAppsForEnv(t *testing.T) {
 	}
 	testApp1 := &archer.Application{
 		Project: "testProject",
-		Name: "testApp1",
-		Type: "load-balanced",
+		Name:    "testApp1",
+		Type:    "load-balanced",
 	}
-	testApp3 :=	&archer.Application{
+	testApp3 := &archer.Application{
 		Project: "testProject",
 		Name:    "testApp3",
-		Type: "load-balanced",
+		Type:    "load-balanced",
 	}
 	env1apps := []*archer.Application{testApp1, testApp3}
 	badTags := map[string]string{stack.EnvTagKey: "badEnvName"}
+	goodTags := map[string]string{stack.EnvTagKey: "testEnv1"}
+	mockError := errors.New("some error")
 	testCases := map[string]struct {
-		env *archer.Environment
-		proj *archer.Project
-		apps []*archer.Application
+		//env  *archer.Environment
+		//proj *archer.Project
+		//apps []*archer.Application
 
-		setupMocks func(mocks envDescriberMocks)
-		mockrgClient	func(m *mocks.MockresourceGroupsClient)
+		setupMocks   func(mocks envDescriberMocks)
+		mockrgClient func(m *mocks.MockresourceGroupsClient)
 
-		wantedApps   []*archer.Application
+		wantedApps  []*archer.Application
 		wantedError error
 	}{
 		"return error if fail to get ARN": {
-			env: testEnv1,
-			proj: testProject,
-			apps: env1apps,
-			setupMocks: 	func(m envDescriberMocks){
+			setupMocks: func(m envDescriberMocks) {
 				gomock.InOrder(
-					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, badTags).Return())
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, badTags).Return(nil, mockError),
+				)
 			},
-
-
+			wantedError: fmt.Errorf("get ARN: some error"),
 		},
-		"return error if fail to get stack name from ARN": {
-
-		},
-		"successfully return correct apps for env": {
-
+		"successfully return appropriate app objects": {
+			setupMocks: func(m envDescriberMocks) {
+				gomock.InOrder(
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, goodTags).Return([]string{
+						testARN1,
+						testARN2,
+					}, nil),
+				)
+			},
+			wantedApps: env1apps,
 		},
 	}
 
@@ -87,18 +96,18 @@ func TestEnvDescriber_FilterAppsForEnv(t *testing.T) {
 			mockStoreService := mocks.NewMockstoreSvc(ctrl)
 			mockResourceGroupsClient := mocks.NewMockresourceGroupsClient(ctrl)
 			mocks := envDescriberMocks{
-				mockStoreService:   mockStoreService,
+				mockStoreService:         mockStoreService,
 				mockResourceGroupsClient: mockResourceGroupsClient,
 			}
 
 			tc.setupMocks(mocks)
 
 			d := &EnvDescriber{
-				env: testEnvironment1,
+				env:  testEnv1,
 				proj: testProject,
 				apps: env1apps,
 
-				store: mockStoreService,
+				store:    mockStoreService,
 				rgClient: mockResourceGroupsClient,
 			}
 
