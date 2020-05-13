d3e2a7f574e46b1c72a81d9cac333b167a8c48ff
Janice Huang <huanjani@amazon.com>
2020-05-13 09:55:11 -0700

    chore: add Describe tests back in

--- internal/pkg/describe/env.go
+++ internal/pkg/describe/env.go
@@ -1,21 +1,31 @@
-// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 package describe
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy/cloudformation/stack"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/term/color"
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"sort"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/aws/resourcegroups"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/aws/session"
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/store"
-	sess "github.com/aws/aws-sdk-go/aws/session"
 )
 
+const (
+	cloudformationResourceType = "AWS::CloudFormation::Stack"
+)
+
+type resourceGroupsClient interface {
+	GetResourcesByTags(resourceType string, tags map[string]string) ([]string, error)
+}
+
+// EnvDescription contains the information about an environment.
 type EnvDescription struct {
 	Environment  *archer.Environment   `json:"environment"`
 	Applications []*archer.Application `json:"applications"`
@@ -25,10 +35,11 @@ type EnvDescription struct {
 // EnvDescriber retrieves information about an environment.
 type EnvDescriber struct {
 	env  *archer.Environment
+	proj *archer.Project
 	apps []*archer.Application
 
-	store        storeSvc
-	sessProvider *sess.Session
+	store    storeSvc
+	rgClient resourceGroupsClient
 }
 
 // NewEnvDescriber instantiates an environment describer.
@@ -39,8 +50,9 @@ func NewEnvDescriber(projectName string, envName string) (*EnvDescriber, error) 
 	}
 	env, err := svc.GetEnvironment(projectName, envName)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("get environment: %w", err)
 	}
+	proj, err := svc.GetProject(projectName)
 	apps, err := svc.ListApplications(projectName)
 	if err != nil {
 		return nil, err
@@ -50,21 +62,66 @@ func NewEnvDescriber(projectName string, envName string) (*EnvDescriber, error) 
 		return nil, fmt.Errorf("assuming role for environment %s: %w", env.ManagerRoleARN, err)
 	}
 	return &EnvDescriber{
-		env:          env,
-		store:        svc,
-		apps:         apps,
-		sessProvider: sess,
+		env:      env,
+		store:    svc,
+		proj:     proj,
+		apps:     apps,
+		rgClient: resourcegroups.New(sess),
 	}, nil
 }
 
 // Describe returns info about a project's environment.
 func (e *EnvDescriber) Describe() (*EnvDescription, error) {
-	var tags map[string]string
+	appsForEnv, err := e.filterAppsForEnv()
+	if err != nil {
+		return nil, err
+	}
+
 	return &EnvDescription{
 		Environment:  e.env,
-		Applications: e.apps,
-		Tags:         tags,
+		Applications: appsForEnv,
+		Tags:         e.proj.Tags,
 	}, nil
+}
+
+func (e *EnvDescriber) filterAppsForEnv() ([]*archer.Application, error) {
+	var appObjects []*archer.Application
+
+	tags := map[string]string{
+		stack.EnvTagKey: e.env.Name,
+	}
+	arns, err := e.rgClient.GetResourcesByTags(cloudformationResourceType, tags)
+	if err != nil {
+		return nil, fmt.Errorf("get resources for env %s: %w", e.env.Name, err)
+	}
+
+	stacksOfEnvironment := make(map[string]bool)
+	for _, arn := range arns {
+		stack, err := e.getStackName(arn)
+		if err != nil {
+			return nil, fmt.Errorf("get stack name from arn %s: %w", arn, err)
+		}
+		stacksOfEnvironment[stack] = true
+	}
+	for _, app := range e.apps {
+		stackName := stack.NameForApp(e.proj.Name, e.env.Name, app.Name)
+		if stacksOfEnvironment[stackName] {
+			appObjects = append(appObjects, app)
+		}
+	}
+	return appObjects, nil
+}
+
+func (e *EnvDescriber) getStackName(resourceArn string) (string, error) {
+	parsedArn, err := arn.Parse(resourceArn)
+	if err != nil {
+		return "", fmt.Errorf("parse ARN %s: %w", resourceArn, err)
+	}
+	stack := strings.Split(parsedArn.Resource, "/")
+	if len(stack) < 2 {
+		return "", fmt.Errorf("cannot parse ARN resource %s", parsedArn.Resource)
+	}
+	return stack[1], nil
 }
 
 // JSONString returns the stringified EnvDescription struct with json format.
@@ -91,6 +148,21 @@ func (e *EnvDescription) HumanString() string {
 	fmt.Fprintf(writer, "  %s\t%s\n", "Name", "Type")
 	for _, app := range e.Applications {
 		fmt.Fprintf(writer, "  %s\t%s\n", app.Name, app.Type)
+	}
+	writer.Flush()
+	if len(e.Tags) != 0 {
+		fmt.Fprintf(writer, color.Bold.Sprint("\nTags\n\n"))
+		writer.Flush()
+		fmt.Fprintf(writer, "  %s\t%s\n", "Key", "Value")
+		// sort Tags in alpha order by keys
+		keys := make([]string, 0, len(e.Tags))
+		for k := range e.Tags {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, key := range keys {
+			fmt.Fprintf(writer, "  %s\t%s\n", key, e.Tags[key])
+		}
 	}
 	writer.Flush()
 	return b.String()
--- internal/pkg/describe/env_test.go
+++ internal/pkg/describe/env_test.go
@@ -4,9 +4,142 @@
 package describe
 
 import (
+	"errors"
+	"fmt"
+	"testing"
+
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/archer"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/deploy/cloudformation/stack"
+	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/describe/mocks"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
+
+type envDescriberMocks struct {
+	storeSvc                 *mocks.MockstoreSvc
+	mockResourceGroupsClient *mocks.MockresourceGroupsClient
+}
+
+func TestEnvDescriber_Describe(t *testing.T) {
+	const (
+		testARN1 = "arn:aws:cloudformation:us-west-2:123456789012:stack/testProject-testEnv-testApp1/6d75d1g0-8b1a-11ea-b358-06c1882c17fd"
+		testARN2 = "arn:aws:cloudformation:us-west-2:123456789012:stack/testProject-testEnv-testApp2/7d75d1f0-8c1a-11ea-b358-06c1882c17fc"
+		badARN   = "arn:aws:cloudformation:us-west-2:123456789012:stacktestProject-testEnv-testApp16d75d1g0-8b1a-11ea-b358-06c1882c17fd"
+	)
+	testProject := &archer.Project{
+		Name: "testProject",
+		Tags: map[string]string{"key1": "value1", "key2": "value2"},
+	}
+	testEnv := &archer.Environment{
+		Project:          "testProject",
+		Name:             "testEnv",
+		Region:           "us-west-2",
+		AccountID:        "123456789012",
+		Prod:             false,
+		RegistryURL:      "",
+		ExecutionRoleARN: "",
+		ManagerRoleARN:   "",
+	}
+	testApp1 := &archer.Application{
+		Project: "testProject",
+		Name:    "testApp1",
+		Type:    "load-balanced",
+	}
+	testApp2 := &archer.Application{
+		Project: "testProject",
+		Name:    "testApp2",
+		Type:    "load-balanced",
+	}
+	testApp3 := &archer.Application{
+		Project: "testProject",
+		Name:    "testApp3",
+		Type:    "load-balanced",
+	}
+
+	allApps := []*archer.Application{testApp1, testApp2, testApp3}
+	envApps := []*archer.Application{testApp1, testApp2}
+	rgTags := map[string]string{stack.EnvTagKey: "testEnv"}
+	mockError := errors.New("some error")
+	testCases := map[string]struct {
+		setupMocks func(mocks envDescriberMocks)
+
+		wantedEnv   *EnvDescription
+		wantedApps  []*archer.Application
+		wantedError error
+	}{
+		"error if GetResourcesByTags fails": {
+			setupMocks: func(m envDescriberMocks) {
+				gomock.InOrder(
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, rgTags).
+						Return(nil, mockError),
+				)
+			},
+			wantedError: fmt.Errorf("get resources for env testEnv: some error"),
+		},
+		"error if getStackName fails because cannot parse resourceARN": {
+			setupMocks: func(m envDescriberMocks) {
+				gomock.InOrder(
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, rgTags).Return([]string{
+						badARN,
+					}, nil),
+				)
+			},
+			wantedError: fmt.Errorf("get stack name from arn arn:aws:cloudformation:us-west-2:123456789012:stacktestProject-testEnv-testApp16d75d1g0-8b1a-11ea-b358-06c1882c17fd: cannot parse ARN resource stacktestProject-testEnv-testApp16d75d1g0-8b1a-11ea-b358-06c1882c17fd"),
+		},
+		"success": {
+			setupMocks: func(m envDescriberMocks) {
+				gomock.InOrder(
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, rgTags).Return([]string{
+						testARN1,
+						testARN2,
+					}, nil),
+				)
+			},
+			wantedEnv: &EnvDescription{
+				Environment:  testEnv,
+				Applications: envApps,
+				Tags:         map[string]string{"key1": "value1", "key2": "value2"},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// GIVEN
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStore := mocks.NewMockstoreSvc(ctrl)
+			mockResourceGroupsClient := mocks.NewMockresourceGroupsClient(ctrl)
+			mocks := envDescriberMocks{
+				storeSvc:                 mockStore,
+				mockResourceGroupsClient: mockResourceGroupsClient,
+			}
+
+			tc.setupMocks(mocks)
+
+			d := &EnvDescriber{
+				env:  testEnv,
+				proj: testProject,
+				apps: allApps,
+
+				store:    mockStore,
+				rgClient: mockResourceGroupsClient,
+			}
+
+			// WHEN
+			actual, err := d.Describe()
+
+			// THEN
+			if tc.wantedError != nil {
+				require.EqualError(t, err, tc.wantedError.Error())
+			} else {
+				require.Nil(t, err)
+				require.Equal(t, tc.wantedEnv, actual)
+			}
+		})
+	}
+}
 
 func TestEnvDescriber_JSONString(t *testing.T) {
 	mockApplications := []*archer.Application{
