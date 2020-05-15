2598523325354e633d16f7575695dba8352e277d
David Killmon <killmond+gh@amazon.com>
2020-05-14 21:04:59 -0700

    chore(e2e): Update E2E tests to use new commands
    Update all the e2e tests to use the new app/service
    named commands.

--- e2e/init/init_test.go
+++ e2e/init/init_test.go
@@ -13,119 +13,119 @@ import (
 var _ = Describe("init flow", func() {
 
 	var (
-		appName string
+		svcName string
 		initErr error
 	)
 
 	BeforeAll(func() {
-		appName = "front-end"
+		svcName = "front-end"
 		_, initErr = cli.Init(&client.InitRequest{
-			ProjectName: projectName,
-			AppName:     appName,
-			ImageTag:    "gallopinggurdey",
-			Dockerfile:  "./front-end/Dockerfile",
-			AppType:     "Load Balanced Web App",
-			Deploy:      true,
-			AppPort:     "80",
+			AppName:    appName,
+			SvcName:    svcName,
+			ImageTag:   "gallopinggurdey",
+			Dockerfile: "./front-end/Dockerfile",
+			SvcType:    "Load Balanced Web Service",
+			Deploy:     true,
+			SvcPort:    "80",
 		})
 	})
 
-	Context("creating a brand new project, app and deploying to a test environment", func() {
+	Context("creating a brand new app, svc and deploying to a test environment", func() {
 		It("init does not return an error", func() {
 			Expect(initErr).NotTo(HaveOccurred())
 		})
 	})
 
-	Context("app ls", func() {
+	Context("svc ls", func() {
 		var (
-			appList      *client.AppListOutput
-			appListError error
+			svcList      *client.SvcListOutput
+			svcListError error
 		)
 
 		BeforeAll(func() {
-			appList, appListError = cli.AppList(projectName)
+			svcList, svcListError = cli.SvcList(appName)
 		})
 
 		It("should not return an error", func() {
-			Expect(appListError).NotTo(HaveOccurred())
+			Expect(svcListError).NotTo(HaveOccurred())
 		})
 
-		It("should return one app", func() {
-			Expect(len(appList.Apps)).To(Equal(1))
-			Expect(appList.Apps[0].AppName).To(Equal(appName))
-			Expect(appList.Apps[0].Project).To(Equal(projectName))
+		It("should return one service", func() {
+			Expect(len(svcList.Services)).To(Equal(1))
+			Expect(svcList.Services[0].Name).To(Equal(svcName))
+			Expect(svcList.Services[0].AppName).To(Equal(appName))
 		})
 	})
 
-	Context("app show", func() {
+	Context("svc show", func() {
 		var (
-			app        *client.AppShowOutput
-			appShowErr error
+			svc        *client.SvcShowOutput
+			svcShowErr error
 		)
 
 		BeforeAll(func() {
-			app, appShowErr = cli.AppShow(&client.AppShowRequest{
-				ProjectName: projectName,
-				AppName:     appName,
+			svc, svcShowErr = cli.SvcShow(&client.SvcShowRequest{
+				AppName: appName,
+				Name:    svcName,
 			})
 
 		})
 
 		It("should not return an error", func() {
-			Expect(appShowErr).NotTo(HaveOccurred())
+			Expect(svcShowErr).NotTo(HaveOccurred())
 		})
 
 		It("should return the correct configuration", func() {
-			Expect(app.AppName).To(Equal(appName))
-			Expect(app.Project).To(Equal(projectName))
+			Expect(svc.SvcName).To(Equal(svcName))
+			Expect(svc.AppName).To(Equal(appName))
 		})
 
 		It("should return a valid route", func() {
-			Expect(len(app.Routes)).To(Equal(1))
-			Expect(app.Routes[0].Environment).To(Equal("test"))
+			Expect(len(svc.Routes)).To(Equal(1))
+			Expect(svc.Routes[0].Environment).To(Equal("test"))
 			Eventually(func() (int, error) {
-				resp, fetchErr := http.Get(app.Routes[0].URL)
+				resp, fetchErr := http.Get(svc.Routes[0].URL)
 				return resp.StatusCode, fetchErr
 			}, "30s", "1s").Should(Equal(200))
 		})
 
 		It("should return a valid service discovery namespace", func() {
-			Expect(len(app.ServiceDiscoveries)).To(Equal(1))
-			Expect(app.ServiceDiscoveries[0].Environment).To(Equal([]string{"test"}))
-			Expect(app.ServiceDiscoveries[0].Namespace).To(Equal(fmt.Sprintf("%s.%s.local:80", appName, projectName)))
+			Expect(len(svc.ServiceDiscoveries)).To(Equal(1))
+			Expect(svc.ServiceDiscoveries[0].Environment).To(Equal([]string{"test"}))
+			Expect(svc.ServiceDiscoveries[0].Namespace).To(Equal(fmt.Sprintf("%s.%s.local:80", svcName, appName)))
 		})
 
 		It("should return the correct environment variables", func() {
-			Expect(len(app.Variables)).To(Equal(5))
+			Expect(len(svc.Variables)).To(Equal(5))
 			expectedVars := map[string]string{
-				"ECS_CLI_APP_NAME":           appName,
-				"COPILOT_ENVIRONMENT_NAME":   "test",
-				"COPILOT_LB_DNS":             strings.TrimPrefix(app.Routes[0].URL, "http://"),
-				"ECS_CLI_PROJECT_NAME":       projectName,
-				"ECS_APP_DISCOVERY_ENDPOINT": fmt.Sprintf("%s.local", projectName),
+				"COPILOT_APPLICATION_NAME":           appName,
+				"COPILOT_ENVIRONMENT_NAME":           "test",
+				"COPILOT_LB_DNS":                     strings.TrimPrefix(svc.Routes[0].URL, "http://"),
+				"COPILOT_SERVICE_NAME":               svcName,
+				"COPILOT_SERVICE_DISCOVERY_ENDPOINT": fmt.Sprintf("%s.local", appName),
 			}
-			for _, variable := range app.Variables {
+			for _, variable := range svc.Variables {
 				Expect(variable.Value).To(Equal(expectedVars[variable.Name]))
 			}
 		})
 	})
 
-	Context("app logs", func() {
+	Context("svc logs", func() {
 
 		It("should return valid log lines", func() {
-			var appLogs []client.AppLogsOutput
-			var appLogsErr error
-			Eventually(func() ([]client.AppLogsOutput, error) {
-				appLogs, appLogsErr = cli.AppLogs(&client.AppLogsRequest{
-					ProjectName: projectName,
-					AppName:     appName,
-					EnvName:     "test",
-					Since:       "1h",
+			var svcLogs []client.SvcLogsOutput
+			var svcLogsErr error
+			Eventually(func() ([]client.SvcLogsOutput, error) {
+				svcLogs, svcLogsErr = cli.SvcLogs(&client.SvcLogsRequest{
+					AppName: appName,
+					Name:    svcName,
+					EnvName: "test",
+					Since:   "1h",
 				})
-				return appLogs, appLogsErr
+				return svcLogs, svcLogsErr
 			}, "60s", "10s").ShouldNot(BeEmpty())
 
-			for _, logLine := range appLogs {
+			for _, logLine := range svcLogs {
 				Expect(logLine.Message).NotTo(Equal(""))
 				Expect(logLine.TaskID).NotTo(Equal(""))
 				Expect(logLine.Timestamp).NotTo(Equal(0))
