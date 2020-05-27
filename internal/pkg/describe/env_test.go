17d374378066605c192846937217a271cbd8f0d0
Janice Huang <60631893+huanjani@users.noreply.github.com>
2020-05-26 17:31:08 -0700

    chore: add resources and tags to env show subcommand (#960)
    * chore: add resources and tags and accompanying tests
    
    * delete extraneous files
    
    * fix: add tests and format
    
    * chore: add underlines to category headings and make headings flush with content (long tags)
    
    * fix: adjust tests to reflect addition of underlines
    
    * fix: format wantedContent for human string for readability
    
    * fix: unwrap unexported error msg
    
    * fix: change to catch all errors from StackResources method in envOutputs(), including IsStackNotExistsErr
    
    * fix: exclude addition of tags from app struct bc automatically included in cfn stack
    
    * fix: take manual tags out of tests
    
    * refactor: add separate constructor for outputResources
    
    * fix: remove parameter from initDescriber function, fix if statement logic
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- internal/pkg/describe/env_test.go
+++ internal/pkg/describe/env_test.go
@@ -6,6 +6,8 @@ package describe
 import (
 	"errors"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"testing"
 
 	"github.com/aws/amazon-ecs-cli-v2/internal/pkg/config"
@@ -18,6 +20,18 @@ import (
 type envDescriberMocks struct {
 	storeSvc                 *mocks.MockstoreSvc
 	mockResourceGroupsClient *mocks.MockresourceGroupsClient
+	mockStackDescriber       *mocks.MockstackAndResourcesDescriber
+}
+
+var wantedResources = []*CfnResource{
+	{
+		Type:       "AWS::IAM::Role",
+		PhysicalID: "testApp-testEnv-CFNExecutionRole",
+	},
+	{
+		Type:       "testApp-testEnv-Cluster",
+		PhysicalID: "AWS::ECS::Cluster-jI63pYBWU6BZ",
+	},
 }
 
 func TestEnvDescriber_Describe(t *testing.T) {
@@ -29,7 +43,6 @@ func TestEnvDescriber_Describe(t *testing.T) {
 	)
 	testApp := &config.Application{
 		Name: "testApp",
-		Tags: map[string]string{"key1": "value1", "key2": "value2"},
 	}
 	testEnv := &config.Environment{
 		App:              "testApp",
@@ -56,12 +69,31 @@ func TestEnvDescriber_Describe(t *testing.T) {
 		Name: "testSvc3",
 		Type: "load-balanced",
 	}
-
+	mockResource1 := &cloudformation.StackResource{
+		PhysicalResourceId: aws.String("testApp-testEnv-CFNExecutionRole"),
+		ResourceType:       aws.String("AWS::IAM::Role"),
+	}
+	mockResource2 := &cloudformation.StackResource{
+		PhysicalResourceId: aws.String("AWS::ECS::Cluster-jI63pYBWU6BZ"),
+		ResourceType:       aws.String("testApp-testEnv-Cluster"),
+	}
 	allSvcs := []*config.Service{testSvc1, testSvc2, testSvc3}
 	envSvcs := []*config.Service{testSvc1, testSvc2}
 	rgTags := map[string]string{stack.EnvTagKey: "testEnv"}
+	stackTags := []*cloudformation.Tag{
+		{
+			Key:   aws.String("copilot-application"),
+			Value: aws.String("testApp"),
+		},
+		{
+			Key:   aws.String("copilot-environment"),
+			Value: aws.String("testEnv"),
+		},
+	}
 	mockError := errors.New("some error")
 	testCases := map[string]struct {
+		shouldOutputResources bool
+
 		setupMocks func(mocks envDescriberMocks)
 
 		wantedEnv   *EnvDescription
@@ -75,7 +107,6 @@ func TestEnvDescriber_Describe(t *testing.T) {
 						Return(nil, mockError),
 				)
 			},
-
 			wantedError: fmt.Errorf("get AWS::CloudFormation::Stack resources for env testEnv: some error"),
 		},
 		"error if getStackName fails because can't parse resource ARN": {
@@ -98,19 +129,47 @@ func TestEnvDescriber_Describe(t *testing.T) {
 			},
 			wantedError: fmt.Errorf("invalid ARN resource format stacktestApp-testEnv-testSvc16d75d1g0-8b1a-11ea-b358-06c1882c17fd. Ex: arn:partition:service:region:account-id:resource-type/resource-id"),
 		},
-		"success": {
+		"success without resources": {
+			shouldOutputResources: false,
 			setupMocks: func(m envDescriberMocks) {
 				gomock.InOrder(
 					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, rgTags).Return([]string{
 						testARN1,
 						testARN2,
 					}, nil),
+					m.mockStackDescriber.EXPECT().Stack(stack.NameForEnv(testApp.Name, testEnv.Name)).Return(&cloudformation.Stack{
+						Tags: stackTags,
+					}, nil),
 				)
 			},
 			wantedEnv: &EnvDescription{
 				Environment: testEnv,
 				Services:    envSvcs,
-				Tags:        map[string]string{"key1": "value1", "key2": "value2"},
+				Tags:        map[string]string{"copilot-application": "testApp", "copilot-environment": "testEnv"},
+			},
+		},
+		"success with resources": {
+			shouldOutputResources: true,
+			setupMocks: func(m envDescriberMocks) {
+				gomock.InOrder(
+					m.mockResourceGroupsClient.EXPECT().GetResourcesByTags(cloudformationResourceType, rgTags).Return([]string{
+						testARN1,
+						testARN2,
+					}, nil),
+					m.mockStackDescriber.EXPECT().Stack(stack.NameForEnv(testApp.Name, testEnv.Name)).Return(&cloudformation.Stack{
+						Tags: stackTags,
+					}, nil),
+					m.mockStackDescriber.EXPECT().StackResources(stack.NameForEnv(testApp.Name, testEnv.Name)).Return([]*cloudformation.StackResource{
+						mockResource1,
+						mockResource2,
+					}, nil),
+				)
+			},
+			wantedEnv: &EnvDescription{
+				Environment: testEnv,
+				Services:    envSvcs,
+				Tags:        map[string]string{"copilot-application": "testApp", "copilot-environment": "testEnv"},
+				Resources:   wantedResources,
 			},
 		},
 	}
@@ -123,20 +182,24 @@ func TestEnvDescriber_Describe(t *testing.T) {
 
 			mockStore := mocks.NewMockstoreSvc(ctrl)
 			mockResourceGroupsClient := mocks.NewMockresourceGroupsClient(ctrl)
+			mockStackDescriber := mocks.NewMockstackAndResourcesDescriber(ctrl)
 			mocks := envDescriberMocks{
 				storeSvc:                 mockStore,
 				mockResourceGroupsClient: mockResourceGroupsClient,
+				mockStackDescriber:       mockStackDescriber,
 			}
 
 			tc.setupMocks(mocks)
 
 			d := &EnvDescriber{
-				env:  testEnv,
-				app:  testApp,
-				svcs: allSvcs,
+				env:             testEnv,
+				app:             testApp,
+				svcs:            allSvcs,
+				enableResources: tc.shouldOutputResources,
 
-				store:    mockStore,
-				rgClient: mockResourceGroupsClient,
+				store:          mockStore,
+				rgClient:       mockResourceGroupsClient,
+				stackDescriber: mockStackDescriber,
 			}
 
 			// WHEN
@@ -184,7 +247,7 @@ func TestEnvDescription_JSONString(t *testing.T) {
 		Type: "load-balanced",
 	}
 	allSvcs := []*config.Service{testSvc1, testSvc2, testSvc3}
-	wantedContent := "{\"environment\":{\"app\":\"testApp\",\"name\":\"testEnv\",\"region\":\"us-west-2\",\"accountID\":\"123456789012\",\"prod\":false,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"},\"services\":[{\"app\":\"testApp\",\"name\":\"testSvc1\",\"type\":\"load-balanced\"},{\"app\":\"testApp\",\"name\":\"testSvc2\",\"type\":\"load-balanced\"},{\"app\":\"testApp\",\"name\":\"testSvc3\",\"type\":\"load-balanced\"}],\"tags\":{\"key1\":\"value1\",\"key2\":\"value2\"}}\n"
+	wantedContent := "{\"environment\":{\"app\":\"testApp\",\"name\":\"testEnv\",\"region\":\"us-west-2\",\"accountID\":\"123456789012\",\"prod\":false,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"},\"services\":[{\"app\":\"testApp\",\"name\":\"testSvc1\",\"type\":\"load-balanced\"},{\"app\":\"testApp\",\"name\":\"testSvc2\",\"type\":\"load-balanced\"},{\"app\":\"testApp\",\"name\":\"testSvc3\",\"type\":\"load-balanced\"}],\"tags\":{\"key1\":\"value1\",\"key2\":\"value2\"},\"resources\":[{\"type\":\"AWS::IAM::Role\",\"physicalID\":\"testApp-testEnv-CFNExecutionRole\"},{\"type\":\"testApp-testEnv-Cluster\",\"physicalID\":\"AWS::ECS::Cluster-jI63pYBWU6BZ\"}]}\n"
 
 	// GIVEN
 	ctrl := gomock.NewController(t)
@@ -194,6 +257,7 @@ func TestEnvDescription_JSONString(t *testing.T) {
 		Environment: testEnv,
 		Services:    allSvcs,
 		Tags:        testApp.Tags,
+		Resources:   wantedResources,
 	}
 
 	// WHEN
@@ -245,6 +309,7 @@ func TestEnvDescription_HumanString(t *testing.T) {
 Services
 
   Name              Type
+  ----              ----
   testSvc1          load-balanced
   testSvc2          load-balanced
   testSvc3          load-balanced
@@ -252,8 +317,14 @@ Services
 Tags
 
   Key               Value
+  ---               -----
   key1              value1
   key2              value2
+
+Resources
+
+  AWS::IAM::Role           testApp-testEnv-CFNExecutionRole
+  testApp-testEnv-Cluster  AWS::ECS::Cluster-jI63pYBWU6BZ
 `
 	// GIVEN
 	ctrl := gomock.NewController(t)
@@ -263,6 +334,7 @@ Tags
 		Environment: testEnv,
 		Services:    allSvcs,
 		Tags:        testApp.Tags,
+		Resources:   wantedResources,
 	}
 
 	// WHEN
