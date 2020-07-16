e02d3439dc5940daaf2d68041cb762a4ba83234d
Wanxian Yang <wanxiay@amazon.com>
2020-07-16 10:49:45 -0500

    chore: implement run task in copilot environment

---
+++ internal/pkg/task/env_runner.go
@@ -0,0 +1,121 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package task
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/copilot-cli/internal/pkg/aws/ec2"
+	"github.com/aws/copilot-cli/internal/pkg/aws/ecs"
+	"github.com/aws/copilot-cli/internal/pkg/deploy"
+)
+
+const (
+	clusterResourceType  = "ecs:cluster"
+	fmtErrNoClusterFound = "no cluster found in env %s"
+)
+
+// EnvRunner can run an Amazon ECS task in the VPC and the cluster of an environment.
+type EnvRunner struct {
+	// Count of the tasks to be launched.
+	Count int
+	// Group Name of the tasks that use the same task definition.
+	GroupName string
+
+	// App and Env in which the tasks will be launched.
+	App string
+	Env string
+
+	// Interfaces to interact with dependencies. Must not be nil.
+	VPCGetter     VPCGetter
+	ClusterGetter ResourceGetter
+	Starter       TaskRunner
+}
+
+// Run runs tasks in the environment of the application, and returns the task ARNs.
+func (r *EnvRunner) Run() ([]string, error) {
+	if err := r.validateDependencies(); err != nil {
+		return nil, err
+	}
+
+	cluster, err := r.cluster(r.App, r.Env)
+	if err != nil {
+		return nil, err
+	}
+
+	filters := r.filtersForVPCFromAppEnv()
+
+	subnets, err := r.VPCGetter.PublicSubnetIDs(filters...)
+	if err != nil {
+		return nil, fmt.Errorf("get subnet IDs from %s: %w", r.Env, err)
+	}
+	if len(subnets) == 0 {
+		return nil, errNoSubnetFound
+	}
+
+	securityGroups, err := r.VPCGetter.SecurityGroups(filters...)
+	if err != nil {
+		return nil, fmt.Errorf("get security groups from %s: %w", r.Env, err)
+	}
+
+	arns, err := r.Starter.RunTask(ecs.RunTaskInput{
+		Cluster:        cluster,
+		Count:          r.Count,
+		Subnets:        subnets,
+		SecurityGroups: securityGroups,
+		TaskFamilyName: taskFamilyName(r.GroupName),
+		StartedBy:      startedBy,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("run task %s: %w", r.GroupName, err)
+	}
+	return arns, nil
+}
+
+func (r *EnvRunner) cluster(app, env string) (string, error) {
+	clusters, err := r.ClusterGetter.GetResourcesByTags(clusterResourceType, map[string]string{
+		deploy.AppTagKey: app,
+		deploy.EnvTagKey: env,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("get cluster by env %s: %w", env, err)
+	}
+
+	if len(clusters) == 0 {
+		return "", fmt.Errorf(fmtErrNoClusterFound, env)
+	}
+
+	// NOTE: only one cluster is associated with an application and an environment
+	return clusters[0].ARN, nil
+}
+
+func (r *EnvRunner) filtersForVPCFromAppEnv() []ec2.Filter {
+	return []ec2.Filter{
+		ec2.Filter{
+			Name:   ec2.TagFilterNameForEnv,
+			Values: []string{r.Env},
+		},
+		ec2.Filter{
+			Name:   ec2.TagFilterNameForApp,
+			Values: []string{r.App},
+		},
+	}
+}
+
+func (r *EnvRunner) validateDependencies() error {
+	if r.VPCGetter == nil {
+		return errors.New("vpc getter is not set")
+	}
+
+	if r.ClusterGetter == nil {
+		return errors.New("cluster getter is not set")
+	}
+
+	if r.Starter == nil {
+		return errors.New("starter is not set")
+	}
+
+	return nil
+}
---
+++ internal/pkg/task/env_runner_test.go
@@ -0,0 +1,188 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package task
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/copilot-cli/internal/pkg/aws/ec2"
+	"github.com/aws/copilot-cli/internal/pkg/aws/ecs"
+	"github.com/aws/copilot-cli/internal/pkg/aws/resourcegroups"
+	"github.com/aws/copilot-cli/internal/pkg/deploy"
+	"github.com/aws/copilot-cli/internal/pkg/task/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestEnvRunner_Run(t *testing.T) {
+	inApp := "my-app"
+	inEnv := "my-env"
+
+	resourceTagFiltersForCluster := map[string]string{
+		deploy.AppTagKey: inApp,
+		deploy.EnvTagKey: inEnv,
+	}
+	filtersForVPCFromAppEnv := []ec2.Filter{
+		ec2.Filter{
+			Name:   ec2.TagFilterNameForEnv,
+			Values: []string{inEnv},
+		},
+		ec2.Filter{
+			Name:   ec2.TagFilterNameForApp,
+			Values: []string{inApp},
+		},
+	}
+
+	mockResourceGetterWithCluster := func(m *mocks.MockResourceGetter) {
+		m.EXPECT().GetResourcesByTags(clusterResourceType, resourceTagFiltersForCluster).Return([]*resourcegroups.Resource{
+			&resourcegroups.Resource{ARN: "cluster-1"},
+		}, nil)
+	}
+	mockVPCGetterAny := func(m *mocks.MockVPCGetter) {
+		m.EXPECT().SubnetIDs(gomock.Any()).AnyTimes()
+		m.EXPECT().SecurityGroups(gomock.Any()).AnyTimes()
+	}
+	mockStarterNotRun := func(m *mocks.MockTaskRunner) {
+		m.EXPECT().RunTask(gomock.Any()).Times(0)
+	}
+
+	testCases := map[string]struct {
+		count     int
+		groupName string
+
+		mockVPCGetter      func(m *mocks.MockVPCGetter)
+		mockResourceGetter func(m *mocks.MockResourceGetter)
+		mockStarter        func(m *mocks.MockTaskRunner)
+
+		wantedError error
+		wantedARNs  []string
+	}{
+		"failed to get cluster": {
+			mockResourceGetter: func(m *mocks.MockResourceGetter) {
+				m.EXPECT().GetResourcesByTags(clusterResourceType, resourceTagFiltersForCluster).
+					Return(nil, errors.New("error getting resources"))
+			},
+			mockVPCGetter: mockVPCGetterAny,
+			mockStarter:   mockStarterNotRun,
+			wantedError:   fmt.Errorf("get cluster by env %s: error getting resources", inEnv),
+		},
+		"no cluster found": {
+			mockResourceGetter: func(m *mocks.MockResourceGetter) {
+				m.EXPECT().GetResourcesByTags(clusterResourceType, resourceTagFiltersForCluster).
+					Return([]*resourcegroups.Resource{}, nil)
+			},
+			mockVPCGetter: mockVPCGetterAny,
+			mockStarter:   mockStarterNotRun,
+			wantedError:   fmt.Errorf("no cluster found in env %s", inEnv),
+		},
+		"failed to get subnets": {
+			mockResourceGetter: mockResourceGetterWithCluster,
+			mockVPCGetter: func(m *mocks.MockVPCGetter) {
+				m.EXPECT().PublicSubnetIDs(filtersForVPCFromAppEnv).
+					Return(nil, errors.New("error getting subnets"))
+				m.EXPECT().SecurityGroups(gomock.Any()).AnyTimes()
+			},
+			mockStarter: mockStarterNotRun,
+			wantedError: fmt.Errorf("get subnet IDs from %s: error getting subnets", inEnv),
+		},
+		"no subnet is found": {
+			mockResourceGetter: mockResourceGetterWithCluster,
+			mockVPCGetter: func(m *mocks.MockVPCGetter) {
+				m.EXPECT().PublicSubnetIDs(filtersForVPCFromAppEnv).
+					Return([]string{}, nil)
+				m.EXPECT().SecurityGroups(gomock.Any()).AnyTimes()
+			},
+			mockStarter: mockStarterNotRun,
+			wantedError: errNoSubnetFound,
+		},
+		"failed to get security groups": {
+			mockResourceGetter: mockResourceGetterWithCluster,
+			mockVPCGetter: func(m *mocks.MockVPCGetter) {
+				m.EXPECT().PublicSubnetIDs(gomock.Any()).Return([]string{"subnet-1"}, nil)
+				m.EXPECT().SecurityGroups(filtersForVPCFromAppEnv).
+					Return(nil, errors.New("error getting security groups"))
+			},
+			mockStarter: mockStarterNotRun,
+			wantedError: fmt.Errorf("get security groups from %s: error getting security groups", inEnv),
+		},
+		"failed to kick off task": {
+			count:     1,
+			groupName: "my-task",
+
+			mockResourceGetter: mockResourceGetterWithCluster,
+			mockVPCGetter: func(m *mocks.MockVPCGetter) {
+				m.EXPECT().PublicSubnetIDs(filtersForVPCFromAppEnv).Return([]string{"subnet-1", "subnet-2"}, nil)
+				m.EXPECT().SecurityGroups(filtersForVPCFromAppEnv).Return([]string{"sg-1", "sg-2"}, nil)
+			},
+			mockStarter: func(m *mocks.MockTaskRunner) {
+				m.EXPECT().RunTask(ecs.RunTaskInput{
+					Cluster:        "cluster-1",
+					Count:          1,
+					Subnets:        []string{"subnet-1", "subnet-2"},
+					SecurityGroups: []string{"sg-1", "sg-2"},
+					TaskFamilyName: taskFamilyName("my-task"),
+					StartedBy:      startedBy,
+				}).Return(nil, errors.New("error running task"))
+			},
+
+			wantedError: fmt.Errorf("run task %s: error running task", "my-task"),
+		},
+		"run in env success": {
+			count:     1,
+			groupName: "my-task",
+
+			mockResourceGetter: mockResourceGetterWithCluster,
+			mockVPCGetter: func(m *mocks.MockVPCGetter) {
+				m.EXPECT().PublicSubnetIDs(filtersForVPCFromAppEnv).Return([]string{"subnet-1", "subnet-2"}, nil)
+				m.EXPECT().SecurityGroups(filtersForVPCFromAppEnv).Return([]string{"sg-1", "sg-2"}, nil)
+			},
+			mockStarter: func(m *mocks.MockTaskRunner) {
+				m.EXPECT().RunTask(ecs.RunTaskInput{
+					Cluster:        "cluster-1",
+					Count:          1,
+					Subnets:        []string{"subnet-1", "subnet-2"},
+					SecurityGroups: []string{"sg-1", "sg-2"},
+					TaskFamilyName: taskFamilyName("my-task"),
+					StartedBy:      startedBy,
+				}).Return([]string{"task-1"}, nil)
+			},
+			wantedARNs: []string{"task-1"},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockVPCGetter := mocks.NewMockVPCGetter(ctrl)
+			mockResourceGetter := mocks.NewMockResourceGetter(ctrl)
+			mockStarter := mocks.NewMockTaskRunner(ctrl)
+
+			tc.mockVPCGetter(mockVPCGetter)
+			tc.mockResourceGetter(mockResourceGetter)
+			tc.mockStarter(mockStarter)
+
+			task := &EnvRunner{
+				Count:     tc.count,
+				GroupName: tc.groupName,
+
+				App: inApp,
+				Env: inEnv,
+
+				VPCGetter:     mockVPCGetter,
+				ClusterGetter: mockResourceGetter,
+				Starter:       mockStarter,
+			}
+
+			arns, err := task.Run()
+			if tc.wantedError != nil {
+				require.EqualError(t, tc.wantedError, err.Error())
+			} else {
+				require.Nil(t, err)
+				require.Equal(t, tc.wantedARNs, arns)
+			}
+		})
+	}
+}
