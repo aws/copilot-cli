5f61174c563b5f32496544fd234fa3b9efb0bdf4
louise15926 <wanxiay@amazon.com>
2020-07-20 16:01:36 -0500

    chore(repository): add repository package to support building and pushing images (#1161)
    This PR adds a repository package that builds and push images to a repository.
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

---
+++ internal/pkg/repository/repository_test.go
@@ -0,0 +1,118 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package repository
+
+import (
+	"errors"
+	"fmt"
+	"github.com/aws/copilot-cli/internal/pkg/repository/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestRepository_BuildAndPush(t *testing.T) {
+	inRepoName := "my-repo"
+	inDockerfilePath := "path/to/dockerfile"
+
+	mockTag1, mockTag2, mockTag3 := "tag1", "tag2", "tag3"
+	mockRepoURI := "mockURI"
+
+	testCases := map[string]struct {
+		inRepoName string
+		inDockerfilePath string
+		inMockDocker func(m *mocks.MockContainerLoginBuildPusher)
+
+		mockRegistry func(m *mocks.MockRegistry)
+
+		wantedError error
+		wantedURI string
+	}{
+		"failed to get auth": {
+			mockRegistry: func(m *mocks.MockRegistry) {
+				m.EXPECT().Auth().Return("", "", errors.New("error getting auth"))
+			},
+			inMockDocker: func(m *mocks.MockContainerLoginBuildPusher) {
+				m.EXPECT().Build(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+				m.EXPECT().Login(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+				m.EXPECT().Push(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			wantedError: errors.New("get auth: error getting auth"),
+		},
+		"failed to build image": {
+			mockRegistry: func(m *mocks.MockRegistry) {
+				m.EXPECT().Auth().Return("", "", nil).AnyTimes()
+			},
+			inMockDocker: func(m *mocks.MockContainerLoginBuildPusher) {
+				m.EXPECT().Build(mockRepoURI, inDockerfilePath, mockTag1, mockTag2, mockTag3).Return(errors.New("error building image"))
+				m.EXPECT().Login(gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+				m.EXPECT().Push(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			wantedError: fmt.Errorf("build Dockerfile at %s: error building image", inDockerfilePath),
+		},
+		"failed to login": {
+			mockRegistry: func(m *mocks.MockRegistry) {
+				m.EXPECT().Auth().Return("my-name", "my-pwd", nil)
+			},
+			inMockDocker: func(m *mocks.MockContainerLoginBuildPusher) {
+				m.EXPECT().Build(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).AnyTimes()
+				m.EXPECT().Login(mockRepoURI, "my-name", "my-pwd").Return(errors.New("error logging in"))
+				m.EXPECT().Push(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			wantedError: fmt.Errorf("login to repo %s: error logging in", inRepoName),
+		},
+		"failed to push": {
+			mockRegistry: func(m *mocks.MockRegistry) {
+				m.EXPECT().Auth().Times(1)
+			},
+			inMockDocker: func(m *mocks.MockContainerLoginBuildPusher) {
+				m.EXPECT().Build(mockRepoURI, inDockerfilePath, mockTag1, mockTag2, mockTag3).Times(1)
+				m.EXPECT().Login(gomock.Any(), gomock.Any(), gomock.Any()).Times(1)
+				m.EXPECT().Push(mockRepoURI, mockTag1, mockTag2, mockTag3).Return(errors.New("error pushing image"))
+			},
+			wantedError: errors.New("push to repo my-repo: error pushing image"),
+		},
+		"success": {
+			mockRegistry: func(m *mocks.MockRegistry) {
+				m.EXPECT().Auth().Return("my-name", "my-pwd", nil).Times(1)
+			},
+			inMockDocker: func(m *mocks.MockContainerLoginBuildPusher) {
+				m.EXPECT().Build(mockRepoURI, inDockerfilePath, mockTag1, mockTag2, mockTag3).Return(nil).Times(1)
+				m.EXPECT().Login(mockRepoURI, "my-name", "my-pwd").Return(nil).Times(1)
+				m.EXPECT().Push(mockRepoURI, mockTag1, mockTag2, mockTag3).Return(nil)
+			},
+			wantedURI: mockRepoURI,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockRepoGetter := mocks.NewMockRegistry(ctrl)
+			mockDocker := mocks.NewMockContainerLoginBuildPusher(ctrl)
+
+			if tc.mockRegistry != nil {
+				tc.mockRegistry(mockRepoGetter)
+			}
+			if tc.inMockDocker != nil {
+				tc.inMockDocker(mockDocker)
+			}
+
+			repo := &Repository{
+				name:     inRepoName,
+				registry: mockRepoGetter,
+
+				uri: mockRepoURI,
+			}
+
+			err := repo.BuildAndPush(mockDocker, inDockerfilePath, mockTag1, []string{mockTag2, mockTag3}...)
+			if tc.wantedError != nil {
+				require.EqualError(t, tc.wantedError, err.Error())
+			} else {
+				require.Nil(t, err)
+			}
+		})
+	}
+}
