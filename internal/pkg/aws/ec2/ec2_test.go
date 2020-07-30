5d5f75c95779a0e16282d2556118cb66d9e5edde
penghaoh <penghaoh@amazon.com>
2020-07-30 14:51:49 -0700

    chore(ec2): add ListVpcSubnets method and pagination

--- internal/pkg/aws/ec2/ec2_test.go
+++ internal/pkg/aws/ec2/ec2_test.go
@@ -6,22 +6,23 @@ package ec2
 import (
 	"errors"
 	"fmt"
+	"testing"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/copilot-cli/internal/pkg/aws/ec2/mocks"
 	"github.com/aws/copilot-cli/internal/pkg/deploy"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
-	"testing"
 )
 
 var (
 	inAppEnvFilters = []Filter{
-		Filter{
-			Name: fmt.Sprintf(TagFilterName, deploy.AppTagKey),
+		{
+			Name:   fmt.Sprintf(TagFilterName, deploy.AppTagKey),
 			Values: []string{"my-app"},
 		},
-		Filter{
+		{
 			Name:   fmt.Sprintf(TagFilterName, deploy.EnvTagKey),
 			Values: []string{"my-env"},
 		},
@@ -41,6 +42,64 @@ var (
 	}
 )
 
+func TestEC2_ListVpcSubnets(t *testing.T) {
+	const mockVpcID = "mockVpcID"
+	testCases := map[string]struct {
+		mockEC2Client func(m *mocks.Mockapi)
+
+		wantedError   error
+		wantedSubnets *Subnets
+	}{
+		"fail to describe subnets": {
+			mockEC2Client: func(m *mocks.Mockapi) {
+				m.EXPECT().DescribeSubnets(gomock.Any()).Return(nil, errors.New("error describing subnets"))
+			},
+			wantedError: fmt.Errorf("describe subnets: error describing subnets"),
+		},
+		"success": {
+			mockEC2Client: func(m *mocks.Mockapi) {
+				m.EXPECT().DescribeSubnets(&ec2.DescribeSubnetsInput{
+					Filters: toEC2Filter([]Filter{
+						{
+							Name:   "vpc-id",
+							Values: []string{mockVpcID},
+						},
+					}),
+				}).Return(&ec2.DescribeSubnetsOutput{
+					Subnets: []*ec2.Subnet{
+						subnet1,
+						subnet2,
+						subnet3,
+					}}, nil)
+			},
+			wantedSubnets: &Subnets{
+				PublicSubnets:  []string{"subnet-2", "subnet-3"},
+				PrivateSubnets: []string{"subnet-1"},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+
+			mockAPI := mocks.NewMockapi(ctrl)
+			tc.mockEC2Client(mockAPI)
+
+			ec2Client := EC2{
+				client: mockAPI,
+			}
+
+			subnets, err := ec2Client.ListVpcSubnets(mockVpcID)
+			if tc.wantedError != nil {
+				require.EqualError(t, tc.wantedError, err.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.wantedSubnets, subnets)
+			}
+		})
+	}
+}
 func TestEC2_PublicSubnetIDs(t *testing.T) {
 	testCases := map[string]struct {
 		inFilter []Filter
@@ -98,6 +157,7 @@ func TestEC2_PublicSubnetIDs(t *testing.T) {
 }
 
 func TestEC2_SubnetIDs(t *testing.T) {
+	mockNextToken := aws.String("mockNextToken")
 	testCases := map[string]struct {
 		inFilter []Filter
 
@@ -127,6 +187,27 @@ func TestEC2_SubnetIDs(t *testing.T) {
 				}, nil)
 			},
 			wantedARNs: []string{"subnet-1", "subnet-2"},
+		},
+		"successfully get subnets with pagination": {
+			inFilter: inAppEnvFilters,
+			mockEC2Client: func(m *mocks.Mockapi) {
+				m.EXPECT().DescribeSubnets(&ec2.DescribeSubnetsInput{
+					Filters: toEC2Filter(inAppEnvFilters),
+				}).Return(&ec2.DescribeSubnetsOutput{
+					Subnets: []*ec2.Subnet{
+						subnet1, subnet2,
+					},
+					NextToken: mockNextToken,
+				}, nil)
+				m.EXPECT().DescribeSubnets(&ec2.DescribeSubnetsInput{
+					Filters: toEC2Filter(inAppEnvFilters),
+				}).Return(&ec2.DescribeSubnetsOutput{
+					Subnets: []*ec2.Subnet{
+						subnet3,
+					},
+				}, nil)
+			},
+			wantedARNs: []string{"subnet-1", "subnet-2", "subnet-3"},
 		},
 	}
 
@@ -178,10 +259,10 @@ func TestEC2_SecurityGroups(t *testing.T) {
 					Filters: toEC2Filter(inAppEnvFilters),
 				}).Return(&ec2.DescribeSecurityGroupsOutput{
 					SecurityGroups: []*ec2.SecurityGroup{
-						&ec2.SecurityGroup{
+						{
 							GroupId: aws.String("sg-1"),
 						},
-						&ec2.SecurityGroup{
+						{
 							GroupId: aws.String("sg-2"),
 						},
 					},
