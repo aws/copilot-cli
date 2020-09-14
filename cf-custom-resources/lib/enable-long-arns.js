f06fcd8b72ac1a5d6c789c0cfa4e55d7777e64f4
penghaoh <penghaoh@amazon.com>
2020-09-14 14:11:59 -0700

    chore(custom-resources): prettify js files to have consistent format

--- cf-custom-resources/lib/enable-long-arns.js
+++ cf-custom-resources/lib/enable-long-arns.js
@@ -1,8 +1,8 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-'use strict';
+"use strict";
 
-const aws = require('aws-sdk');
+const aws = require("aws-sdk");
 
 /**
  * Upload a CloudFormation response object to S3.
@@ -15,47 +15,53 @@ const aws = require('aws-sdk');
  * @param {string} [reason] reason for failure, if any, to convey to the user
  * @returns {Promise} Promise that is resolved on success, or rejected on connection error or HTTP error response
  */
-const report = function (event, context, responseStatus, physicalResourceId, responseData, reason) {
-    return new Promise((resolve, reject) => {
-        const https = require('https');
-        const {
-            URL
-        } = require('url');
+const report = function (
+  event,
+  context,
+  responseStatus,
+  physicalResourceId,
+  responseData,
+  reason
+) {
+  return new Promise((resolve, reject) => {
+    const https = require("https");
+    const { URL } = require("url");
 
-        var responseBody = JSON.stringify({
-            Status: responseStatus,
-            Reason: reason,
-            PhysicalResourceId: physicalResourceId || context.logStreamName,
-            StackId: event.StackId,
-            RequestId: event.RequestId,
-            LogicalResourceId: event.LogicalResourceId,
-            Data: responseData
-        });
-
-        const parsedUrl = new URL(event.ResponseURL || defaultResponseURL);
-        const options = {
-            hostname: parsedUrl.hostname,
-            port: 443,
-            path: parsedUrl.pathname + parsedUrl.search,
-            method: 'PUT',
-            headers: {
-                'Content-Type': '',
-                'Content-Length': responseBody.length
-            }
-        };
-
-        https.request(options)
-            .on('error', reject)
-            .on('response', res => {
-                res.resume();
-                if (res.statusCode >= 400) {
-                    reject(new Error(`Error ${res.statusCode}: ${res.statusMessage}`));
-                } else {
-                    resolve();
-                }
-            })
-            .end(responseBody, 'utf8');
+    var responseBody = JSON.stringify({
+      Status: responseStatus,
+      Reason: reason,
+      PhysicalResourceId: physicalResourceId || context.logStreamName,
+      StackId: event.StackId,
+      RequestId: event.RequestId,
+      LogicalResourceId: event.LogicalResourceId,
+      Data: responseData,
     });
+
+    const parsedUrl = new URL(event.ResponseURL || defaultResponseURL);
+    const options = {
+      hostname: parsedUrl.hostname,
+      port: 443,
+      path: parsedUrl.pathname + parsedUrl.search,
+      method: "PUT",
+      headers: {
+        "Content-Type": "",
+        "Content-Length": responseBody.length,
+      },
+    };
+
+    https
+      .request(options)
+      .on("error", reject)
+      .on("response", (res) => {
+        res.resume();
+        if (res.statusCode >= 400) {
+          reject(new Error(`Error ${res.statusCode}: ${res.statusMessage}`));
+        } else {
+          resolve();
+        }
+      })
+      .end(responseBody, "utf8");
+  });
 };
 
 /**
@@ -64,46 +70,59 @@ const report = function (event, context, responseStatus, physicalResourceId, res
  *
  * By enabling the long ARN format, ECS services, tasks, and container instances become taggable.
  */
-const enableLongArnFormat = async function() {
-    const longFormats = ['serviceLongArnFormat', 'taskLongArnFormat', 'containerInstanceLongArnFormat'];
-    const ecs = new aws.ECS();
-    for (const fmt of longFormats) {
-        try{
-            await ecs.putAccountSetting({
-                name: fmt,
-                value: "enabled",
-            }).promise();
-        } catch (err) {
-            console.log(`enable ${fmt}: ${err}.`)
-            throw err;
-        }
+const enableLongArnFormat = async function () {
+  const longFormats = [
+    "serviceLongArnFormat",
+    "taskLongArnFormat",
+    "containerInstanceLongArnFormat",
+  ];
+  const ecs = new aws.ECS();
+  for (const fmt of longFormats) {
+    try {
+      await ecs
+        .putAccountSetting({
+          name: fmt,
+          value: "enabled",
+        })
+        .promise();
+    } catch (err) {
+      console.log(`enable ${fmt}: ${err}.`);
+      throw err;
     }
+  }
 };
 
 /**
  * Handler invoked by Lambda.
  */
-exports.handler = async function(event, context) {
-    var responseData = {};
-    var physicalResourceId;
+exports.handler = async function (event, context) {
+  var responseData = {};
+  var physicalResourceId;
 
-    try {
-        switch (event.RequestType) {
-            case 'Create':
-                await enableLongArnFormat();
-                physicalResourceId = event.PhysicalResourceId
-                break;
-            // Do nothing on update and delete, since this isn't a "real" resource.
-            case 'Update':
-            case 'Delete':
-                physicalResourceId = event.PhysicalResourceId
-                break;
-            default:
-                throw new Error(`Unsupported request type ${event.RequestType}`);
-        }
-        await report(event, context, 'SUCCESS', physicalResourceId, responseData);
-    } catch (err) {
-        console.log(`Caught error ${err}.`);
-        await report(event, context, 'FAILED', physicalResourceId, null, err.message);
+  try {
+    switch (event.RequestType) {
+      case "Create":
+        await enableLongArnFormat();
+        physicalResourceId = event.PhysicalResourceId;
+        break;
+      // Do nothing on update and delete, since this isn't a "real" resource.
+      case "Update":
+      case "Delete":
+        physicalResourceId = event.PhysicalResourceId;
+        break;
+      default:
+        throw new Error(`Unsupported request type ${event.RequestType}`);
     }
+    await report(event, context, "SUCCESS", physicalResourceId, responseData);
+  } catch (err) {
+    console.log(`Caught error ${err}.`);
+    await report(
+      event,
+      context,
+      "FAILED",
+      physicalResourceId,
+      null,
+      err.message
+    );
+  }
 };
