7d4ee9e0b85400779d2e02f3aaaf35e03ff52041
Penghao He <penghaoh@amazon.com>
2020-09-15 08:47:49 -0700

    chore(custom-resources): prettify js files to have consistent format (#1383)
    <!-- Provide summary of changes -->
    Reformat JavaScript files and json file in `cf-custom-resources` dir to make the format consistent across the library.
    <!-- Issue number, if available. E.g. "Fixes #31", "Addresses #42, 77" -->
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

--- cf-custom-resources/test/enable-long-arn-test.js
+++ cf-custom-resources/test/enable-long-arn-test.js
@@ -1,104 +1,116 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-'use strict';
+"use strict";
 
-const AWS = require('aws-sdk-mock');
-const sinon = require('sinon');
-const LongArnsLambda = require('../lib/enable-long-arns');
-const LambdaTester = require('lambda-tester').noVersionCheck();
-const nock = require('nock');
+const AWS = require("aws-sdk-mock");
+const sinon = require("sinon");
+const LongArnsLambda = require("../lib/enable-long-arns");
+const LambdaTester = require("lambda-tester").noVersionCheck();
+const nock = require("nock");
 
-describe('Enable Long ARN Handler', () => {
-    const responseURL = 'https://cloudwatch-response-mock.example.com/';
-    const testRequestId = 'f4ef1b10-c39a-44e3-99c0-fbf7e53c3943';
+describe("Enable Long ARN Handler", () => {
+  const responseURL = "https://cloudwatch-response-mock.example.com/";
+  const testRequestId = "f4ef1b10-c39a-44e3-99c0-fbf7e53c3943";
 
-    beforeEach(() => {
-        AWS.restore();
+  beforeEach(() => {
+    AWS.restore();
+  });
+
+  test("create operation", () => {
+    // GIVEN
+    const mockPutAccountSetting = sinon.stub();
+    mockPutAccountSetting.onFirstCall().resolves(null);
+    mockPutAccountSetting.onSecondCall().resolves(null);
+    mockPutAccountSetting.onThirdCall().resolves(null);
+
+    AWS.mock("ECS", "putAccountSetting", mockPutAccountSetting);
+    const request = nock(responseURL)
+      .put("/", (body) => {
+        return body.Status === "SUCCESS";
+      })
+      .reply(200);
+
+    // WHEN
+    const lambda = LambdaTester(LongArnsLambda.handler).event({
+      RequestType: "Create",
+      RequestId: testRequestId,
+      ResponseURL: responseURL,
     });
 
-    test('create operation', () => {
-        // GIVEN
-        const mockPutAccountSetting = sinon.stub();
-        mockPutAccountSetting.onFirstCall().resolves(null);
-        mockPutAccountSetting.onSecondCall().resolves(null);
-        mockPutAccountSetting.onThirdCall().resolves(null);
+    // THEN
+    lambda.expectResolve(() => {
+      sinon.assert.calledWith(
+        mockPutAccountSetting.firstCall,
+        sinon.match({
+          name: "serviceLongArnFormat",
+          value: "enabled",
+        })
+      );
+      sinon.assert.calledWith(
+        mockPutAccountSetting.secondCall,
+        sinon.match({
+          name: "taskLongArnFormat",
+          value: "enabled",
+        })
+      );
+      sinon.assert.calledWith(
+        mockPutAccountSetting.thirdCall,
+        sinon.match({
+          name: "containerInstanceLongArnFormat",
+          value: "enabled",
+        })
+      );
+      expect(request.isDone()).toBe(true);
+    });
+  });
 
-        AWS.mock('ECS', 'putAccountSetting', mockPutAccountSetting)
-        const request = nock(responseURL).put('/', body => {
-            return body.Status === 'SUCCESS';
-        }).reply(200);
+  test("update operation should do nothing", () => {
+    // GIVEN
+    const mockPutAccountSetting = sinon.stub();
 
-        // WHEN
-        const lambda = LambdaTester(LongArnsLambda.handler)
-            .event({
-                RequestType: 'Create',
-                RequestId: testRequestId,
-                ResponseURL: responseURL,
-            });
+    AWS.mock("ECS", "putAccountSetting", mockPutAccountSetting);
+    const request = nock(responseURL)
+      .put("/", (body) => {
+        return body.Status === "SUCCESS";
+      })
+      .reply(200);
 
-        // THEN
-        lambda.expectResolve(() => {
-            sinon.assert.calledWith(mockPutAccountSetting.firstCall, sinon.match({
-                name: 'serviceLongArnFormat',
-                value: 'enabled',
-            }));
-            sinon.assert.calledWith(mockPutAccountSetting.secondCall, sinon.match({
-                name: 'taskLongArnFormat',
-                value: 'enabled',
-            }));
-            sinon.assert.calledWith(mockPutAccountSetting.thirdCall, sinon.match({
-                name: 'containerInstanceLongArnFormat',
-                value: 'enabled',
-            }));
-            expect(request.isDone()).toBe(true);
-        });
+    // WHEN
+    const lambda = LambdaTester(LongArnsLambda.handler).event({
+      RequestType: "Update",
+      RequestId: testRequestId,
+      ResponseURL: responseURL,
     });
 
-    test('update operation should do nothing', () => {
-        // GIVEN
-        const mockPutAccountSetting = sinon.stub();
+    // THEN
+    lambda.expectResolve(() => {
+      sinon.assert.notCalled(mockPutAccountSetting);
+      expect(request.isDone()).toBe(true);
+    });
+  });
 
-        AWS.mock('ECS', 'putAccountSetting', mockPutAccountSetting)
-        const request = nock(responseURL).put('/', body => {
-            return body.Status === 'SUCCESS';
-        }).reply(200);
+  test("delete operation should do nothing", () => {
+    // GIVEN
+    const mockPutAccountSetting = sinon.stub();
 
-        // WHEN
-        const lambda = LambdaTester(LongArnsLambda.handler)
-            .event({
-                RequestType: 'Update',
-                RequestId: testRequestId,
-                ResponseURL: responseURL,
-            });
+    AWS.mock("ECS", "putAccountSetting", mockPutAccountSetting);
+    const request = nock(responseURL)
+      .put("/", (body) => {
+        return body.Status === "SUCCESS";
+      })
+      .reply(200);
 
-        // THEN
-        lambda.expectResolve(() => {
-            sinon.assert.notCalled(mockPutAccountSetting);
-            expect(request.isDone()).toBe(true);
-        });
+    // WHEN
+    const lambda = LambdaTester(LongArnsLambda.handler).event({
+      RequestType: "Delete",
+      RequestId: testRequestId,
+      ResponseURL: responseURL,
     });
 
-    test('delete operation should do nothing', () => {
-        // GIVEN
-        const mockPutAccountSetting = sinon.stub();
-
-        AWS.mock('ECS', 'putAccountSetting', mockPutAccountSetting)
-        const request = nock(responseURL).put('/', body => {
-            return body.Status === 'SUCCESS';
-        }).reply(200);
-
-        // WHEN
-        const lambda = LambdaTester(LongArnsLambda.handler)
-            .event({
-                RequestType: 'Delete',
-                RequestId: testRequestId,
-                ResponseURL: responseURL,
-            });
-
-        // THEN
-        lambda.expectResolve(() => {
-            sinon.assert.notCalled(mockPutAccountSetting);
-            expect(request.isDone()).toBe(true);
-        });
+    // THEN
+    lambda.expectResolve(() => {
+      sinon.assert.notCalled(mockPutAccountSetting);
+      expect(request.isDone()).toBe(true);
     });
+  });
 });
