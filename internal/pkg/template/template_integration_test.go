6b5c3e3a1dfd62eb839f950999031b6984dade5d
Austin Ely <austiely@amazon.com>
2020-09-29 14:01:53 -0700

    chore(e2e): resolve template parsing error which made e2es fail

--- internal/pkg/template/template_integration_test.go
+++ internal/pkg/template/template_integration_test.go
@@ -15,23 +15,60 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTemplate_ParseLoadBalancedWebService(t *testing.T) {
+func TestTemplate_ParseScheduledJob(t *testing.T) {
 	testCases := map[string]struct {
-		opts template.ServiceOpts
+		opts template.WorkloadOpts
 	}{
 		"renders a valid template by default": {
-			opts: template.ServiceOpts{},
+			opts: template.WorkloadOpts{},
+		},
+		"renders with options": {
+			opts: template.WorkloadOpts{
+				StateMachine: &template.StateMachineOpts{
+					Retries: aws.Int(5),
+					Timeout: aws.Int(3600),
+				},
+			},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// GIVEN
+			sess, err := sessions.NewProvider().Default()
+			require.NoError(t, err)
+			cfn := cloudformation.New(sess)
+			tpl := template.New()
+
+			// WHEN
+			content, err := tpl.ParseScheduledJob(tc.opts)
+			require.NoError(t, err)
+
+			// THEN
+			_, err = cfn.ValidateTemplate(&cloudformation.ValidateTemplateInput{
+				TemplateBody: aws.String(content.String()),
+			})
+			require.NoError(t, err, content.String())
+		})
+	}
+}
+
+func TestTemplate_ParseLoadBalancedWebService(t *testing.T) {
+	testCases := map[string]struct {
+		opts template.WorkloadOpts
+	}{
+		"renders a valid template by default": {
+			opts: template.WorkloadOpts{},
 		},
 		"renders a valid template with addons with no outputs": {
-			opts: template.ServiceOpts{
-				NestedStack: &template.ServiceNestedStackOpts{
+			opts: template.WorkloadOpts{
+				NestedStack: &template.WorkloadNestedStackOpts{
 					StackName: "AddonsStack",
 				},
 			},
 		},
 		"renders a valid template with addons with outputs": {
-			opts: template.ServiceOpts{
-				NestedStack: &template.ServiceNestedStackOpts{
+			opts: template.WorkloadOpts{
+				NestedStack: &template.WorkloadNestedStackOpts{
 					StackName:       "AddonsStack",
 					VariableOutputs: []string{"TableName"},
 					SecretOutputs:   []string{"TablePassword"},
