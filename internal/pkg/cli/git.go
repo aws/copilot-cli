49c0d31a42df2f1f5013d4093f892ff4a4519ffc
Janice <jwh@amazon.com>
2020-10-13 09:43:54 -0700

    fix: make askImageTag more streamlined

--- internal/pkg/cli/git.go
+++ internal/pkg/cli/git.go
@@ -31,16 +31,16 @@ func getVersionTag(runner runner) (string, error) {
 }
 
 func askImageTag(tag string, prompter prompter, cmd runner) (string, error) {
-	var err error
-	if tag == "" {
-		tag, err = getVersionTag(cmd)
-		log.Warningln("Failed to default tag, are you in a git repository?")
+	if tag != "" {
+		return tag, nil
+	}
+	tag, err := getVersionTag(cmd)
+	log.Warningln("Failed to default tag, are you in a git repository?")
+	if err != nil {
+		// User is not in a Git repository, so prompt for a tag.
+		tag, err = prompter.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
 		if err != nil {
-			// User is not in a Git repository, so prompt for a tag.
-			tag, err = prompter.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
-			if err != nil {
-				return "", fmt.Errorf("prompt get image tag: %w", err)
-			}
+			return "", fmt.Errorf("prompt get image tag: %w", err)
 		}
 	}
 	return tag, err
