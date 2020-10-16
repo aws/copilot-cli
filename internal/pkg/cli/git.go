aws/copilot-cli

commit 1785cd7432fa6ae10b3874b680786e05e28ffb93

chore(cli): add Validate and Ask methods to job_package (#1480)
Implements the `Validate()` and `Ask()` methods of the `job_package` subcommand. 

Related: #1131, #1428 

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/git.go
@@ -5,18 +5,43 @@ package cli
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 
 	"github.com/aws/copilot-cli/internal/pkg/term/command"
+
+	"github.com/aws/copilot-cli/internal/pkg/term/log"
+
+	"github.com/aws/copilot-cli/internal/pkg/term/prompt"
+)
+
+const (
+	inputImageTagPrompt = "Input an image tag value:"
 )
 
 func getVersionTag(runner runner) (string, error) {
-	var b bytes.Buffer
-
-	if err := runner.Run("git", []string{"describe", "--always"}, command.Stdout(&b)); err != nil {
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	if err := runner.Run("git", []string{"describe", "--always"}, command.Stdout(&stdout), command.Stderr(&stderr)); err != nil {
 		return "", err
 	}
 
 	// NOTE: `git describe` output bytes includes a `\n` character, so we trim it out.
-	return strings.TrimSpace(b.String()), nil
+	return strings.TrimSpace(stdout.String()), nil
+}
+
+func askImageTag(tag string, prompter prompter, cmd runner) (string, error) {
+	if tag != "" {
+		return tag, nil
+	}
+	tag, err := getVersionTag(cmd)
+	log.Warningln("Failed to default tag, are you in a git repository?")
+	if err != nil {
+		// User is not in a Git repository, so prompt for a tag.
+		tag, err = prompter.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
+		if err != nil {
+			return "", fmt.Errorf("prompt get image tag: %w", err)
+		}
+	}
+	return tag, err
 }

internal/pkg/cli/job.go
@@ -20,7 +20,7 @@ Jobs are Amazon ECS tasks which run on a fixed schedule.`,
 	}
 
 	cmd.AddCommand(buildJobInitCmd())
-	// cmd.AddCommand(BuildJobPackageCmd())
+	cmd.AddCommand(buildJobPackageCmd())
 	cmd.AddCommand(buildJobDeployCmd())
 	cmd.AddCommand(buildJobDeleteCmd())
 	cmd.AddCommand(buildJobListCmd())

internal/pkg/cli/job_deploy.go
@@ -114,9 +114,11 @@ func (o *deployJobOpts) Ask() error {
 	if err := o.askEnvName(); err != nil {
 		return err
 	}
-	if err := o.askImageTag(); err != nil {
+	tag, err := askImageTag(o.imageTag, o.prompt, o.cmd)
+	if err != nil {
 		return err
 	}
+	o.imageTag = tag
 	return nil
 }
 
@@ -383,29 +385,6 @@ func (o *deployJobOpts) askEnvName() error {
 		return fmt.Errorf("select environment: %w", err)
 	}
 	o.envName = name
-	return nil
-}
-
-func (o *deployJobOpts) askImageTag() error {
-	if o.imageTag != "" {
-		return nil
-	}
-
-	tag, err := getVersionTag(o.cmd)
-
-	if err == nil {
-		o.imageTag = tag
-
-		return nil
-	}
-
-	log.Warningln("Failed to default tag, are you in a git repository?")
-
-	userInputTag, err := o.prompt.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
-	if err != nil {
-		return fmt.Errorf("prompt for image tag: %w", err)
-	}
-	o.imageTag = userInputTag
 	return nil
 }
 

internal/pkg/cli/job_package.go
@@ -1,0 +1,168 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/aws/copilot-cli/internal/pkg/config"
+	"github.com/aws/copilot-cli/internal/pkg/term/command"
+	"github.com/aws/copilot-cli/internal/pkg/term/prompt"
+	"github.com/aws/copilot-cli/internal/pkg/term/selector"
+	"github.com/aws/copilot-cli/internal/pkg/workspace"
+)
+
+const (
+	jobPackageJobNamePrompt = "Which job would you like to generate a CloudFormation template for?"
+	jobPackageEnvNamePrompt = "Which environment would you like to package this stack for?"
+)
+
+type packageJobVars struct {
+	name      string
+	envName   string
+	appName   string
+	tag       string
+	outputDir string
+}
+
+type packageJobOpts struct {
+	packageJobVars
+
+	// Interfaces to interact with dependencies.
+	ws     wsJobDirReader
+	store  store
+	runner runner
+	sel    wsSelector
+	prompt prompter
+}
+
+func newPackageJobOpts(vars packageJobVars) (*packageJobOpts, error) {
+	ws, err := workspace.New()
+	if err != nil {
+		return nil, fmt.Errorf("new workspace: %w", err)
+	}
+	store, err := config.NewStore()
+	if err != nil {
+		return nil, fmt.Errorf("connect to config store: %w", err)
+	}
+	prompter := prompt.New()
+	opts := &packageJobOpts{
+		packageJobVars: vars,
+		ws:             ws,
+		store:          store,
+		runner:         command.New(),
+		sel:            selector.NewWorkspaceSelect(prompter, store, ws),
+		prompt:         prompter,
+	}
+	return opts, nil
+}
+
+// Validate returns an error if the values provided by the user are invalid.
+func (o *packageJobOpts) Validate() error {
+	if o.appName == "" {
+		return errNoAppInWorkspace
+	}
+	if o.name != "" {
+		names, err := o.ws.JobNames()
+		if err != nil {
+			return fmt.Errorf("list jobs in the workspace: %w", err)
+		}
+		if !contains(o.name, names) {
+			return fmt.Errorf("job '%s' does not exist in the workspace", o.name)
+		}
+	}
+	if o.envName != "" {
+		if _, err := o.store.GetEnvironment(o.appName, o.envName); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Ask prompts the user for any missing required fields.
+func (o *packageJobOpts) Ask() error {
+	if err := o.askJobName(); err != nil {
+		return err
+	}
+	if err := o.askEnvName(); err != nil {
+		return err
+	}
+	tag, err := askImageTag(o.tag, o.prompt, o.runner)
+	if err != nil {
+		return err
+	}
+	o.tag = tag
+	return nil
+}
+
+// Execute prints the CloudFormation template of the application for the environment.
+func (o *packageJobOpts) Execute() error {
+	return nil
+}
+
+func (o *packageJobOpts) askJobName() error {
+	if o.name != "" {
+		return nil
+	}
+
+	name, err := o.sel.Job(jobPackageJobNamePrompt, "")
+	if err != nil {
+		return fmt.Errorf("select job: %w", err)
+	}
+	o.name = name
+	return nil
+}
+
+func (o *packageJobOpts) askEnvName() error {
+	if o.envName != "" {
+		return nil
+	}
+
+	name, err := o.sel.Environment(jobPackageEnvNamePrompt, "", o.appName)
+	if err != nil {
+		return fmt.Errorf("select environment: %w", err)
+	}
+	o.envName = name
+	return nil
+}
+
+// buildJobPackageCmd builds the command for printing a job's CloudFormation template.
+func buildJobPackageCmd() *cobra.Command {
+	vars := packageJobVars{}
+	cmd := &cobra.Command{
+		Use:   "package",
+		Short: "Prints the AWS CloudFormation template of a job.",
+		Long:  `Prints the CloudFormation template used to deploy a job to an environment.`,
+		Example: `
+  Print the CloudFormation template for the "report-generator" job parametrized for the "test" environment.
+  /code $ copilot job package -n report-generator -e test
+
+  Write the CloudFormation stack and configuration to a "infrastructure/" sub-directory instead of printing.
+  /code $ copilot job package -n report-generator -e test --output-dir ./infrastructure
+  /code $ ls ./infrastructure
+  /code report-generator.stack.yml      report-generator-test.config.yml`,
+		RunE: runCmdE(func(cmd *cobra.Command, args []string) error {
+			opts, err := newPackageJobOpts(vars)
+			if err != nil {
+				return err
+			}
+
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+			if err := opts.Ask(); err != nil {
+				return err
+			}
+			return opts.Execute()
+		}),
+	}
+	cmd.Flags().StringVarP(&vars.name, nameFlag, nameFlagShort, "", jobFlagDescription)
+	cmd.Flags().StringVarP(&vars.envName, envFlag, envFlagShort, "", envFlagDescription)
+	cmd.Flags().StringVarP(&vars.appName, appFlag, appFlagShort, tryReadingAppName(), appFlagDescription)
+	cmd.Flags().StringVar(&vars.tag, imageTagFlag, "", imageTagFlagDescription)
+	cmd.Flags().StringVar(&vars.outputDir, stackOutputDirFlag, "", stackOutputDirFlagDescription)
+	return cmd
+}

internal/pkg/cli/job_package_test.go
@@ -1,0 +1,238 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/aws/copilot-cli/internal/pkg/cli/mocks"
+	"github.com/aws/copilot-cli/internal/pkg/config"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPackageJobOpts_Validate(t *testing.T) {
+	var (
+		mockWorkspace *mocks.MockwsJobDirReader
+		mockStore     *mocks.Mockstore
+	)
+
+	testCases := map[string]struct {
+		inAppName string
+		inEnvName string
+		inJobName string
+
+		setupMocks func()
+
+		wantedErrorS string
+	}{
+		"invalid workspace": {
+			setupMocks: func() {
+				mockWorkspace.EXPECT().JobNames().Times(0)
+				mockStore.EXPECT().GetEnvironment(gomock.Any(), gomock.Any()).Times(0)
+			},
+			wantedErrorS: "could not find an application attached to this workspace, please run `app init` first",
+		},
+		"error while fetching job": {
+			inAppName: "phonetool",
+			inJobName: "resizer",
+			setupMocks: func() {
+				mockWorkspace.EXPECT().JobNames().Return(nil, errors.New("some error"))
+				mockStore.EXPECT().GetEnvironment(gomock.Any(), gomock.Any()).Times(0)
+			},
+
+			wantedErrorS: "list jobs in the workspace: some error",
+		},
+		"error when job not in workspace": {
+			inAppName: "phonetool",
+			inJobName: "resizer",
+			setupMocks: func() {
+				mockWorkspace.EXPECT().JobNames().Return([]string{"other-job"}, nil)
+				mockStore.EXPECT().GetEnvironment(gomock.Any(), gomock.Any()).Times(0)
+			},
+
+			wantedErrorS: "job 'resizer' does not exist in the workspace",
+		},
+		"error while fetching environment": {
+			inAppName: "phonetool",
+			inEnvName: "test",
+
+			setupMocks: func() {
+				mockWorkspace.EXPECT().JobNames().Times(0)
+				mockStore.EXPECT().GetEnvironment("phonetool", "test").Return(nil, &config.ErrNoSuchEnvironment{
+					ApplicationName: "phonetool",
+					EnvironmentName: "test",
+				})
+			},
+
+			wantedErrorS: (&config.ErrNoSuchEnvironment{
+				ApplicationName: "phonetool",
+				EnvironmentName: "test",
+			}).Error(),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// GIVEN
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockWorkspace = mocks.NewMockwsJobDirReader(ctrl)
+			mockStore = mocks.NewMockstore(ctrl)
+
+			tc.setupMocks()
+
+			opts := &packageJobOpts{
+				packageJobVars: packageJobVars{
+					name:    tc.inJobName,
+					envName: tc.inEnvName,
+					appName: tc.inAppName,
+				},
+				ws:    mockWorkspace,
+				store: mockStore,
+			}
+
+			// WHEN
+			err := opts.Validate()
+
+			// THEN
+			if tc.wantedErrorS != "" {
+				require.EqualError(t, err, tc.wantedErrorS, "error %v does not match '%s'", err, tc.wantedErrorS)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestPackageJobOpts_Ask(t *testing.T) {
+	const testAppName = "phonetool"
+	testCases := map[string]struct {
+		inJobName string
+		inEnvName string
+		inTag     string
+
+		expectSelector func(m *mocks.MockwsSelector)
+		expectPrompt   func(m *mocks.Mockprompter)
+		expectRunner   func(m *mocks.Mockrunner)
+
+		wantedJobName string
+		wantedEnvName string
+		wantedTag     string
+		wantedErrorS  string
+	}{
+		"prompt for all options": {
+			expectRunner: func(m *mocks.Mockrunner) {
+				m.EXPECT().Run(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("not a git repo"))
+			},
+			expectSelector: func(m *mocks.MockwsSelector) {
+				m.EXPECT().Job(jobPackageJobNamePrompt, "").Return("resizer", nil)
+				m.EXPECT().Environment(jobPackageEnvNamePrompt, "", testAppName).Return("test", nil)
+			},
+			expectPrompt: func(m *mocks.Mockprompter) {
+				m.EXPECT().Get(inputImageTagPrompt, "", gomock.Any()).Return("v1.0.0", nil)
+			},
+
+			wantedJobName: "resizer",
+			wantedEnvName: "test",
+			wantedTag:     "v1.0.0",
+		},
+		"prompt only for the job name": {
+			inEnvName: "test",
+			inTag:     "v1.0.0",
+
+			expectSelector: func(m *mocks.MockwsSelector) {
+				m.EXPECT().Job(jobPackageJobNamePrompt, "").Return("resizer", nil)
+				m.EXPECT().Environment(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			expectPrompt: func(m *mocks.Mockprompter) {
+				m.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			expectRunner: func(m *mocks.Mockrunner) {},
+
+			wantedJobName: "resizer",
+			wantedEnvName: "test",
+			wantedTag:     "v1.0.0",
+		},
+		"prompt only for the env name": {
+			inJobName: "resizer",
+			inTag:     "v1.0.0",
+
+			expectSelector: func(m *mocks.MockwsSelector) {
+				m.EXPECT().Job(gomock.Any(), gomock.Any()).Times(0)
+				m.EXPECT().Environment(jobPackageEnvNamePrompt, "", testAppName).Return("test", nil)
+			},
+			expectPrompt: func(m *mocks.Mockprompter) {
+				m.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			expectRunner: func(m *mocks.Mockrunner) {},
+
+			wantedJobName: "resizer",
+			wantedEnvName: "test",
+			wantedTag:     "v1.0.0",
+		},
+		"don't prompt": {
+			inJobName: "resizer",
+			inEnvName: "test",
+			inTag:     "v1.0.0",
+
+			expectSelector: func(m *mocks.MockwsSelector) {
+				m.EXPECT().Job(gomock.Any(), gomock.Any()).Times(0)
+				m.EXPECT().Environment(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			expectPrompt: func(m *mocks.Mockprompter) {
+				m.EXPECT().Get(gomock.Any(), gomock.Any(), gomock.Any()).Times(0)
+			},
+			expectRunner: func(m *mocks.Mockrunner) {},
+
+			wantedJobName: "resizer",
+			wantedEnvName: "test",
+			wantedTag:     "v1.0.0",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			// GIVEN
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockSelector := mocks.NewMockwsSelector(ctrl)
+			mockPrompt := mocks.NewMockprompter(ctrl)
+			mockRunner := mocks.NewMockrunner(ctrl)
+
+			tc.expectSelector(mockSelector)
+			tc.expectPrompt(mockPrompt)
+			tc.expectRunner(mockRunner)
+
+			opts := &packageJobOpts{
+				packageJobVars: packageJobVars{
+					name:    tc.inJobName,
+					envName: tc.inEnvName,
+					tag:     tc.inTag,
+					appName: testAppName,
+				},
+				sel:    mockSelector,
+				prompt: mockPrompt,
+				runner: mockRunner,
+			}
+
+			// WHEN
+			err := opts.Ask()
+
+			// THEN
+			require.Equal(t, tc.wantedJobName, opts.name)
+			require.Equal(t, tc.wantedEnvName, opts.envName)
+			require.Equal(t, tc.wantedTag, opts.tag)
+
+			if tc.wantedErrorS != "" {
+				require.EqualError(t, err, tc.wantedErrorS)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}

internal/pkg/cli/svc_deploy.go
@@ -32,10 +32,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	inputImageTagPrompt = "Input an image tag value:"
-)
-
 type deploySvcVars struct {
 	appName      string
 	name         string
@@ -120,9 +116,11 @@ func (o *deploySvcOpts) Ask() error {
 	if err := o.askEnvName(); err != nil {
 		return err
 	}
-	if err := o.askImageTag(); err != nil {
+	tag, err := askImageTag(o.imageTag, o.prompt, o.cmd)
+	if err != nil {
 		return err
 	}
+	o.imageTag = tag
 	return nil
 }
 
@@ -222,29 +220,6 @@ func (o *deploySvcOpts) askEnvName() error {
 		return fmt.Errorf("select environment: %w", err)
 	}
 	o.envName = name
-	return nil
-}
-
-func (o *deploySvcOpts) askImageTag() error {
-	if o.imageTag != "" {
-		return nil
-	}
-
-	tag, err := getVersionTag(o.cmd)
-
-	if err == nil {
-		o.imageTag = tag
-
-		return nil
-	}
-
-	log.Warningln("Failed to default tag, are you in a git repository?")
-
-	userInputTag, err := o.prompt.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
-	if err != nil {
-		return fmt.Errorf("prompt for image tag: %w", err)
-	}
-	o.imageTag = userInputTag
 	return nil
 }
 

internal/pkg/cli/svc_package.go
@@ -52,7 +52,7 @@ type packageSvcOpts struct {
 
 	// Interfaces to interact with dependencies.
 	addonsSvc       templater
-	initAddonsSvc   func(*packageSvcOpts) error // Overriden in tests.
+	initAddonsSvc   func(*packageSvcOpts) error // Overridden in tests.
 	ws              wsSvcReader
 	store           store
 	appCFN          appResourcesGetter
@@ -149,13 +149,18 @@ func (o *packageSvcOpts) Validate() error {
 
 // Ask prompts the user for any missing required fields.
 func (o *packageSvcOpts) Ask() error {
-	if err := o.askAppName(); err != nil {
+	if err := o.askSvcName(); err != nil {
 		return err
 	}
 	if err := o.askEnvName(); err != nil {
 		return err
 	}
-	return o.askTag()
+	tag, err := askImageTag(o.tag, o.prompt, o.runner)
+	if err != nil {
+		return err
+	}
+	o.tag = tag
+	return nil
 }
 
 // Execute prints the CloudFormation template of the application for the environment.
@@ -203,7 +208,7 @@ func (o *packageSvcOpts) Execute() error {
 	return err
 }
 
-func (o *packageSvcOpts) askAppName() error {
+func (o *packageSvcOpts) askSvcName() error {
 	if o.name != "" {
 		return nil
 	}
@@ -226,23 +231,6 @@ func (o *packageSvcOpts) askEnvName() error {
 		return fmt.Errorf("select environment: %w", err)
 	}
 	o.envName = name
-	return nil
-}
-
-func (o *packageSvcOpts) askTag() error {
-	if o.tag != "" {
-		return nil
-	}
-
-	tag, err := getVersionTag(o.runner)
-	if err != nil {
-		// We're not in a Git repository, prompt the user for an explicit tag.
-		tag, err = o.prompt.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
-		if err != nil {
-			return fmt.Errorf("prompt get image tag: %w", err)
-		}
-	}
-	o.tag = tag
 	return nil
 }
 

internal/pkg/term/command/command.go
@@ -34,6 +34,13 @@ func Stdout(writer io.Writer) Option {
 	}
 }
 
+// Stderr sets the internal *exec.Cmd's Stderr field.
+func Stderr(writer io.Writer) Option {
+	return func(c *exec.Cmd) {
+		c.Stderr = writer
+	}
+}
+
 // Run runs the input command with input args with Stdout and Stderr defaulted to os.Stderr.
 // Input options will override these defaults.
 func (s Service) Run(name string, args []string, options ...Option) error {

commit 48eb4d8b4ecca6adfc89ecd70ccccad5206d777d

fix(cli): limit image tag warning to log only conditionally (#1517)
Fixes bug introduced in https://github.com/aws/copilot-cli/pull/1480.
Warning about not being in git repo was appearing even when user was in git repo.
This moves that warning so it's within a conditional.

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/git.go
@@ -9,9 +9,7 @@ import (
 	"strings"
 
 	"github.com/aws/copilot-cli/internal/pkg/term/command"
-
 	"github.com/aws/copilot-cli/internal/pkg/term/log"
-
 	"github.com/aws/copilot-cli/internal/pkg/term/prompt"
 )
 
@@ -35,8 +33,8 @@ func askImageTag(tag string, prompter prompter, cmd runner) (string, error) {
 		return tag, nil
 	}
 	tag, err := getVersionTag(cmd)
-	log.Warningln("Failed to default tag, are you in a git repository?")
 	if err != nil {
+		log.Warningln("Couldn't find a git commit sha to use as an image tag. Are you in a git repository?")
 		// User is not in a Git repository, so prompt for a tag.
 		tag, err = prompter.Get(inputImageTagPrompt, "", prompt.RequireNonEmpty)
 		if err != nil {