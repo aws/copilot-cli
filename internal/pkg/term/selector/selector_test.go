aws/copilot-cli

commit 52960893713f20926e492b93a07786df9c63dd60

chore(sel): restrict selectors to intersection of workspace and store workloads  (#1475)
Previously, the selectors for jobs/services returned all workloads found in the workspace, including deleted ones. Now, the selectors return only workloads that are in both the workspace and the config store.

Fixes https://github.com/aws/copilot-cli/issues/1447.

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/init.go
@@ -84,6 +84,10 @@ func newInitOpts(vars initVars) (*initOpts, error) {
 		return nil, err
 	}
 	prompt := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompt, ssm, ws)
+	if err != nil {
+		return nil, err
+	}
 	spin := termprogress.NewSpinner()
 	id := identity.New(defaultSess)
 	deployer := cloudformation.New(defaultSess)
@@ -115,7 +119,7 @@ func newInitOpts(vars initVars) (*initOpts, error) {
 		store:       ssm,
 		appDeployer: deployer,
 		prog:        spin,
-		sel:         selector.NewWorkspaceSelect(prompt, ssm, ws),
+		sel:         sel,
 		prompt:      prompt,
 		setupParser: func(o *initSvcOpts) {
 			o.df = dockerfile.New(o.fs, o.dockerfilePath)
@@ -147,7 +151,7 @@ func newInitOpts(vars initVars) (*initOpts, error) {
 		prompt:       prompt,
 		ws:           ws,
 		unmarshal:    manifest.UnmarshalWorkload,
-		sel:          selector.NewWorkspaceSelect(prompt, ssm, ws),
+		sel:          sel,
 		spinner:      spin,
 		cmd:          command.New(),
 		sessProvider: sessProvider,

internal/pkg/cli/job_delete.go
@@ -83,13 +83,17 @@ func newDeleteJobOpts(vars deleteJobVars) (*deleteJobOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	return &deleteJobOpts{
 		deleteJobVars: vars,
 
 		store:   store,
 		spinner: termprogress.NewSpinner(),
 		prompt:  prompt.New(),
-		sel:     selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:     sel,
 		sess:    provider,
 		appCFN:  cloudformation.New(defaultSession),
 		getJobCFN: func(session *awssession.Session) wlDeleter {

internal/pkg/cli/job_deploy.go
@@ -74,6 +74,10 @@ func newJobDeployOpts(vars deployJobVars) (*deployJobOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	return &deployJobOpts{
 		deployJobVars: vars,
 
@@ -81,7 +85,7 @@ func newJobDeployOpts(vars deployJobVars) (*deployJobOpts, error) {
 		ws:           ws,
 		unmarshal:    manifest.UnmarshalWorkload,
 		spinner:      termprogress.NewSpinner(),
-		sel:          selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:          sel,
 		prompt:       prompter,
 		cmd:          command.New(),
 		sessProvider: sessions.NewProvider(),

internal/pkg/cli/job_init.go
@@ -85,6 +85,10 @@ func newInitJobOpts(vars initJobVars) (*initJobOpts, error) {
 	}
 
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	return &initJobOpts{
 		initJobVars: vars,
 
@@ -94,7 +98,7 @@ func newInitJobOpts(vars initJobVars) (*initJobOpts, error) {
 		appDeployer: cloudformation.New(sess),
 		prog:        termprogress.NewSpinner(),
 		prompt:      prompter,
-		sel:         selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:         sel,
 	}, nil
 }
 

internal/pkg/cli/job_package.go
@@ -49,12 +49,16 @@ func newPackageJobOpts(vars packageJobVars) (*packageJobOpts, error) {
 		return nil, fmt.Errorf("connect to config store: %w", err)
 	}
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	opts := &packageJobOpts{
 		packageJobVars: vars,
 		ws:             ws,
 		store:          store,
 		runner:         command.New(),
-		sel:            selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:            sel,
 		prompt:         prompter,
 	}
 	return opts, nil

internal/pkg/cli/storage_init.go
@@ -125,6 +125,10 @@ func newStorageInitOpts(vars initStorageVars) (*initStorageOpts, error) {
 	}
 
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	return &initStorageOpts{
 		initStorageVars: vars,
 		appName:         tryReadingAppName(),
@@ -132,7 +136,7 @@ func newStorageInitOpts(vars initStorageVars) (*initStorageOpts, error) {
 		fs:     &afero.Afero{Fs: afero.NewOsFs()},
 		store:  store,
 		ws:     ws,
-		sel:    selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:    sel,
 		prompt: prompter,
 	}, nil
 }
@@ -275,9 +279,7 @@ func (o *initStorageOpts) askStorageSvc() error {
 	if o.storageSvc != "" {
 		return nil
 	}
-	svc, err := o.sel.Service(storageInitSvcPrompt,
-		storageInitSvcHelp,
-	)
+	svc, err := o.sel.Service(storageInitSvcPrompt, storageInitSvcHelp)
 	if err != nil {
 		return fmt.Errorf("retrieve local service names: %w", err)
 	}

internal/pkg/cli/svc_delete.go
@@ -83,6 +83,10 @@ func newDeleteSvcOpts(vars deleteSvcVars) (*deleteSvcOpts, error) {
 	if err != nil {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 
 	return &deleteSvcOpts{
 		deleteSvcVars: vars,
@@ -91,7 +95,7 @@ func newDeleteSvcOpts(vars deleteSvcVars) (*deleteSvcOpts, error) {
 		spinner: termprogress.NewSpinner(),
 		prompt:  prompter,
 		sess:    provider,
-		sel:     selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:     sel,
 		appCFN:  cloudformation.New(defaultSession),
 		getSvcCFN: func(session *awssession.Session) wlDeleter {
 			return cloudformation.New(session)

internal/pkg/cli/svc_deploy.go
@@ -76,6 +76,10 @@ func newSvcDeployOpts(vars deploySvcVars) (*deploySvcOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
+	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
+	if err != nil {
+		return nil, err
+	}
 	return &deploySvcOpts{
 		deploySvcVars: vars,
 
@@ -83,7 +87,7 @@ func newSvcDeployOpts(vars deploySvcVars) (*deploySvcOpts, error) {
 		ws:           ws,
 		unmarshal:    manifest.UnmarshalWorkload,
 		spinner:      termprogress.NewSpinner(),
-		sel:          selector.NewWorkspaceSelect(prompter, store, ws),
+		sel:          sel,
 		prompt:       prompter,
 		cmd:          command.New(),
 		sessProvider: sessions.NewProvider(),

commit 3c328af97ceea1f362327b9479c311140af33f27

chore(sel): move Summary() call into Service and Job methods (#1528)
Fixes errors introduced in https://github.com/aws/copilot-cli/pull/1475.
`Summary()` is no longer being called in `NewWorkspaceSelect()` because the necessary workspace will have been created by the time it's needed, so `copilot init` e2e tests should no longer fail.

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/init.go
@@ -84,10 +84,7 @@ func newInitOpts(vars initVars) (*initOpts, error) {
 		return nil, err
 	}
 	prompt := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompt, ssm, ws)
-	if err != nil {
-		return nil, err
-	}
+	sel := selector.NewWorkspaceSelect(prompt, ssm, ws)
 	spin := termprogress.NewSpinner()
 	id := identity.New(defaultSess)
 	deployer := cloudformation.New(defaultSess)

internal/pkg/cli/job_delete.go
@@ -83,17 +83,13 @@ func newDeleteJobOpts(vars deleteJobVars) (*deleteJobOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	return &deleteJobOpts{
 		deleteJobVars: vars,
 
 		store:   store,
 		spinner: termprogress.NewSpinner(),
 		prompt:  prompt.New(),
-		sel:     sel,
+		sel:     selector.NewWorkspaceSelect(prompter, store, ws),
 		sess:    provider,
 		appCFN:  cloudformation.New(defaultSession),
 		getJobCFN: func(session *awssession.Session) wlDeleter {

internal/pkg/cli/job_deploy.go
@@ -74,7 +74,6 @@ func newJobDeployOpts(vars deployJobVars) (*deployJobOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
 	if err != nil {
 		return nil, err
 	}
@@ -85,7 +84,7 @@ func newJobDeployOpts(vars deployJobVars) (*deployJobOpts, error) {
 		ws:           ws,
 		unmarshal:    manifest.UnmarshalWorkload,
 		spinner:      termprogress.NewSpinner(),
-		sel:          sel,
+		sel:          selector.NewWorkspaceSelect(prompter, store, ws),
 		prompt:       prompter,
 		cmd:          command.New(),
 		sessProvider: sessions.NewProvider(),

internal/pkg/cli/job_init.go
@@ -85,10 +85,6 @@ func newInitJobOpts(vars initJobVars) (*initJobOpts, error) {
 	}
 
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	return &initJobOpts{
 		initJobVars: vars,
 
@@ -98,7 +94,7 @@ func newInitJobOpts(vars initJobVars) (*initJobOpts, error) {
 		appDeployer: cloudformation.New(sess),
 		prog:        termprogress.NewSpinner(),
 		prompt:      prompter,
-		sel:         sel,
+		sel:         selector.NewWorkspaceSelect(prompter, store, ws),
 	}, nil
 }
 

internal/pkg/cli/job_package.go
@@ -49,16 +49,12 @@ func newPackageJobOpts(vars packageJobVars) (*packageJobOpts, error) {
 		return nil, fmt.Errorf("connect to config store: %w", err)
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	opts := &packageJobOpts{
 		packageJobVars: vars,
 		ws:             ws,
 		store:          store,
 		runner:         command.New(),
-		sel:            sel,
+		sel:            selector.NewWorkspaceSelect(prompter, store, ws),
 		prompt:         prompter,
 	}
 	return opts, nil

internal/pkg/cli/storage_init.go
@@ -125,10 +125,6 @@ func newStorageInitOpts(vars initStorageVars) (*initStorageOpts, error) {
 	}
 
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	return &initStorageOpts{
 		initStorageVars: vars,
 		appName:         tryReadingAppName(),
@@ -136,7 +132,7 @@ func newStorageInitOpts(vars initStorageVars) (*initStorageOpts, error) {
 		fs:     &afero.Afero{Fs: afero.NewOsFs()},
 		store:  store,
 		ws:     ws,
-		sel:    sel,
+		sel:    selector.NewWorkspaceSelect(prompter, store, ws),
 		prompt: prompter,
 	}, nil
 }

internal/pkg/cli/svc_delete.go
@@ -83,10 +83,6 @@ func newDeleteSvcOpts(vars deleteSvcVars) (*deleteSvcOpts, error) {
 	if err != nil {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 
 	return &deleteSvcOpts{
 		deleteSvcVars: vars,
@@ -95,7 +91,7 @@ func newDeleteSvcOpts(vars deleteSvcVars) (*deleteSvcOpts, error) {
 		spinner: termprogress.NewSpinner(),
 		prompt:  prompter,
 		sess:    provider,
-		sel:     sel,
+		sel:     selector.NewWorkspaceSelect(prompter, store, ws),
 		appCFN:  cloudformation.New(defaultSession),
 		getSvcCFN: func(session *awssession.Session) wlDeleter {
 			return cloudformation.New(session)

internal/pkg/cli/svc_deploy.go
@@ -76,10 +76,6 @@ func newSvcDeployOpts(vars deploySvcVars) (*deploySvcOpts, error) {
 		return nil, fmt.Errorf("new workspace: %w", err)
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	return &deploySvcOpts{
 		deploySvcVars: vars,
 
@@ -87,7 +83,7 @@ func newSvcDeployOpts(vars deploySvcVars) (*deploySvcOpts, error) {
 		ws:           ws,
 		unmarshal:    manifest.UnmarshalWorkload,
 		spinner:      termprogress.NewSpinner(),
-		sel:          sel,
+		sel:          selector.NewWorkspaceSelect(prompter, store, ws),
 		prompt:       prompter,
 		cmd:          command.New(),
 		sessProvider: sessions.NewProvider(),

internal/pkg/cli/svc_init.go
@@ -109,10 +109,6 @@ func newInitSvcOpts(vars initSvcVars) (*initSvcOpts, error) {
 		return nil, err
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	return &initSvcOpts{
 		initSvcVars: vars,
 
@@ -122,7 +118,7 @@ func newInitSvcOpts(vars initSvcVars) (*initSvcOpts, error) {
 		appDeployer: cloudformation.New(sess),
 		prog:        termprogress.NewSpinner(),
 		prompt:      prompter,
-		sel:         sel,
+		sel:         selector.NewWorkspaceSelect(prompter, store, ws),
 
 		setupParser: func(o *initSvcOpts) {
 			o.df = dockerfile.New(o.fs, o.dockerfilePath)

internal/pkg/cli/svc_package.go
@@ -81,10 +81,6 @@ func newPackageSvcOpts(vars packageSvcVars) (*packageSvcOpts, error) {
 		return nil, fmt.Errorf("retrieve default session: %w", err)
 	}
 	prompter := prompt.New()
-	sel, err := selector.NewWorkspaceSelect(prompter, store, ws)
-	if err != nil {
-		return nil, err
-	}
 	opts := &packageSvcOpts{
 		packageSvcVars: vars,
 		initAddonsSvc:  initPackageAddonsSvc,
@@ -92,7 +88,7 @@ func newPackageSvcOpts(vars packageSvcVars) (*packageSvcOpts, error) {
 		store:          store,
 		appCFN:         cloudformation.New(sess),
 		runner:         command.New(),
-		sel:            sel,
+		sel:            selector.NewWorkspaceSelect(prompter, store, ws),
 		prompt:         prompter,
 		stackWriter:    os.Stdout,
 		paramsWriter:   ioutil.Discard,

internal/pkg/term/selector/selector.go
@@ -154,16 +154,11 @@ func NewConfigSelect(prompt Prompter, store ConfigLister) *ConfigSelect {
 
 // NewWorkspaceSelect returns a new selector that chooses applications and environments from the config store, but
 // services from the local workspace.
-func NewWorkspaceSelect(prompt Prompter, store ConfigLister, ws WorkspaceRetriever) (*WorkspaceSelect, error) {
-	summary, err := ws.Summary()
-	if err != nil {
-		return nil, fmt.Errorf("read workspace summary: %w", err)
-	}
+func NewWorkspaceSelect(prompt Prompter, store ConfigLister, ws WorkspaceRetriever) *WorkspaceSelect {
 	return &WorkspaceSelect{
-		Select:  NewSelect(prompt, store),
-		ws:      ws,
-		appName: summary.Application,
-	}, nil
+		Select: NewSelect(prompt, store),
+		ws:     ws,
+	}
 }
 
 // NewDeploySelect returns a new selector that chooses services and environments from the deploy store.
@@ -271,11 +266,15 @@ func (s *DeploySelect) DeployedService(prompt, help string, app string, opts ...
 
 // Service fetches all services in the workspace and then prompts the user to select one.
 func (s *WorkspaceSelect) Service(msg, help string) (string, error) {
+	summary, err := s.ws.Summary()
+	if err != nil {
+		return "", fmt.Errorf("read workspace summary: %w", err)
+	}
 	wsServiceNames, err := s.retrieveWorkspaceServices()
 	if err != nil {
 		return "", fmt.Errorf("retrieve services from workspace: %w", err)
 	}
-	storeServiceNames, err := s.Select.config.ListServices(s.appName)
+	storeServiceNames, err := s.Select.config.ListServices(summary.Application)
 	if err != nil {
 		return "", fmt.Errorf("retrieve services from store: %w", err)
 	}
@@ -297,11 +296,15 @@ func (s *WorkspaceSelect) Service(msg, help string) (string, error) {
 
 // Job fetches all jobs in the workspace and then prompts the user to select one.
 func (s *WorkspaceSelect) Job(msg, help string) (string, error) {
+	summary, err := s.ws.Summary()
+	if err != nil {
+		return "", fmt.Errorf("read workspace summary: %w", err)
+	}
 	wsJobNames, err := s.retrieveWorkspaceJobs()
 	if err != nil {
 		return "", fmt.Errorf("retrieve jobs from workspace: %w", err)
 	}
-	storeJobNames, err := s.Select.config.ListServices(s.appName)
+	storeJobNames, err := s.Select.config.ListServices(summary.Application)
 	if err != nil {
 		return "", fmt.Errorf("retrieve jobs from store: %w", err)
 	}

internal/pkg/term/selector/selector_test.go
@@ -219,6 +219,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 			setupMocks: func(m workspaceSelectMocks) {
 				m.workloadLister.EXPECT().ServiceNames().Return(
 					[]string{}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{}, nil).Times(1)
 				m.prompt.EXPECT().SelectOne(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
@@ -233,6 +237,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 						"service1",
 					}, nil).
 					Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{}, nil).Times(1)
 			},
@@ -243,6 +251,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 				m.workloadLister.EXPECT().ServiceNames().Return(
 					[]string{}, nil).
 					Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -260,6 +272,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 					[]string{
 						"service1",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -282,6 +298,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 						"service2",
 						"service3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -302,6 +322,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 					[]string{
 						"service3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -335,6 +359,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 						"service2",
 						"service3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -369,6 +397,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 				m.workloadLister.
 					EXPECT().ServiceNames().Return(
 					[]string{""}, errors.New("some error"))
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 			},
 			wantErr: errors.New("retrieve services from workspace: some error"),
 		},
@@ -380,6 +412,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 						"service2",
 					}, nil).
 					Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					nil, errors.New("some error"))
 			},
@@ -394,6 +430,10 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 						"service2",
 					}, nil).
 					Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -437,8 +477,7 @@ func TestWorkspaceSelect_Service(t *testing.T) {
 					prompt: mockprompt,
 					config: mockconfigLister,
 				},
-				ws:      mockwsRetriever,
-				appName: "app-name",
+				ws: mockwsRetriever,
 			}
 			got, err := sel.Service("Select a service", "Help text")
 			if tc.wantErr != nil {
@@ -461,6 +500,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 				m.workloadLister.
 					EXPECT().JobNames().Return(
 					[]string{}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{}, nil).Times(1)
 				m.prompt.
@@ -477,6 +520,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 					[]string{
 						"job1",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{}, nil).Times(1)
 				m.prompt.
@@ -491,6 +538,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 				m.workloadLister.
 					EXPECT().JobNames().Return(
 					[]string{}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -514,6 +565,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 					[]string{
 						"resizer",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -537,6 +592,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 						"job2",
 						"job3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -557,6 +616,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 					[]string{
 						"job3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -589,6 +652,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 						"job2",
 						"job3",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.
 					EXPECT().
 					ListServices("app-name").
@@ -627,6 +694,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 				m.workloadLister.
 					EXPECT().JobNames().Return(
 					[]string{""}, errors.New("some error"))
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 			},
 			wantErr: errors.New("retrieve jobs from workspace: some error"),
 		},
@@ -638,6 +709,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 						"service2",
 					}, nil).
 					Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					nil, errors.New("some error"))
 			},
@@ -650,6 +725,10 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 						"resizer1",
 						"resizer2",
 					}, nil).Times(1)
+				m.workloadLister.EXPECT().Summary().Return(
+					&workspace.Summary{
+						Application: "app-name",
+					}, nil)
 				m.configLister.EXPECT().ListServices("app-name").Return(
 					[]*config.Workload{
 						{
@@ -693,8 +772,7 @@ func TestWorkspaceSelect_Job(t *testing.T) {
 					prompt: mockprompt,
 					config: mockconfigLister,
 				},
-				ws:      mockwsRetriever,
-				appName: "app-name",
+				ws: mockwsRetriever,
 			}
 			got, err := sel.Job("Select a job", "Help text")
 			if tc.wantErr != nil {