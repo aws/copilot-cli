c81a1225f9553ae94f39d919bb9ac3c7f2dec5fc
Austin Ely <austiely@amazon.com>
2020-10-20 22:40:39 +0000

    chore(job): allow server-side validation of rate() and cron() expressions

--- internal/pkg/deploy/cloudformation/stack/scheduled_job.go
+++ internal/pkg/deploy/cloudformation/stack/scheduled_job.go
@@ -6,6 +6,7 @@ package stack
 import (
 	"errors"
 	"fmt"
+	"regexp"
 	"strings"
 	"time"
 	"unicode"
@@ -39,6 +40,8 @@ type ScheduledJob struct {
 var (
 	fmtRateScheduleExpression = "rate(%d %s)" // rate({duration} {units})
 	fmtCronScheduleExpression = "cron(%s)"
+
+	awsScheduleRegexp = regexp.MustCompile(`(?:rate|cron)\(.*\)`) // Validates that an expression is of the form rate(xyz) or cron(abc)
 )
 
 const (
@@ -179,11 +182,16 @@ func (j *ScheduledJob) SerializedParameters() (string, error) {
 // Day-of-week expressions are zero-indexed in Golang but one-indexed in AWS.
 // @every cron definition strings are converted to rates.
 // All others become cron expressions.
+// Exception is made for strings of the form "rate( )" or "cron( )". These are accepted as-is and
+// validated server-side by CloudFormation.
 func (j *ScheduledJob) awsSchedule() (string, error) {
 	if j.manifest.Schedule == "" {
 		return "", fmt.Errorf(`missing required field "schedule" in manifest for job %s`, j.name)
 	}
-
+	// If the schedule uses default CloudWatch Events syntax, pass it through for server-side validation.
+	if match := awsScheduleRegexp.FindStringSubmatch(j.manifest.Schedule); match != nil {
+		return j.manifest.Schedule, nil
+	}
 	// Try parsing the string as a cron expression to validate it.
 	if _, err := cron.ParseStandard(j.manifest.Schedule); err != nil {
 		return "", errScheduleInvalid{reason: err}
