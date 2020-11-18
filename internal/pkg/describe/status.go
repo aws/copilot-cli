99185e692d006bd07144e5f17da7f8048c2683d2
Janice <jwh@amazon.com>
2020-11-18 14:58:50 -0800

    chore: add underlines to svc status

--- internal/pkg/describe/status.go
+++ internal/pkg/describe/status.go
@@ -195,12 +195,14 @@ func (s *ServiceStatusDesc) HumanString() string {
 	fmt.Fprint(writer, color.Bold.Sprint("\nTask Status\n\n"))
 	writer.Flush()
 	fmt.Fprintf(writer, "  %s\t%s\t%s\t%s\t%s\t%s\n", "ID", "Image Digest", "Last Status", "Started At", "Stopped At", "Health Status")
+	fmt.Fprintf(writer, "  %s\t%s\t%s\t%s\t%s\t%s\n", "--", "------------", "-----------", "----------", "----------", "-------------")
 	for _, task := range s.Tasks {
 		fmt.Fprint(writer, task.HumanString())
 	}
 	fmt.Fprint(writer, color.Bold.Sprint("\nAlarms\n\n"))
 	writer.Flush()
 	fmt.Fprintf(writer, "  %s\t%s\t%s\t%s\n", "Name", "Condition", "Last Updated", "Health")
+	fmt.Fprintf(writer, "  %s\t%s\t%s\t%s\n", "----", "---------", "------------", "------")
 	for _, alarm := range s.Alarms {
 		updatedTimeSince := humanizeTime(alarm.UpdatedTimes)
 		printWithMaxWidth(writer, "  %s\t%s\t%s\t%s\n", maxAlarmStatusColumnWidth, alarm.Name, alarm.Condition, updatedTimeSince, alarmHealthColor(alarm.Status))
