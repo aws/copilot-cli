6385f25dd93c49fd81e1f8dddaf4999f95b68ae7
Austin Ely <austiely@amazon.com>
2020-12-16 21:59:09 +0000

    chore(cli): add boilerplate for new task delete cmd

---
+++ internal/pkg/cli/task_delete.go
@@ -0,0 +1,209 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/aws/copilot-cli/internal/pkg/config"
+	"github.com/aws/copilot-cli/internal/pkg/term/color"
+	"github.com/aws/copilot-cli/internal/pkg/term/log"
+	"github.com/spf13/cobra"
+)
+
+const (
+	taskDeleteNamePrompt              = "Which task would you like to delete?"
+	fmtTaskDeleteDefaultConfirmPrompt = "Are you sure you want to delete %s from the default cluster?"
+	fmtTaskDeleteFromEnvConfirmPrompt = "Are you sure you want to delete %s from environment %s?"
+	taskDeleteConfirmHelp             = "This will delete the task's stack and stop all current executions."
+)
+
+var (
+	errDefaultClusterWithApp = errors.New("")
+	errTaskDeleteCancelled   = errors.New("task delete cancelled - no changes made")
+)
+
+type deleteTaskVars struct {
+	name             string
+	app              string
+	env              string
+	skipConfirmation bool
+	defaultCluster   bool
+}
+
+type deleteTaskOpts struct {
+	deleteTaskVars
+
+	// Dependencies to interact with other modules
+	store  store
+	prompt prompter
+}
+
+// Validate checks that flag inputs are valid. 
+func (o *deleteTaskOpts) Validate() error {
+
+	if o.name != "" {
+		if err := basicNameValidation(o.name); err != nil {
+			return err
+		}
+	}
+
+	// If default flag specified, 
+	if err := o.validateFlagsWithDefaultCluster(); err != nil {
+		return err
+	}
+
+	if err := o.validateFlagsWithEnv(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *deleteTaskOpts) validateFlagsWithEnv() error {
+	if o.app != "" {
+		return o.validateAppName()
+	}
+
+	if o.env != "" {
+		return o.validateEnvName()
+	}
+
+	return nil
+}
+
+func (o *deleteTaskOpts) validateFlagsWithDefaultCluster() error {
+	if !o.defaultCluster {
+		return nil
+	}
+
+	// If app is specified and the same as the workspace app, don't throw an error. 
+	// If app is not the same as the workspace app, either 
+	//   a) there is no WS app and it's specified erroneously, in which case we should error
+	//   b) there is a WS app and the flag has been set to a different app, in which case we should error.
+	// The app flag defaults to the WS app so there's an edge case where it's possible to specify
+	// `copilot task delete --app ws-app --default` and not error out, but this should be taken as
+	// specifying "default".  
+	if o.app != tryReadingAppName() {
+		return fmt.Errorf("cannot specify both `--app` and `--default`")
+	}
+
+	if o.env != "" {
+		return fmt.Errorf("cannot specify both `--env` and `--default`")
+	}
+
+	return nil
+}
+
+func (o *deleteTaskOpts) validateEnvName() error {
+	if o.app != "" {
+		if _, err := o.targetEnv(); err != nil {
+			return err
+		} 
+	} else {
+		return errNoAppInWorkspace
+	}
+	return nil
+}
+
+func (o *deleteTaskOpts) validateAppName() error {
+	if _, err := o.store.GetApplication(o.app); err != nil {
+		return fmt.Errorf("get application: %w", err)
+	}
+	return nil
+}
+
+func (o *deleteTaskOpts) targetEnv() (*config.Environment, error) {
+	env, err := o.store.GetEnvironment(o.app, o.env)
+	if err != nil {
+		return nil, fmt.Errorf("get environment %s config: %w", o.env, err)
+	}
+	return env, nil
+}
+
+// Ask prompts for missing information and fills in gaps.
+func (o *deleteTaskOpts) Ask() error {
+	// Ask task name
+	if err := o.askTaskName(); err != nil {
+		return err
+	}
+
+	if o.skipConfirmation {
+		return nil
+	}
+
+	// Confirm deletion
+	deletePrompt := fmt.Sprintf(fmtTaskDeleteDefaultConfirmPrompt, color.HighlightUserInput(o.name))
+	if o.env != "" {
+		deletePrompt = fmt.Sprintf(
+			fmtTaskDeleteFromEnvConfirmPrompt,
+			color.HighlightUserInput(o.name),
+			color.HighlightUserInput(o.env),
+		)
+	}
+
+	deleteConfirmed, err := o.prompt.Confirm(
+		deletePrompt,
+		taskDeleteConfirmHelp)
+
+	if err != nil {
+		return fmt.Errorf("svc delete confirmation prompt: %w", err)
+	}
+	if !deleteConfirmed {
+		return errSvcDeleteCancelled
+	}
+	return nil
+}
+
+func (o *deleteTaskOpts) askTaskName() error {
+	return nil
+}
+
+// Execute attempts to delete a task based on best-effort. If it finds an application
+// buildSvcDeleteCmd builds the command to delete application(s).
+func buildTaskDeleteCmd() *cobra.Command {
+	vars := deleteTaskVars{}
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Short: "Deletes a one-off task from an application or default cluster.",
+		Example: `
+  Delete the "test" task from the default cluster.
+  /code $ copilot task delete --name test --default
+
+  Delete the "test" task from the prod environment.
+  /code $ copilot task delete --name test --env prod
+
+  Delete the "test" task without confirmation prompt.
+  /code $ copilot task delete --name test --yes`,
+		RunE: runCmdE(func(cmd *cobra.Command, args []string) error {
+			opts, err := newDeleteSvcOpts(vars)
+			if err != nil {
+				return err
+			}
+			if err := opts.Validate(); err != nil {
+				return err
+			}
+			if err := opts.Ask(); err != nil {
+				return err
+			}
+			if err := opts.Execute(); err != nil {
+				return err
+			}
+
+			log.Infoln("Recommended follow-up actions:")
+			for _, followup := range opts.RecommendedActions() {
+				log.Infof("- %s\n", followup)
+			}
+			return nil
+		}),
+	}
+
+	cmd.Flags().StringVarP(&vars.app, appFlag, appFlagShort, tryReadingAppName(), appFlagDescription)
+	cmd.Flags().StringVarP(&vars.name, nameFlag, nameFlagShort, "", svcFlagDescription)
+	cmd.Flags().StringVarP(&vars.env, envFlag, envFlagShort, "", envFlagDescription)
+	cmd.Flags().BoolVar(&vars.skipConfirmation, yesFlag, false, yesFlagDescription)
+	cmd.Flags().BoolVar(&vars.)
+	return cmd
+}
