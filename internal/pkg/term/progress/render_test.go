fdf600b1f9feaeb8ed25ad2ac3a7c407fd9cfbb4
Efe Karakus <karakuse@amazon.com>
2020-12-24 10:29:11 -0800

    chore(progress): add Render function to periodically print a Renderer (#1786)
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

--- internal/pkg/term/progress/render_test.go
+++ internal/pkg/term/progress/render_test.go
@@ -4,11 +4,14 @@
 package progress
 
 import (
+	"context"
 	"errors"
 	"io"
 	"strings"
 	"testing"
+	"time"
 
+	"github.com/aws/copilot-cli/internal/pkg/term/cursor"
 	"github.com/stretchr/testify/require"
 )
 
@@ -21,8 +24,40 @@ func (m *mockRenderer) Render(out io.Writer) (int, error) {
 	if m.err != nil {
 		return 0, m.err
 	}
-	out.Write([]byte(m.content + "\n"))
+	out.Write([]byte(m.content))
 	return 1, nil
+}
+
+func TestRender(t *testing.T) {
+	// GIVEN
+	ctx, cancel := context.WithTimeout(context.Background(), 350*time.Millisecond)
+	defer cancel()
+	actual := new(strings.Builder)
+	r := &mockRenderer{content: "hi\n"}
+
+	// WHEN
+	err := Render(ctx, actual, r)
+
+	// THEN
+	require.NoError(t, err)
+
+	// We should be doing the following operations in order:
+	// 1. Hide the cursor.
+	// 2. Write "hi\n" and move the cursor up (Repeated x3 times)
+	// 3. The <-ctx.Done() is called so we should write one last time "hi\n" and the cursor should be shown.
+	wanted := new(strings.Builder)
+	c := cursor.NewWithWriter(wanted)
+	c.Hide()
+	wanted.WriteString("hi\n")
+	c.Up(2)
+	wanted.WriteString("hi\n")
+	c.Up(2)
+	wanted.WriteString("hi\n")
+	c.Up(2)
+	wanted.WriteString("hi\n")
+	c.Show()
+
+	require.Equal(t, wanted.String(), actual.String(), "expected the content printed to match")
 }
 
 func TestAllOrNothingRenderer(t *testing.T) {
@@ -33,10 +68,10 @@ func TestAllOrNothingRenderer(t *testing.T) {
 
 		// WHEN
 		r.Partial(&mockRenderer{
-			content: "hello",
+			content: "hello\n",
 		})
 		r.Partial(&mockRenderer{
-			content: "world",
+			content: "world\n",
 		})
 		numLines, err := r.Render(buf)
 
@@ -53,13 +88,13 @@ func TestAllOrNothingRenderer(t *testing.T) {
 
 		// WHEN
 		r.Partial(&mockRenderer{
-			content: "hello",
+			content: "hello\n",
 		})
 		r.Partial(&mockRenderer{
 			err: wantedErr,
 		})
 		r.Partial(&mockRenderer{
-			content: "world",
+			content: "world\n",
 		})
 		numLines, err := r.Render(buf)
 
