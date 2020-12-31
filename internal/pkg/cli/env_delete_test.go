9df635e4b3796281b22e2fa900853b8e579936c1
Efe Karakus <efekarakus@gmail.com>
2020-12-31 13:39:00 -0800

    chore(cli): Unmarshal template in "env upgrade" to check if roles are retained
    Previously, we would check if the following string was included in the
    template to see if the EnvManagerRole or ExecRole were retained:
    ```yaml
    EnvManagerRole:
        DeletionPolicy: Retain
    ```
    Once, we added a comment under the EnvManagerRole:
    ```yaml
    EnvManagerRole:
        # An IAM role to manage your environments
        DeletionPolicy: Retain`
    ```
    The parsing broke and the CLI thought that the resources were not
    retained.
    
    Now, we're using the yaml.Unmarshal library to see if a "DeletionPolicy"
    node is present under the roles.

--- internal/pkg/cli/env_delete_test.go
+++ internal/pkg/cli/env_delete_test.go
@@ -216,17 +216,35 @@ func TestDeleteEnvOpts_Execute(t *testing.T) {
 
 				deployer := mocks.NewMockenvironmentDeployer(ctrl)
 				deployer.EXPECT().EnvironmentTemplate(gomock.Any(), gomock.Any()).Return(`
+Resources:
+  EnableLongARNFormatAction:
+    Type: Custom::EnableLongARNFormatFunction
+    DependsOn:
+    - EnableLongARNFormatFunction
+    Properties:
+    ServiceToken: !GetAtt EnableLongARNFormatFunction.Arn
   CloudformationExecutionRole:
+    Type: AWS::IAM::Role
   EnvironmentManagerRole:
+    Type: AWS::IAM::Role
 `, nil)
 				deployer.EXPECT().UpdateEnvironmentTemplate(
 					"phonetool",
 					"test",
 					`
+Resources:
+  EnableLongARNFormatAction:
+    Type: Custom::EnableLongARNFormatFunction
+    DependsOn:
+    - EnableLongARNFormatFunction
+    Properties:
+    ServiceToken: !GetAtt EnableLongARNFormatFunction.Arn
   CloudformationExecutionRole:
     DeletionPolicy: Retain
+    Type: AWS::IAM::Role
   EnvironmentManagerRole:
     DeletionPolicy: Retain
+    Type: AWS::IAM::Role
 `, "arn").Return(errors.New("some error"))
 
 				prog.EXPECT().Stop(log.Serror("Failed to delete environment test from application phonetool.\n"))
@@ -261,6 +279,7 @@ func TestDeleteEnvOpts_Execute(t *testing.T) {
   CloudformationExecutionRole:
     DeletionPolicy: Retain
   EnvironmentManagerRole:
+    # An IAM Role to manage resources in your environment
     DeletionPolicy: Retain`, nil)
 				deployer.EXPECT().DeleteEnvironment(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("some error"))
 
@@ -295,6 +314,7 @@ func TestDeleteEnvOpts_Execute(t *testing.T) {
   CloudformationExecutionRole:
     DeletionPolicy: Retain
   EnvironmentManagerRole:
+    # An IAM Role to manage resources in your environment
     DeletionPolicy: Retain`, nil)
 				deployer.EXPECT().DeleteEnvironment(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
 
@@ -338,6 +358,7 @@ func TestDeleteEnvOpts_Execute(t *testing.T) {
   CloudformationExecutionRole:
     DeletionPolicy: Retain
   EnvironmentManagerRole:
+    # An IAM Role to manage resources in your environment
     DeletionPolicy: Retain`, nil)
 				deployer.EXPECT().DeleteEnvironment("phonetool", "test", "execARN").Return(nil)
 
