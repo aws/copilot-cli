ddc4a134ef3aa491f33ddd91c8ca24f9643003ee
Austin Ely <austiely@amazon.com>
2021-01-06 18:14:23 +0000

    test(stack): fix failing unit test due to variable extraction

--- internal/pkg/deploy/cloudformation/stack/task_test.go
+++ internal/pkg/deploy/cloudformation/stack/task_test.go
@@ -6,7 +6,6 @@ package stack
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/aws/copilot-cli/internal/pkg/deploy"
@@ -137,7 +136,7 @@ func TestTaskStackConfig_StackName(t *testing.T) {
 		CreateTaskResourcesInput: &taskInput,
 	}
 	got := task.StackName()
-	require.Equal(t, got, fmt.Sprintf(fmtTaskStackName, testTaskName))
+	require.Equal(t, got, TaskStackPrefix + testTaskName)
 }
 
 func TestTaskStackConfig_Tags(t *testing.T) {
