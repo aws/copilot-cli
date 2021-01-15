c3d9415342f5e8fda9dbbd427b11ddd9cd8f8120
Janice <jwh@amazon.com>
2021-01-14 16:13:39 -0800

    fix: delete commented out code

--- internal/pkg/deploy/cloudformation/cc_pipeline_integration_test.go
+++ internal/pkg/deploy/cloudformation/cc_pipeline_integration_test.go
@@ -28,11 +28,8 @@ func TestCCPipelineCreation(t *testing.T) {
 	appCallerInfo, err := appId.Get()
 	require.NoError(t, err)
 	appDeployer := cloudformation.New(appSess)
-	//sm := secretsmanager.New(appSess)
-	//secretId := "testGitHubSecret" + randStringBytes(10)
 
 	t.Run("creates a cross-region pipeline in a region with no environment", func(t *testing.T) {
-		//createMockSecret(t, sm, secretId)
 		appCfClient := awsCF.New(appSess)
 
 		app := config.Application{
@@ -118,8 +115,6 @@ func TestCCPipelineCreation(t *testing.T) {
 				StackName: aws.String(envStackName),
 			})
 			require.NoError(t, err)
-
-			//deleteMockSecretImmediately(t, sm, secretId)
 		}()
 
 		// Given both the application stack and env we are deploying to do not
@@ -185,80 +180,3 @@ func TestCCPipelineCreation(t *testing.T) {
 		assertStackExists(t, appCfClient, pipelineStackName)
 	})
 }
-
-//func createMockSecret(t *testing.T, sm secretsmanageriface.SecretsManagerAPI, secretId string) {
-//	_, err := sm.GetSecretValue(&secretsmanager.GetSecretValueInput{
-//		SecretId: aws.String(secretId),
-//	})
-//	if err != nil {
-//		if awsErr, ok := err.(awserr.Error); ok {
-//			switch awsErr.Code() {
-//			case secretsmanager.ErrCodeResourceNotFoundException:
-//				_, err = sm.CreateSecret(&secretsmanager.CreateSecretInput{
-//					Name:         aws.String(secretId),
-//					SecretString: aws.String("dontCare"),
-//				})
-//				require.NoError(t, err, "CreateSecret should not return an error")
-//				return
-//			default:
-//				require.Fail(t, "GetSecretValue failed: %w", awsErr)
-//			}
-//		}
-//		require.Fail(t, "GetSecretValue failed: %w", err)
-//	}
-//}
-//
-//func deleteMockSecretImmediately(t *testing.T, sm secretsmanageriface.SecretsManagerAPI, secretId string) {
-//	_, err := sm.DeleteSecret(&secretsmanager.DeleteSecretInput{
-//		SecretId:                   aws.String(secretId),
-//		ForceDeleteWithoutRecovery: aws.Bool(true),
-//	})
-//	require.NoError(t, err, "DeleteSecret should not return an error")
-//}
-//
-//func assertStackDoesNotExist(t *testing.T, cfClient *awsCF.CloudFormation, stackName string) {
-//	_, err := cfClient.DescribeStacks(&awsCF.DescribeStacksInput{
-//		StackName: aws.String(stackName),
-//	})
-//	require.Error(t, err, "DescribeStacks should return an error because the stack does not exist")
-//	awsErr, ok := err.(awserr.Error)
-//	require.True(t, ok, "the returned error should be an awserr")
-//	require.Equal(t, awsErr.Code(), "ValidationError")
-//	require.Contains(t, awsErr.Message(), "does not exist", "the returned error should indicate that the stack does not exist")
-//}
-//
-//func assertStackExists(t *testing.T, cfClient *awsCF.CloudFormation, stackName string) *awsCF.DescribeStacksOutput {
-//	resp, err := cfClient.DescribeStacks(&awsCF.DescribeStacksInput{
-//		StackName: aws.String(stackName),
-//	})
-//	require.NoError(t, err)
-//	require.True(t, len(resp.Stacks) == 1, "Stack %s should have been deployed.", stackName)
-//	return resp
-//}
-//
-//func regionalResourcesToArtifactBuckets(t *testing.T, resources []*stack.AppRegionalResources) []deploy.ArtifactBucket {
-//	buckets := make([]deploy.ArtifactBucket, 0, len(resources))
-//	for _, res := range resources {
-//		require.True(t, res.S3Bucket != "", "S3 Bucket shouldn't be blank")
-//		require.True(t, res.KMSKeyARN != "", "KMSKey ARN shouldn't be blank")
-//		buckets = append(buckets, deploy.ArtifactBucket{
-//			BucketName: res.S3Bucket,
-//			KeyArn:     res.KMSKeyARN,
-//		})
-//	}
-//	return buckets
-//}
-//
-//func findUnusedRegion(regionPrefix string, usedRegions ...string) (*endpoints.Region, error) {
-//	usedRegionsMap := make(map[string]bool)
-//	for _, usedRegion := range usedRegions {
-//		usedRegionsMap[usedRegion] = true
-//	}
-//	availableRegions := endpoints.AwsPartition().Services()[endpoints.CloudformationServiceID].Regions()
-//	for _, r := range availableRegions {
-//		if _, exists := usedRegionsMap[r.ID()]; !exists && strings.HasPrefix(r.ID(), regionPrefix) {
-//			return &r, nil
-//		}
-//	}
-//	return nil, errors.New("all regions are used")
-//}
