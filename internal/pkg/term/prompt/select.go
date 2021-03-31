cdb0119648b4b0983bf4d0df7d8cce01727ed2d2
Wanxian Yang <wanxiay@amazon.com>
2021-03-30 20:11:35 -0500

    update comment

--- internal/pkg/term/prompt/select.go
+++ internal/pkg/term/prompt/select.go
@@ -26,7 +26,7 @@ const (
 // '\x1b[2m'        (for Faint output)
 // '\x1b[1;31m'     (for bold, red output)
 const (
-	sgrStart = "\x1b\\["            // SGR sequences start with "\[".
+	sgrStart = "\x1b\\["            // SGR sequences start with "ESC[".
 	sgrEnd = "m"                    // SGR sequences end with "m".
 	sgrParameter = "[0-9]{1,2,3}"   // SGR parameter values range from 0 to 107.
 )
