ac47e631a094489bf10ec91fb56b08f1e78f0254
Efe Karakus <efekarakus@gmail.com>
2021-04-14 14:36:30 -0700

    chore: fix integ tests for pipelines

--- internal/pkg/deploy/cloudformation/stack/gh_pipeline_integration_test.go
+++ internal/pkg/deploy/cloudformation/stack/gh_pipeline_integration_test.go
@@ -28,6 +28,7 @@ func TestGHPipeline_Template(t *testing.T) {
 			RepositoryURL: "https://github.com/aws/phonetool",
 			Branch:        "mainline",
 		},
+		Build: deploy.PipelineBuildFromManifest(nil),
 		Stages: []deploy.PipelineStage{
 			{
 				AssociatedEnvironment: &deploy.AssociatedEnvironment{
