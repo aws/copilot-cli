aws/copilot-cli

commit b390ab2f5a1d39ccbb7f07b34a44a8fd3a787f15

chore(cli): include app version in app show (#2209)
<!-- Provide summary of changes -->
This PR adds the application version to the app show human string as below
```
About

  Name              my-app
  Version           v0.0.0 (latest: v1.0.1)
  URI               example.com

Environments

  Name              AccountID           Region
  ----              ---------           ------
  test              123456789           us-west-2
  prod              123456789           us-west-1

Services

  Name              Type
  ----              ----
  my-svc            lb-web-svc

Pipelines

  Name
  ----
  pipeline1
  pipeline2
```
Also, it fixes a bug within the CFN pkg that doesn't include the app name when retrieving info from the last deployed app stackset.
<!-- Issue number, if available. E.g. "Fixes #31", "Addresses #42, 77" -->

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/app_show.go
@@ -32,11 +32,11 @@ type showAppVars struct {
 type showAppOpts struct {
 	showAppVars
 
-	prompt      prompter
-	store       store
-	w           io.Writer
-	sel         appSelector
-	pipelineSvc pipelineGetter
+	store         store
+	w             io.Writer
+	sel           appSelector
+	pipelineSvc   pipelineGetter
+	versionGetter versionGetter
 }
 
 func newShowAppOpts(vars showAppVars) (*showAppOpts, error) {
@@ -44,19 +44,21 @@ func newShowAppOpts(vars showAppVars) (*showAppOpts, error) {
 	if err != nil {
 		return nil, fmt.Errorf("new config store: %w", err)
 	}
-
 	defaultSession, err := sessions.NewProvider().Default()
 	if err != nil {
 		return nil, fmt.Errorf("default session: %w", err)
 	}
-	prompter := prompt.New()
+	d, err := describe.NewAppDescriber(vars.name)
+	if err != nil {
+		return nil, fmt.Errorf("new app describer for application %s: %v", vars.name, err)
+	}
 	return &showAppOpts{
-		showAppVars: vars,
-		store:       store,
-		w:           log.OutputWriter,
-		prompt:      prompter,
-		sel:         selector.NewSelect(prompter, store),
-		pipelineSvc: codepipeline.New(defaultSession),
+		showAppVars:   vars,
+		store:         store,
+		w:             log.OutputWriter,
+		sel:           selector.NewSelect(prompt.New(), store),
+		pipelineSvc:   codepipeline.New(defaultSession),
+		versionGetter: d,
 	}, nil
 }
 
@@ -137,8 +139,13 @@ func (o *showAppOpts) description() (*describe.App, error) {
 			Type: svc.Type,
 		})
 	}
+	version, err := o.versionGetter.Version()
+	if err != nil {
+		return nil, fmt.Errorf("get version for application %s: %w", o.name, err)
+	}
 	return &describe.App{
 		Name:      app.Name,
+		Version:   version,
 		URI:       app.Domain,
 		Envs:      trimmedEnvs,
 		Services:  trimmedSvcs,

internal/pkg/cli/app_show_test.go
@@ -12,15 +12,16 @@ import (
 	"github.com/aws/copilot-cli/internal/pkg/aws/codepipeline"
 	"github.com/aws/copilot-cli/internal/pkg/cli/mocks"
 	"github.com/aws/copilot-cli/internal/pkg/config"
+	"github.com/aws/copilot-cli/internal/pkg/deploy"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
 type showAppMocks struct {
-	storeSvc    *mocks.Mockstore
-	prompt      *mocks.Mockprompter
-	sel         *mocks.MockappSelector
-	pipelineSvc *mocks.MockpipelineGetter
+	storeSvc      *mocks.Mockstore
+	sel           *mocks.MockappSelector
+	pipelineSvc   *mocks.MockpipelineGetter
+	versionGetter *mocks.MockversionGetter
 }
 
 func TestShowAppOpts_Validate(t *testing.T) {
@@ -58,11 +59,9 @@ func TestShowAppOpts_Validate(t *testing.T) {
 			defer ctrl.Finish()
 
 			mockStoreReader := mocks.NewMockstore(ctrl)
-			mockPrompter := mocks.NewMockprompter(ctrl)
 
 			mocks := showAppMocks{
 				storeSvc: mockStoreReader,
-				prompt:   mockPrompter,
 			}
 			tc.setupMocks(mocks)
 
@@ -70,8 +69,7 @@ func TestShowAppOpts_Validate(t *testing.T) {
 				showAppVars: showAppVars{
 					name: tc.inAppName,
 				},
-				store:  mockStoreReader,
-				prompt: mockPrompter,
+				store: mockStoreReader,
 			}
 
 			// WHEN
@@ -202,9 +200,10 @@ func TestShowAppOpts_Execute(t *testing.T) {
 						{Name: "pipeline1"},
 						{Name: "pipeline2"},
 					}, nil)
+				m.versionGetter.EXPECT().Version().Return("v0.0.0", nil)
 			},
 
-			wantedContent: "{\"name\":\"my-app\",\"uri\":\"example.com\",\"environments\":[{\"app\":\"\",\"name\":\"test\",\"region\":\"us-west-2\",\"accountID\":\"123456789\",\"prod\":false,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"},{\"app\":\"\",\"name\":\"prod\",\"region\":\"us-west-1\",\"accountID\":\"123456789\",\"prod\":true,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"}],\"services\":[{\"app\":\"\",\"name\":\"my-svc\",\"type\":\"lb-web-svc\"}],\"pipelines\":[{\"name\":\"pipeline1\",\"region\":\"\",\"accountId\":\"\",\"stages\":null,\"createdAt\":\"0001-01-01T00:00:00Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"},{\"name\":\"pipeline2\",\"region\":\"\",\"accountId\":\"\",\"stages\":null,\"createdAt\":\"0001-01-01T00:00:00Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"}]}\n",
+			wantedContent: "{\"name\":\"my-app\",\"version\":\"v0.0.0\",\"uri\":\"example.com\",\"environments\":[{\"app\":\"\",\"name\":\"test\",\"region\":\"us-west-2\",\"accountID\":\"123456789\",\"prod\":false,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"},{\"app\":\"\",\"name\":\"prod\",\"region\":\"us-west-1\",\"accountID\":\"123456789\",\"prod\":true,\"registryURL\":\"\",\"executionRoleARN\":\"\",\"managerRoleARN\":\"\"}],\"services\":[{\"app\":\"\",\"name\":\"my-svc\",\"type\":\"lb-web-svc\"}],\"pipelines\":[{\"name\":\"pipeline1\",\"region\":\"\",\"accountId\":\"\",\"stages\":null,\"createdAt\":\"0001-01-01T00:00:00Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"},{\"name\":\"pipeline2\",\"region\":\"\",\"accountId\":\"\",\"stages\":null,\"createdAt\":\"0001-01-01T00:00:00Z\",\"updatedAt\":\"0001-01-01T00:00:00Z\"}]}\n",
 		},
 		"correctly shows human output": {
 			setupMocks: func(m showAppMocks) {
@@ -236,11 +235,73 @@ func TestShowAppOpts_Execute(t *testing.T) {
 						{Name: "pipeline1"},
 						{Name: "pipeline2"},
 					}, nil)
+				m.versionGetter.EXPECT().Version().Return("v0.0.0", nil)
 			},
 
 			wantedContent: `About
 
   Name              my-app
+  Version           v0.0.0 (latest available: v1.0.1)
+  URI               example.com
+
+Environments
+
+  Name              AccountID           Region
+  ----              ---------           ------
+  test              123456789           us-west-2
+  prod              123456789           us-west-1
+
+Services
+
+  Name              Type
+  ----              ----
+  my-svc            lb-web-svc
+
+Pipelines
+
+  Name
+  ----
+  pipeline1
+  pipeline2
+`,
+		},
+		"correctly shows human output with latest version": {
+			setupMocks: func(m showAppMocks) {
+				m.storeSvc.EXPECT().GetApplication("my-app").Return(&config.Application{
+					Name:   "my-app",
+					Domain: "example.com",
+				}, nil)
+				m.storeSvc.EXPECT().ListServices("my-app").Return([]*config.Workload{
+					{
+						Name: "my-svc",
+						Type: "lb-web-svc",
+					},
+				}, nil)
+				m.storeSvc.EXPECT().ListEnvironments("my-app").Return([]*config.Environment{
+					{
+						Name:      "test",
+						Region:    "us-west-2",
+						AccountID: "123456789",
+					},
+					{
+						Name:      "prod",
+						AccountID: "123456789",
+						Region:    "us-west-1",
+					},
+				}, nil)
+				m.pipelineSvc.EXPECT().
+					GetPipelinesByTags(gomock.Eq(map[string]string{"copilot-application": "my-app"})).
+					Return([]*codepipeline.Pipeline{
+						{Name: "pipeline1"},
+						{Name: "pipeline2"},
+					}, nil)
+				m.versionGetter.EXPECT().Version().Return(deploy.LatestAppTemplateVersion, nil)
+			},
+
+			wantedContent: `About
+
+  Name              my-app
+  Version           v1.0.1 
   URI               example.com
 
 Environments
@@ -341,6 +402,42 @@ Pipelines
 			},
 			wantedError: fmt.Errorf("list pipelines in application %s: %w", "my-app", testError),
 		},
+		"returns error if fail to get app version": {
+			shouldOutputJSON: false,
+
+			setupMocks: func(m showAppMocks) {
+				m.storeSvc.EXPECT().GetApplication("my-app").Return(&config.Application{
+					Name:   "my-app",
+					Domain: "example.com",
+				}, nil)
+				m.storeSvc.EXPECT().ListEnvironments("my-app").Return([]*config.Environment{
+					{
+						Name:      "test",
+						Region:    "us-west-2",
+						AccountID: "123456789",
+					},
+					{
+						Name:      "prod",
+						AccountID: "123456789",
+						Region:    "us-west-1",
+					},
+				}, nil)
+				m.storeSvc.EXPECT().ListServices("my-app").Return([]*config.Workload{
+					{
+						Name: "my-svc",
+						Type: "lb-web-svc",
+					},
+				}, nil)
+				m.pipelineSvc.EXPECT().
+					GetPipelinesByTags(gomock.Eq(map[string]string{"copilot-application": "my-app"})).
+					Return([]*codepipeline.Pipeline{
+						{Name: "pipeline1"},
+						{Name: "pipeline2"},
+					}, nil)
+				m.versionGetter.EXPECT().Version().Return("", testError)
+			},
+			wantedError: fmt.Errorf("get version for application %s: %w", "my-app", testError),
+		},
 	}
 
 	for name, tc := range testCases {
@@ -351,10 +448,12 @@ Pipelines
 			b := &bytes.Buffer{}
 			mockStoreReader := mocks.NewMockstore(ctrl)
 			mockPLSvc := mocks.NewMockpipelineGetter(ctrl)
+			mockVersionGetter := mocks.NewMockversionGetter(ctrl)
 
 			mocks := showAppMocks{
-				storeSvc:    mockStoreReader,
-				pipelineSvc: mockPLSvc,
+				storeSvc:      mockStoreReader,
+				pipelineSvc:   mockPLSvc,
+				versionGetter: mockVersionGetter,
 			}
 			tc.setupMocks(mocks)
 
@@ -363,9 +462,10 @@ Pipelines
 					shouldOutputJSON: tc.shouldOutputJSON,
 					name:             testAppName,
 				},
-				store:       mockStoreReader,
-				w:           b,
-				pipelineSvc: mockPLSvc,
+				store:         mockStoreReader,
+				w:             b,
+				pipelineSvc:   mockPLSvc,
+				versionGetter: mockVersionGetter,
 			}
 
 			// WHEN

internal/pkg/cli/app_upgrade.go
@@ -15,6 +15,8 @@ import (
 	"github.com/aws/copilot-cli/internal/pkg/term/color"
 	"github.com/aws/copilot-cli/internal/pkg/term/log"
 	termprogress "github.com/aws/copilot-cli/internal/pkg/term/progress"
+	"github.com/aws/copilot-cli/internal/pkg/term/prompt"
+	"github.com/aws/copilot-cli/internal/pkg/term/selector"
 	"github.com/spf13/cobra"
 	"golang.org/x/mod/semver"
 )
@@ -23,6 +25,9 @@ const (
 	fmtAppUpgradeStart    = "Upgrading application %s from version %s to version %s."
 	fmtAppUpgradeFailed   = "Failed to upgrade application %s's template to version %s.\n"
 	fmtAppUpgradeComplete = "Upgraded application %s's template to version %s.\n"
+
+	appUpgradeNamePrompt     = "Which application would you like to upgrade?"
+	appUpgradeNameHelpPrompt = "An application is a collection of related services."
 )
 
 // appUpgradeVars holds flag values.
@@ -38,6 +43,7 @@ type appUpgradeOpts struct {
 	store         store
 	prog          progress
 	versionGetter versionGetter
+	sel           appSelector
 	identity      identityService
 	upgrader      appUpgrader
 }
@@ -60,9 +66,29 @@ func newAppUpgradeOpts(vars appUpgradeVars) (*appUpgradeOpts, error) {
 		store:          store,
 		identity:       identity.New(sess),
 		prog:           termprogress.NewSpinner(log.DiagnosticWriter),
+		sel:            selector.NewSelect(prompt.New(), store),
 		versionGetter:  d,
 		upgrader:       cloudformation.New(sess),
 	}, nil
+}
+
+// Validate returns an error if the values provided by the user are invalid.
+func (o *appUpgradeOpts) Validate() error {
+	if o.name != "" {
+		_, err := o.store.GetApplication(o.name)
+		if err != nil {
+			return fmt.Errorf("get application %s: %w", o.name, err)
+		}
+	}
+	return nil
+}
+
+// Ask asks for fields that are required but not passed in.
+func (o *appUpgradeOpts) Ask() error {
+	if err := o.askName(); err != nil {
+		return err
+	}
+	return nil
 }
 
 // Execute updates the cloudformation stack as well as the stackset of an application to the latest version.
@@ -91,6 +117,18 @@ func (o *appUpgradeOpts) Execute() error {
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (o *appUpgradeOpts) askName() error {
+	if o.name != "" {
+		return nil
+	}
+	name, err := o.sel.Application(appUpgradeNamePrompt, appUpgradeNameHelpPrompt)
+	if err != nil {
+		return fmt.Errorf("select application: %w", err)
+	}
+	o.name = name
 	return nil
 }
 
@@ -130,18 +168,15 @@ func (o *appUpgradeOpts) upgradeApplication(app *config.Application, fromVersion
 	return nil
 }
 
-// RecommendedActions is a no-op for this command.
-func (o *appUpgradeOpts) RecommendedActions() []string {
-	return nil
-}
-
 // buildAppUpgradeCmd builds the command to update an application to the latest version.
 func buildAppUpgradeCmd() *cobra.Command {
 	vars := appUpgradeVars{}
 	cmd := &cobra.Command{
-		Use:    "upgrade",
-		Short:  "Upgrades the template of an application to the latest version.",
-		Hidden: true,
+		Use:   "upgrade",
+		Short: "Upgrades the template of an application to the latest version.",
+		Example: `
+    Upgrade the application "my-app" to the latest version
+    /code $ copilot app upgrade -n my-app`,
 		RunE: runCmdE(func(cmd *cobra.Command, args []string) error {
 			opts, err := newAppUpgradeOpts(vars)
 			if err != nil {
@@ -150,6 +185,6 @@ func buildAppUpgradeCmd() *cobra.Command {
 			return opts.Execute()
 		}),
 	}
-	cmd.Flags().StringVarP(&vars.name, nameFlag, nameFlagShort, "", appFlagDescription)
+	cmd.Flags().StringVarP(&vars.name, nameFlag, nameFlagShort, tryReadingAppName(), appFlagDescription)
 	return cmd
 }

internal/pkg/cli/app_upgrade_test.go
@@ -16,6 +16,142 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+type appUpgradeMocks struct {
+	storeSvc *mocks.Mockstore
+	sel      *mocks.MockappSelector
+}
+
+func TestAppUpgradeOpts_Validate(t *testing.T) {
+	testError := errors.New("some error")
+	testCases := map[string]struct {
+		inAppName  string
+		setupMocks func(mocks appUpgradeMocks)
+
+		wantedError error
+	}{
+		"valid app name": {
+			inAppName: "my-app",
+
+			setupMocks: func(m appUpgradeMocks) {
+				m.storeSvc.EXPECT().GetApplication("my-app").Return(&config.Application{
+					Name: "my-app",
+				}, nil)
+			},
+			wantedError: nil,
+		},
+		"invalid app name": {
+			inAppName: "my-app",
+
+			setupMocks: func(m appUpgradeMocks) {
+				m.storeSvc.EXPECT().GetApplication("my-app").Return(nil, testError)
+			},
+
+			wantedError: fmt.Errorf("get application %s: %w", "my-app", testError),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockStoreReader := mocks.NewMockstore(ctrl)
+
+			mocks := appUpgradeMocks{
+				storeSvc: mockStoreReader,
+			}
+			tc.setupMocks(mocks)
+
+			opts := &appUpgradeOpts{
+				appUpgradeVars: appUpgradeVars{
+					name: tc.inAppName,
+				},
+				store: mockStoreReader,
+			}
+
+			// WHEN
+			err := opts.Validate()
+
+			// THEN
+			if tc.wantedError != nil {
+				require.EqualError(t, err, tc.wantedError.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestAppUpgradeOpts_Ask(t *testing.T) {
+	testError := errors.New("some error")
+	testCases := map[string]struct {
+		inApp string
+
+		setupMocks func(mocks appUpgradeMocks)
+
+		wantedApp   string
+		wantedError error
+	}{
+		"with all flags": {
+			inApp: "my-app",
+
+			setupMocks: func(m appUpgradeMocks) {},
+
+			wantedApp:   "my-app",
+			wantedError: nil,
+		},
+		"prompt for all input": {
+			inApp: "",
+
+			setupMocks: func(m appUpgradeMocks) {
+				m.sel.EXPECT().Application(appUpgradeNamePrompt, appUpgradeNameHelpPrompt).Return("my-app", nil)
+			},
+			wantedApp:   "my-app",
+			wantedError: nil,
+		},
+		"returns error if failed to select application": {
+			inApp: "",
+
+			setupMocks: func(m appUpgradeMocks) {
+				m.sel.EXPECT().Application(gomock.Any(), gomock.Any()).Return("", testError)
+			},
+
+			wantedError: fmt.Errorf("select application: %w", testError),
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mocks := appUpgradeMocks{
+				sel: mocks.NewMockappSelector(ctrl),
+			}
+			tc.setupMocks(mocks)
+
+			opts := &appUpgradeOpts{
+				appUpgradeVars: appUpgradeVars{
+					name: tc.inApp,
+				},
+				sel: mocks.sel,
+			}
+
+			// WHEN
+			err := opts.Ask()
+
+			// THEN
+			if tc.wantedError != nil {
+				require.EqualError(t, err, tc.wantedError.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.wantedApp, opts.name, "expected app names to match")
+
+			}
+		})
+	}
+}
+
 func TestAppUpgradeOpts_Execute(t *testing.T) {
 	testCases := map[string]struct {
 		given     func(ctrl *gomock.Controller) *appUpgradeOpts

internal/pkg/deploy/cloudformation/app.go
@@ -477,6 +477,7 @@ func (cf CloudFormation) getLastDeployedAppConfig(appConfig *stack.AppStackConfi
 	if err != nil {
 		return nil, fmt.Errorf("parse previous deployed stackset %w", err)
 	}
+	previouslyDeployedConfig.App = appConfig.Name
 	return previouslyDeployedConfig, nil
 }
 

internal/pkg/describe/app.go
@@ -24,6 +24,7 @@ import (
 // App contains serialized parameters for an application.
 type App struct {
 	Name      string                   `json:"name"`
+	Version   string                   `json:"version"`
 	URI       string                   `json:"uri"`
 	Envs      []*config.Environment    `json:"environments"`
 	Services  []*config.Workload       `json:"services"`
@@ -46,6 +47,11 @@ func (a *App) HumanString() string {
 	fmt.Fprint(writer, color.Bold.Sprint("About\n\n"))
 	writer.Flush()
 	fmt.Fprintf(writer, "  %s\t%s\n", "Name", a.Name)
+	availableVersion := ""
+	if deploy.LatestAppTemplateVersion != a.Version {
+		availableVersion = color.Yellow.Sprintf("(latest available: %s)", deploy.LatestAppTemplateVersion)
+	}
+	fmt.Fprintf(writer, "  %s\t%s %s\n", "Version", a.Version, availableVersion)
 	fmt.Fprintf(writer, "  %s\t%s\n", "URI", a.URI)
 	fmt.Fprint(writer, color.Bold.Sprint("\nEnvironments\n\n"))
 	writer.Flush()

commit 355cf83571a53410b6df8f2d40471bcfa555472d

fix(cli): ensure expected experience for running commands out of workspace (#2269)
Checked all commands when running them out of the workspace. This PR
1. Fix the experience for `env delete` which throws not helpful error currently like
```
✘ select environment to delete: get environments for app  from metadata store: list environments: list environments for application : ValidationException: The parameter doesn't meet the parameter name requirements. The parameter name must begin with a forward slash "/". It can't be prefixed with \"aws\" or \"ssm\" (case-insensitive). It must use only letters, numbers, or the following symbols: . (period), - (hyphen), _ (underscore). Special characters are not allowed. All sub-paths, if specified, must use the forward slash symbol "/". Valid example: /get/parameters2-/by1./path0_.
	status code: 400, request id: 90fc142d-eaf6-412f-a219-dae8a21aa500
```
2. Fix `app show` experience which was broken by #2209.

<!-- Provide summary of changes -->

<!-- Issue number, if available. E.g. "Fixes #31", "Addresses #42, 77" -->

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

internal/pkg/cli/app_show.go
@@ -32,11 +32,11 @@ type showAppVars struct {
 type showAppOpts struct {
 	showAppVars
 
-	store         store
-	w             io.Writer
-	sel           appSelector
-	pipelineSvc   pipelineGetter
-	versionGetter versionGetter
+	store            store
+	w                io.Writer
+	sel              appSelector
+	pipelineSvc      pipelineGetter
+	newVersionGetter func(string) (versionGetter, error)
 }
 
 func newShowAppOpts(vars showAppVars) (*showAppOpts, error) {
@@ -48,17 +48,19 @@ func newShowAppOpts(vars showAppVars) (*showAppOpts, error) {
 	if err != nil {
 		return nil, fmt.Errorf("default session: %w", err)
 	}
-	d, err := describe.NewAppDescriber(vars.name)
-	if err != nil {
-		return nil, fmt.Errorf("new app describer for application %s: %v", vars.name, err)
-	}
 	return &showAppOpts{
-		showAppVars:   vars,
-		store:         store,
-		w:             log.OutputWriter,
-		sel:           selector.NewSelect(prompt.New(), store),
-		pipelineSvc:   codepipeline.New(defaultSession),
-		versionGetter: d,
+		showAppVars: vars,
+		store:       store,
+		w:           log.OutputWriter,
+		sel:         selector.NewSelect(prompt.New(), store),
+		pipelineSvc: codepipeline.New(defaultSession),
+		newVersionGetter: func(s string) (versionGetter, error) {
+			d, err := describe.NewAppDescriber(s)
+			if err != nil {
+				return d, fmt.Errorf("new app describer for application %s: %v", s, err)
+			}
+			return d, nil
+		},
 	}, nil
 }
 
@@ -139,7 +141,11 @@ func (o *showAppOpts) description() (*describe.App, error) {
 			Type: svc.Type,
 		})
 	}
-	version, err := o.versionGetter.Version()
+	versionGetter, err := o.newVersionGetter(o.name)
+	if err != nil {
+		return nil, err
+	}
+	version, err := versionGetter.Version()
 	if err != nil {
 		return nil, fmt.Errorf("get version for application %s: %w", o.name, err)
 	}

internal/pkg/cli/env_delete.go
@@ -16,6 +16,7 @@ import (
 	"github.com/aws/copilot-cli/internal/pkg/config"
 	"github.com/aws/copilot-cli/internal/pkg/deploy"
 	"github.com/aws/copilot-cli/internal/pkg/deploy/cloudformation"
+	"github.com/aws/copilot-cli/internal/pkg/term/color"
 	"github.com/aws/copilot-cli/internal/pkg/term/log"
 	termprogress "github.com/aws/copilot-cli/internal/pkg/term/progress"
 	"github.com/aws/copilot-cli/internal/pkg/term/prompt"
@@ -25,14 +26,19 @@ import (
 )
 
 const (
-	envDeleteNamePrompt = "Which environment would you like to delete?"
-	fmtDeleteEnvPrompt  = "Are you sure you want to delete environment %s from application %s?"
+	envDeleteAppNameHelpPrompt = "An environment will be deleted in the selected application."
+	envDeleteNamePrompt        = "Which environment would you like to delete?"
+	fmtDeleteEnvPrompt         = "Are you sure you want to delete environment %s from application %s?"
 )
 
 const (
 	fmtDeleteEnvStart    = "Deleting environment %s from application %s."
 	fmtDeleteEnvFailed   = "Failed to delete environment %s from application %s.\n"
 	fmtDeleteEnvComplete = "Deleted environment %s from application %s.\n"
+)
+
+var (
+	envDeleteAppNamePrompt = fmt.Sprintf("In which %s would you like to delete the environment?", color.Emphasize("application"))
 )
 
 var (
@@ -112,10 +118,12 @@ func (o *deleteEnvOpts) Validate() error {
 
 // Ask prompts for fields that are required but not passed in.
 func (o *deleteEnvOpts) Ask() error {
+	if err := o.askAppName(); err != nil {
+		return err
+	}
 	if err := o.askEnvName(); err != nil {
 		return err
 	}
-
 	if o.skipConfirmation {
 		return nil
 	}
@@ -174,6 +182,19 @@ func (o *deleteEnvOpts) validateEnvName() error {
 	if _, err := o.getEnvConfig(); err != nil {
 		return err
 	}
+	return nil
+}
+
+func (o *deleteEnvOpts) askAppName() error {
+	if o.appName != "" {
+		return nil
+	}
+
+	app, err := o.sel.Application(envDeleteAppNamePrompt, envDeleteAppNameHelpPrompt)
+	if err != nil {
+		return fmt.Errorf("ask for application: %w", err)
+	}
+	o.appName = app
 	return nil
 }
 

internal/pkg/cli/app_show_test.go
@@ -462,10 +462,12 @@ Pipelines
 					shouldOutputJSON: tc.shouldOutputJSON,
 					name:             testAppName,
 				},
-				store:         mockStoreReader,
-				w:             b,
-				pipelineSvc:   mockPLSvc,
-				versionGetter: mockVersionGetter,
+				store:       mockStoreReader,
+				w:           b,
+				pipelineSvc: mockPLSvc,
+				newVersionGetter: func(s string) (versionGetter, error) {
+					return mockVersionGetter, nil
+				},
 			}
 
 			// WHEN

internal/pkg/cli/env_delete_test.go
@@ -85,6 +85,7 @@ func TestDeleteEnvOpts_Ask(t *testing.T) {
 		testEnv = "test"
 	)
 	testCases := map[string]struct {
+		inAppName          string
 		inEnvName          string
 		inSkipConfirmation bool
 
@@ -97,6 +98,8 @@ func TestDeleteEnvOpts_Ask(t *testing.T) {
 			inSkipConfirmation: false,
 			mockDependencies: func(ctrl *gomock.Controller, o *deleteEnvOpts) {
 				mockSelector := mocks.NewMockconfigSelector(ctrl)
+				mockSelector.EXPECT().Application(envDeleteAppNamePrompt, envDeleteAppNameHelpPrompt, gomock.Any()).
+					Return(testApp, nil)
 				mockSelector.EXPECT().Environment(envDeleteNamePrompt, "", testApp).Return(testEnv, nil)
 
 				mockPrompter := mocks.NewMockprompter(ctrl)
@@ -107,8 +110,19 @@ func TestDeleteEnvOpts_Ask(t *testing.T) {
 			},
 			wantedEnvName: testEnv,
 		},
+		"error if fail to select applications": {
+			mockDependencies: func(ctrl *gomock.Controller, o *deleteEnvOpts) {
+				mockSelector := mocks.NewMockconfigSelector(ctrl)
+				mockSelector.EXPECT().Application(envDeleteAppNamePrompt, envDeleteAppNameHelpPrompt, gomock.Any()).
+					Return("", errors.New("some error"))
+
+				o.sel = mockSelector
+			},
+			wantedError: fmt.Errorf("ask for application: some error"),
+		},
 		"wraps error from prompting for confirmation": {
 			inSkipConfirmation: false,
+			inAppName:          testApp,
 			inEnvName:          testEnv,
 			mockDependencies: func(ctrl *gomock.Controller, o *deleteEnvOpts) {
 
@@ -130,7 +144,7 @@ func TestDeleteEnvOpts_Ask(t *testing.T) {
 			opts := &deleteEnvOpts{
 				deleteEnvVars: deleteEnvVars{
 					name:             tc.inEnvName,
-					appName:          testApp,
+					appName:          tc.inAppName,
 					skipConfirmation: tc.inSkipConfirmation,
 				},
 			}