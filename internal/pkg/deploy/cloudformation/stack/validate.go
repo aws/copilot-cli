c3e3f25837b165b4044b4c12b67b142f61f70089
Alexandra French <xartol@amazon.com>
2021-06-04 11:14:49 -0400

    adds dependency validation logic

--- internal/pkg/deploy/cloudformation/stack/validate.go
+++ internal/pkg/deploy/cloudformation/stack/validate.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/copilot-cli/internal/pkg/manifest"
+	"github.com/yourbasic/graph"
 )
 
 // Validation errors when rendering manifest into template.
@@ -29,6 +30,15 @@ var (
 	errInvalidUIDGIDConfig          = errors.New("must specify both UID and GID, or neither")
 	errInvalidEFSConfig             = errors.New("bad EFS configuration: cannot specify both bool and config")
 	errReservedUID                  = errors.New("UID must not be 0")
+	errCircularDependency           = errors.New("Bad dependency name: circular dependency present")
+	errInvalidContainer             = errors.New("Container dependency does not exist")
+	errInvalidDependsOnStatus       = errors.New("Container dependency status must be one of < start | complete | success >")
+	errEssentialContainerStatus     = errors.New("Essential containers dependencies can only have status 'start'")
+)
+
+var (
+	// Container dependency status options
+	DependsOnStatus = []string{"start", "complete", "success"}
 )
 
 // Validate that paths contain only an approved set of characters to guard against command injection.
@@ -98,6 +108,135 @@ func validateSidecarMountPoints(in []manifest.SidecarMountPoint) error {
 		}
 	}
 	return nil
+}
+
+func validateSidecarDependsOn(in manifest.SidecarConfig, sidecarName string, sidecars map[string]*manifest.SidecarConfig, manifestName string) error {
+	if in.DependsOn == nil {
+		return nil
+	}
+
+	for name, status := range in.DependsOn {
+		// don't let a sidecar depend on itself
+		if name == sidecarName {
+			return errCircularDependency
+		}
+		// status must be one of < start | complete | success >
+		if !isValidStatus(status) {
+			return errInvalidDependsOnStatus
+		}
+		// essential containers must have 'start' as a status
+		if name == manifestName || sidecars[name].Essential == nil || *sidecars[name].Essential == true {
+			if status != "start" {
+				return errEssentialContainerStatus
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateNoCircularDependencies(sidecars map[string]*manifest.SidecarConfig, img manifest.Image, manifestName string) error {
+	// don't let anything circularly depend on each other
+	dependencies := buildDependencyGraph(sidecars, img, manifestName)
+
+	// don't let invalid containers happen
+	if dependencies == nil {
+		return errInvalidContainer
+	}
+
+	// don't let circular dependencies happen
+	if !graph.Acyclic(dependencies) {
+		return errCircularDependency
+	}
+
+	return nil
+}
+
+func buildDependencyGraph(sidecars map[string]*manifest.SidecarConfig, img manifest.Image, manifestName string) *graph.Mutable {
+	var currNode, depNode int
+	last := 1
+	nodes := map[string]int{}
+	dependencies := graph.New(len(sidecars) + 1)
+
+	// sidecar dependencies
+	for name, sidecar := range sidecars {
+		// add any existing dependency to the graph
+		if len(sidecar.DependsOn) != 0 {
+			currNode, nodes, last = getNode(name, nodes, last)
+
+			for dep := range sidecar.DependsOn {
+				// containers being depended on must exist
+				if sidecars[dep] == nil && dep != manifestName {
+					return nil
+				}
+
+				depNode, nodes, last = getNode(dep, nodes, last)
+				dependencies.Add(currNode, depNode)
+			}
+		}
+	}
+
+	// add any image dependencies to the graph
+	if len(img.DependsOn) != 0 {
+		currNode, nodes, last = getNode(manifestName, nodes, last)
+
+		for dep := range img.DependsOn {
+			// containers being depended on must exist
+			if sidecars[dep] == nil {
+				return nil
+			}
+
+			depNode, nodes, last = getNode(dep, nodes, last)
+			dependencies.Add(currNode, depNode)
+		}
+	}
+
+	return dependencies
+}
+
+func getNode(name string, nodes map[string]int, last int) (int, map[string]int, int) {
+	if nodes[name] != 0 {
+		return nodes[name] - 1, nodes, last
+	}
+	nodes[name] = last
+	return nodes[name] - 1, nodes, last + 1
+}
+
+func validateImageDependsOn(img manifest.Image, sidecars map[string]*manifest.SidecarConfig, manifestName string) error {
+	if img.DependsOn == nil {
+		return nil
+	}
+
+	for name, status := range img.DependsOn {
+		// don't let image depend on itself
+		if name == manifestName {
+			return errCircularDependency
+		}
+		// status must be one of < start | complete | success >
+		if !isValidStatus(status) {
+			return errInvalidDependsOnStatus
+		}
+		// essential containers must have 'start' as a status
+		if sidecars != nil {
+			if sidecars[name].Essential == nil || *sidecars[name].Essential == true {
+				if status != "start" {
+					return errEssentialContainerStatus
+				}
+			}
+		}
+	}
+
+	return validateNoCircularDependencies(sidecars, img, manifestName)
+}
+
+func isValidStatus(s string) bool {
+	for _, allowed := range DependsOnStatus {
+		if s == allowed {
+			return true
+		}
+	}
+
+	return false
 }
 
 func validateEFSConfig(in manifest.Volume) error {
--- internal/pkg/deploy/cloudformation/stack/validate_test.go
+++ internal/pkg/deploy/cloudformation/stack/validate_test.go
@@ -152,3 +152,282 @@ func Test_validateEFSConfig(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateSidecarDependsOn(t *testing.T) {
+	mockSidecarName := "sidecar"
+	mockManifestName := "frontend"
+	testCases := map[string]struct {
+		inSidecar   *manifest.SidecarConfig
+		allSidecars map[string]*manifest.SidecarConfig
+
+		wantErr error
+	}{
+		"No sidecar dependencies": {
+			inSidecar: &manifest.SidecarConfig{},
+			wantErr:   nil,
+		},
+		"Working set essential sidecar with container dependency": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"sidecar1": "start",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar1": {
+					Essential: aws.Bool(true),
+				},
+			},
+			wantErr: nil,
+		},
+		"Working implied essential container with container dependency": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"frontend": "start",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: nil,
+		},
+		"Working non-essential sidecar with container dependency": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"sidecar2": "complete",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+				"sidecar2": {
+					Essential: aws.Bool(false),
+				},
+			},
+			wantErr: nil,
+		},
+		"Error when sidecar container dependency status is invalid": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"sidecar2": "end",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+				"sidecar2": {
+					Essential: aws.Bool(false),
+				},
+			},
+			wantErr: errInvalidDependsOnStatus,
+		},
+		"Error when set essential sidecar has a status besides start": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"sidecar2": "complete",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+				"sidecar2": {
+					Essential: aws.Bool(true),
+				},
+			},
+			wantErr: errEssentialContainerStatus,
+		},
+		"Error when implied essential sidecar has a status besides start": {
+			inSidecar: &manifest.SidecarConfig{
+				DependsOn: map[string]string{
+					"frontend": "complete",
+				},
+			},
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: errEssentialContainerStatus,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			gotErr := validateSidecarDependsOn(*tc.inSidecar, mockSidecarName, tc.allSidecars, mockManifestName)
+			if tc.wantErr == nil {
+				require.NoError(t, gotErr)
+			} else {
+				require.EqualError(t, gotErr, tc.wantErr.Error())
+			}
+		})
+	}
+}
+
+func TestValidateNoCircularDependency(t *testing.T) {
+	mockManifestName := "frontend"
+	image := manifest.Image{}
+	testCases := map[string]struct {
+		allSidecars map[string]*manifest.SidecarConfig
+
+		wantErr error
+	}{
+		"No sidecar dependencies": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: nil,
+		},
+		"Working sidecars with container dependency": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					DependsOn: map[string]string{
+						"frontend": "start",
+					},
+				},
+			},
+			wantErr: nil,
+		},
+		"Error when sidecar depends on itself": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					DependsOn: map[string]string{
+						"sidecar": "start",
+					},
+				},
+			},
+			wantErr: errCircularDependency,
+		},
+		"Error when sidecars circularly depend on each other": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					DependsOn: map[string]string{
+						"frontend": "start",
+					},
+				},
+				"frontend": {
+					DependsOn: map[string]string{
+						"sidecar": "start",
+					},
+				},
+			},
+			wantErr: errCircularDependency,
+		},
+		"Error when sidecars inadvertently depend on each other": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					DependsOn: map[string]string{
+						"secondCar": "start",
+					},
+				},
+				"secondCar": {
+					DependsOn: map[string]string{
+						"thirdCar": "start",
+					},
+				},
+				"thirdCar": {
+					DependsOn: map[string]string{
+						"fourthCar": "start",
+					},
+				},
+				"fourthCar": {
+					DependsOn: map[string]string{
+						"sidecar": "start",
+					},
+				},
+			},
+			wantErr: errCircularDependency,
+		},
+		"Error when container doesn't exist": {
+			allSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					DependsOn: map[string]string{
+						"something": "start",
+					},
+				},
+			},
+			wantErr: errInvalidContainer,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			gotErr := validateNoCircularDependencies(tc.allSidecars, image, mockManifestName)
+			if tc.wantErr == nil {
+				require.NoError(t, gotErr)
+			} else {
+				require.EqualError(t, gotErr, tc.wantErr.Error())
+			}
+		})
+	}
+}
+
+func TestValidateImageDependsOn(t *testing.T) {
+	mockManifestName := "frontend"
+	testCases := map[string]struct {
+		inImage    *manifest.Image
+		inSidecars map[string]*manifest.SidecarConfig
+
+		wantErr error
+	}{
+		"No image container dependencies": {
+			inImage: &manifest.Image{},
+			wantErr: nil,
+		},
+		"Working image with container dependency": {
+			inImage: &manifest.Image{
+				DependsOn: map[string]string{
+					"sidecar": "start",
+				},
+			},
+			inSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: nil,
+		},
+		"Error when image depends on itself": {
+			inImage: &manifest.Image{
+				DependsOn: map[string]string{
+					"frontend": "start",
+				},
+			},
+			wantErr: errCircularDependency,
+		},
+		"Error when image container dependency status is invalid": {
+			inImage: &manifest.Image{
+				DependsOn: map[string]string{
+					"sidecar": "end",
+				},
+			},
+			inSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: errInvalidDependsOnStatus,
+		},
+		"Error when set essential container has a status besides start": {
+			inImage: &manifest.Image{
+				DependsOn: map[string]string{
+					"sidecar": "complete",
+				},
+			},
+			inSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {
+					Essential: aws.Bool(true),
+				},
+			},
+			wantErr: errEssentialContainerStatus,
+		},
+		"Error when implied essential container has a status besides start": {
+			inImage: &manifest.Image{
+				DependsOn: map[string]string{
+					"sidecar": "complete",
+				},
+			},
+			inSidecars: map[string]*manifest.SidecarConfig{
+				"sidecar": {},
+			},
+			wantErr: errEssentialContainerStatus,
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			gotErr := validateImageDependsOn(*tc.inImage, tc.inSidecars, mockManifestName)
+			if tc.wantErr == nil {
+				require.NoError(t, gotErr)
+			} else {
+				require.EqualError(t, gotErr, tc.wantErr.Error())
+			}
+		})
+	}
+}
