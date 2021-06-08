59f8c87b3a42a8457cdf32d4f590c0d979e07e9a
Efe Karakus <karakuse@amazon.com>
2021-06-07 20:41:51 -0700

    test(e2e): add skeleton for pipeline e2e test (#2422)
    This initial e2e/pipeline pkg just creates a codecommit repository
    where we can push the service code. Once this PR is merged and we know
    that it works with CodeBuild, we'll send a follow-up PR to actually run
    the "copilot pipeline" commands.
    
    - [x] Tested the changes against my AWS account and local machine
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.

---
+++ e2e/pipeline/pipeline_suite_test.go
@@ -0,0 +1,54 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package pipeline_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/aws/copilot-cli/e2e/internal/client"
+)
+
+// Command-line tools.
+var (
+	copilot *client.CLI
+	aws     *client.AWS
+)
+
+// Application identifiers.
+var (
+	appName = fmt.Sprintf("e2e-pipeline-%d", time.Now().Unix())
+)
+
+// CodeCommit credentials.
+var (
+	repoName          = appName
+	codeCommitIAMUser = fmt.Sprintf("%s-cc", appName)
+	codeCommitCreds   *client.IAMServiceCreds
+)
+
+func TestPipeline(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Pipeline Suite")
+}
+
+var _ = BeforeSuite(func() {
+	cli, err := client.NewCLIWithDir(repoName)
+	Expect(err).NotTo(HaveOccurred())
+	copilot = cli
+	aws = client.NewAWS()
+
+	creds, err := aws.CreateCodeCommitIAMUser(codeCommitIAMUser)
+	Expect(err).NotTo(HaveOccurred())
+	codeCommitCreds = creds
+})
+
+var _ = AfterSuite(func() {
+	_ = aws.DeleteCodeCommitRepo(appName)
+	_ = aws.DeleteCodeCommitIAMUser(codeCommitIAMUser, codeCommitCreds.CredentialID)
+})
