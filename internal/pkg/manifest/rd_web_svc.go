a75a23ae91aebbaefbcab588a32f145b8b1139c4
Janice <jwh@amazon.com>
2021-09-09 09:24:52 -0700

    Revert "chore: remove platform from rdws props"
    This reverts commit 2ddd2ca6e3d3630a1cd1961230e599b7ea8be5b6.

--- internal/pkg/manifest/rd_web_svc.go
+++ internal/pkg/manifest/rd_web_svc.go
@@ -47,7 +47,8 @@ type AppRunnerInstanceConfig struct {
 // RequestDrivenWebServiceProps contains properties for creating a new request-driven web service manifest.
 type RequestDrivenWebServiceProps struct {
 	*WorkloadProps
-	Port uint16
+	Port     uint16
+	Platform *PlatformArgsOrString
 }
 
 // NewRequestDrivenWebService creates a new Request-Driven Web Service manifest with default values.
