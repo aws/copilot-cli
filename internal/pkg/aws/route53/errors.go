d93ebe13b78a66e0bed5fae960f33c5ac53626cd
Wanxian Yang <wanxiay@amazon.com>
2021-11-30 11:52:38 -0800

    implement route53domains client to get domain info

--- internal/pkg/aws/route53/errors.go
+++ internal/pkg/aws/route53/errors.go
@@ -3,7 +3,19 @@
 
 package route53
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // ErrNoDefaultCluster occurs when the default cluster is not found.
 var ErrDomainNotExist = errors.New("domain does not exist")
+
+// ErrDomainNotFound occurs when the domain is not found in the account.
+type ErrDomainNotFound struct {
+	domainName string
+}
+
+func (e *ErrDomainNotFound) Error() string {
+	return fmt.Sprintf("domain %s is not found in the account", e.domainName)
+}
--- internal/pkg/aws/route53/route53.go
+++ internal/pkg/aws/route53/route53.go
@@ -14,9 +14,8 @@ import (
 )
 
 const (
-	// See https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/DNSLimitations.html#limits-service-quotas
-	// > To view limits and request higher limits for Route 53, you must change the Region to US East (N. Virginia).
-	// So we have to set the region to us-east-1 to be able to find out if a domain name exists in the account.
+	// See https://docs.aws.amazon.com/general/latest/gr/r53.html
+	// For Route53 API endpoint, "Route 53 in AWS Regions other than the Beijing and Ningxia Regions: specify us-east-1 as the Region."
 	route53Region = "us-east-1"
 )
 
