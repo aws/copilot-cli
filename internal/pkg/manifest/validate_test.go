aws/copilot-cli

commit d357a80bd23b27d7916992da370ee05427d1fea0

chore: push template to s3 and deploy workload stack (#3183)
This PR is to address [this comment](https://github.com/aws/copilot-cli/pull/3171#discussion_r781820226) in a previous PR. When deploying workloads, instead of using the template body string, we first upload the template to the s3 bucket, and then refer to the URL in the API call. This is similar to the current approach to deploy a pipeline.

We need to do this because there is a 51KB limit of template body string, but 1MB if it's uploaded to s3 bucket. As the template is growing larger and larger, 51KB may become insufficient.

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

internal/pkg/cli/svc_deploy_test.go
@@ -884,7 +884,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).Return(errors.New("some error"))
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).Return(errors.New("some error"))
 			},
 			wantErr: fmt.Errorf("deploy service: some error"),
 		},
@@ -904,7 +910,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).Return(cloudformation.NewMockErrChangeSetEmpty())
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).Return(cloudformation.NewMockErrChangeSetEmpty())
 			},
 			wantErr: fmt.Errorf("deploy service: change set with name mockChangeSet for stack mockStack has no changes"),
 		},
@@ -925,7 +937,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).
 					Return(nil)
 				m.mockServiceUpdater.EXPECT().LastUpdatedAt(mockAppName, mockEnvName, mockSvcName).
 					Return(time.Time{}, mockError)
@@ -949,7 +967,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).
 					Return(nil)
 				m.mockServiceUpdater.EXPECT().LastUpdatedAt(mockAppName, mockEnvName, mockSvcName).
 					Return(mockAfterTime, nil)
@@ -969,7 +993,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockWs.EXPECT().ReadWorkloadManifest(mockSvcName).Return([]byte{}, nil)
 				m.mockInterpolator.EXPECT().Interpolate("").Return("", nil)
 				m.mockEndpointGetter.EXPECT().ServiceDiscoveryEndpoint().Return("mockApp.local", nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).
 					Return(cloudformation.NewMockErrChangeSetEmpty())
 				m.mockServiceUpdater.EXPECT().LastUpdatedAt(mockAppName, mockEnvName, mockSvcName).
 					Return(mockBeforeTime, nil)
@@ -999,7 +1029,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).
 					Return(cloudformation.NewMockErrChangeSetEmpty())
 				m.mockServiceUpdater.EXPECT().LastUpdatedAt(mockAppName, mockEnvName, mockSvcName).
 					Return(mockBeforeTime, nil)
@@ -1036,7 +1072,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).Return(nil)
 			},
 		},
 		"success with force update": {
@@ -1056,7 +1098,13 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 				m.mockIdentity.EXPECT().Get().Return(identity.Caller{
 					RootUserARN: "1234",
 				}, nil)
-				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), gomock.Any()).
+				m.mockAppResourcesGetter.EXPECT().GetAppResourcesByRegion(&config.Application{
+					Name:   mockAppName,
+					Domain: "mockDomain",
+				}, "us-west-2").Return(&stack.AppRegionalResources{
+					S3Bucket: "mockBucket",
+				}, nil)
+				m.mockServiceDeployer.EXPECT().DeployService(gomock.Any(), gomock.Any(), "mockBucket", gomock.Any()).
 					Return(cloudformation.NewMockErrChangeSetEmpty())
 				m.mockServiceUpdater.EXPECT().LastUpdatedAt(mockAppName, mockEnvName, mockSvcName).
 					Return(mockBeforeTime, nil)

internal/pkg/aws/partitions/partition.go
@@ -1,0 +1,21 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package partitions
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+)
+
+type Region string
+
+// Partition returns the first partition which includes the region passed in, from a list of the partitions the SDK is bundled with.
+func (r Region) Partition() (endpoints.Partition, error) {
+	partition, ok := endpoints.PartitionForRegion(endpoints.DefaultPartitions(), string(r))
+	if !ok {
+		return endpoints.Partition{}, fmt.Errorf("find the partition for region %s", string(r))
+	}
+	return partition, nil
+}

internal/pkg/aws/partitions/partition_test.go
@@ -1,0 +1,37 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package partitions
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRegion_Partition(t *testing.T) {
+	testCases := map[string]struct {
+		region    string
+		wantedErr error
+	}{
+		"error finding the partition": {
+			region:    "weird region",
+			wantedErr: errors.New("find the partition for region weird region"),
+		},
+		"success": {
+			region: "us-west-2",
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			_, err := Region(tc.region).Partition()
+			if tc.wantedErr != nil {
+				require.EqualError(t, err, tc.wantedErr.Error())
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}

internal/pkg/cli/env_init.go
@@ -16,6 +16,7 @@ import (
 	"github.com/aws/copilot-cli/internal/pkg/aws/ec2"
 	"github.com/aws/copilot-cli/internal/pkg/aws/iam"
 	"github.com/aws/copilot-cli/internal/pkg/aws/identity"
+	"github.com/aws/copilot-cli/internal/pkg/aws/partitions"
 	"github.com/aws/copilot-cli/internal/pkg/aws/profile"
 	"github.com/aws/copilot-cli/internal/pkg/aws/s3"
 	"github.com/aws/copilot-cli/internal/pkg/aws/sessions"
@@ -290,7 +291,15 @@ func (o *initEnvOpts) Execute() error {
 	}
 
 	// 4. Start creating the CloudFormation stack for the environment.
-	if err := o.deployEnv(app, urls); err != nil {
+	if resources.S3Bucket == "" {
+		log.Errorf("Cannot find the S3 artifact bucket in %s region created by app %s. The S3 bucket is necessary for many future operations. For example, when you need addons to your services.", envRegion, app.Name)
+		return fmt.Errorf("cannot find the S3 artifact bucket in %s region", envRegion)
+	}
+	partition, err := partitions.Region(envRegion).Partition()
+	if err != nil {
+		return err
+	}
+	if err := o.deployEnv(app, s3.FormatARN(partition.ID(), resources.S3Bucket), resources.KMSKeyARN, urls); err != nil {
 		return err
 	}
 
@@ -644,7 +653,8 @@ func (o *initEnvOpts) adjustVPCConfig() *config.AdjustVPC {
 	}
 }
 
-func (o *initEnvOpts) deployEnv(app *config.Application, customResourcesURLs map[string]string) error {
+func (o *initEnvOpts) deployEnv(app *config.Application,
+	artifactBucketARN, artifactBucketKeyARN string, customResourcesURLs map[string]string) error {
 	caller, err := o.identity.Get()
 	if err != nil {
 		return fmt.Errorf("get identity: %w", err)
@@ -656,12 +666,14 @@ func (o *initEnvOpts) deployEnv(app *config.Application, customResourcesURLs map
 			DNSName:             app.Domain,
 			AccountPrincipalARN: caller.RootUserARN,
 		},
-		Prod:                o.isProduction,
-		AdditionalTags:      app.Tags,
-		CustomResourcesURLs: customResourcesURLs,
-		AdjustVPCConfig:     o.adjustVPCConfig(),
-		ImportVPCConfig:     o.importVPCConfig(),
-		Version:             deploy.LatestEnvTemplateVersion,
+		Prod:                 o.isProduction,
+		AdditionalTags:       app.Tags,
+		CustomResourcesURLs:  customResourcesURLs,
+		ArtifactBucketARN:    artifactBucketARN,
+		ArtifactBucketKeyARN: artifactBucketKeyARN,
+		AdjustVPCConfig:      o.adjustVPCConfig(),
+		ImportVPCConfig:      o.importVPCConfig(),
+		Version:              deploy.LatestEnvTemplateVersion,
 	}
 
 	if err := o.cleanUpDanglingRoles(o.appName, o.name); err != nil {

internal/pkg/cli/env_init_test.go
@@ -1089,8 +1089,10 @@ func TestInitEnvOpts_Execute(t *testing.T) {
 						Name:                "phonetool",
 						AccountPrincipalARN: "some arn",
 					},
-					CustomResourcesURLs: map[string]string{"mockCustomResource": "mockURL"},
-					Version:             deploy.LatestEnvTemplateVersion,
+					CustomResourcesURLs:  map[string]string{"mockCustomResource": "mockURL"},
+					Version:              deploy.LatestEnvTemplateVersion,
+					ArtifactBucketARN:    "arn:aws:s3:::mockBucket",
+					ArtifactBucketKeyARN: "mockKMS",
 				}).Return(&cloudformation.ErrStackAlreadyExists{})
 				m.EXPECT().GetEnvironment("phonetool", "test").Return(&config.Environment{
 					AccountID: "1234",
@@ -1103,7 +1105,8 @@ func TestInitEnvOpts_Execute(t *testing.T) {
 			expectAppCFN: func(m *mocks.MockappResourcesGetter) {
 				m.EXPECT().GetAppResourcesByRegion(&config.Application{Name: "phonetool"}, "us-west-2").
 					Return(&stack.AppRegionalResources{
-						S3Bucket: "mockBucket",
+						S3Bucket:  "mockBucket",
+						KMSKeyARN: "mockKMS",
 					}, nil)
 			},
 			expectResourcesUploader: func(m *mocks.MockcustomResourcesUploader) {

internal/pkg/cli/env_upgrade.go
@@ -7,6 +7,8 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+
 	"github.com/aws/copilot-cli/internal/pkg/aws/s3"
 	"github.com/aws/copilot-cli/internal/pkg/aws/sessions"
 	"github.com/aws/copilot-cli/internal/pkg/config"
@@ -180,7 +182,7 @@ func (o *envUpgradeOpts) Execute() error {
 		if err != nil {
 			return fmt.Errorf("upload custom resources to bucket %s: %w", resources.S3Bucket, err)
 		}
-		if err := o.upgrade(env, urls); err != nil {
+		if err := o.upgrade(env, s3.FormatARN(endpoints.AwsPartitionID, resources.S3Bucket), resources.KMSKeyARN, urls); err != nil {
 			return err
 		}
 	}
@@ -208,7 +210,8 @@ func (o *envUpgradeOpts) listEnvsToUpgrade() ([]*config.Environment, error) {
 	return envs, nil
 }
 
-func (o *envUpgradeOpts) upgrade(env *config.Environment, customResourcesURLs map[string]string) (err error) {
+func (o *envUpgradeOpts) upgrade(env *config.Environment,
+	artifactBucketARN, artifactBucketKeyARN string, customResourcesURLs map[string]string) (err error) {
 	version, err := o.envVersion(env.Name)
 	if err != nil {
 		return err
@@ -230,9 +233,9 @@ func (o *envUpgradeOpts) upgrade(env *config.Environment, customResourcesURLs ma
 		return err
 	}
 	if version == deploy.LegacyEnvTemplateVersion {
-		return o.upgradeLegacyEnvironment(upgrader, env, customResourcesURLs, version, deploy.LatestEnvTemplateVersion)
+		return o.upgradeLegacyEnvironment(upgrader, env, artifactBucketARN, artifactBucketKeyARN, customResourcesURLs, version, deploy.LatestEnvTemplateVersion)
 	}
-	return o.upgradeEnvironment(upgrader, env, customResourcesURLs, version, deploy.LatestEnvTemplateVersion)
+	return o.upgradeEnvironment(upgrader, env, artifactBucketARN, artifactBucketKeyARN, customResourcesURLs, version, deploy.LatestEnvTemplateVersion)
 }
 
 func (o *envUpgradeOpts) envVersion(name string) (string, error) {
@@ -267,6 +270,7 @@ Are you using the latest version of AWS Copilot?`, env, deploy.LatestEnvTemplate
 }
 
 func (o *envUpgradeOpts) upgradeEnvironment(upgrader envUpgrader, conf *config.Environment,
+	artifactBucketARN, artifactBucketKeyARN string,
 	customResourcesURLs map[string]string, fromVersion, toVersion string) error {
 	var importedVPC *config.ImportVPC
 	var adjustedVPC *config.AdjustVPC
@@ -280,11 +284,13 @@ func (o *envUpgradeOpts) upgradeEnvironment(upgrader envUpgrader, conf *config.E
 		App: deploy.AppInformation{
 			Name: conf.App,
 		},
-		Name:                conf.Name,
-		CustomResourcesURLs: customResourcesURLs,
-		ImportVPCConfig:     importedVPC,
-		AdjustVPCConfig:     adjustedVPC,
-		CFNServiceRoleARN:   conf.ExecutionRoleARN,
+		Name:                 conf.Name,
+		ArtifactBucketKeyARN: artifactBucketKeyARN,
+		ArtifactBucketARN:    artifactBucketARN,
+		CustomResourcesURLs:  customResourcesURLs,
+		ImportVPCConfig:      importedVPC,
+		AdjustVPCConfig:      adjustedVPC,
+		CFNServiceRoleARN:    conf.ExecutionRoleARN,
 	}); err != nil {
 		return fmt.Errorf("upgrade environment %s from version %s to version %s: %v", conf.Name, fromVersion, toVersion, err)
 	}
@@ -292,6 +298,7 @@ func (o *envUpgradeOpts) upgradeEnvironment(upgrader envUpgrader, conf *config.E
 }
 
 func (o *envUpgradeOpts) upgradeLegacyEnvironment(upgrader legacyEnvUpgrader, conf *config.Environment,
+	artifactBucketARN, artifactBucketKeyARN string,
 	customResourcesURLs map[string]string, fromVersion, toVersion string) error {
 	isDefaultEnv, err := o.isDefaultLegacyTemplate(upgrader, conf.App, conf.Name)
 	if err != nil {
@@ -307,9 +314,11 @@ func (o *envUpgradeOpts) upgradeLegacyEnvironment(upgrader legacyEnvUpgrader, co
 			App: deploy.AppInformation{
 				Name: conf.App,
 			},
-			Name:                conf.Name,
-			CustomResourcesURLs: customResourcesURLs,
-			CFNServiceRoleARN:   conf.ExecutionRoleARN,
+			Name:                 conf.Name,
+			ArtifactBucketKeyARN: artifactBucketKeyARN,
+			ArtifactBucketARN:    artifactBucketARN,
+			CustomResourcesURLs:  customResourcesURLs,
+			CFNServiceRoleARN:    conf.ExecutionRoleARN,
 		}, albWorkloads...); err != nil {
 			return fmt.Errorf("upgrade environment %s from version %s to version %s: %v", conf.Name, fromVersion, toVersion, err)
 		}

internal/pkg/cli/env_upgrade_test.go
@@ -272,7 +272,8 @@ func TestEnvUpgradeOpts_Execute(t *testing.T) {
 				mockAppCFN := mocks.NewMockappResourcesGetter(ctrl)
 				mockAppCFN.EXPECT().GetAppResourcesByRegion(&config.Application{Name: "phonetool"}, "us-west-2").
 					Return(&stack.AppRegionalResources{
-						S3Bucket: "mockBucket",
+						S3Bucket:  "mockBucket",
+						KMSKeyARN: "mockKMS",
 					}, nil)
 				mockUploader := mocks.NewMockcustomResourcesUploader(ctrl)
 				mockUploader.EXPECT().UploadEnvironmentCustomResources(gomock.Any()).Return(map[string]string{"mockCustomResource": "mockURL"}, nil)
@@ -287,8 +288,10 @@ func TestEnvUpgradeOpts_Execute(t *testing.T) {
 					ImportVPCConfig: &config.ImportVPC{
 						ID: "abc",
 					},
-					CFNServiceRoleARN:   "execARN",
-					CustomResourcesURLs: map[string]string{"mockCustomResource": "mockURL"},
+					CFNServiceRoleARN:    "execARN",
+					CustomResourcesURLs:  map[string]string{"mockCustomResource": "mockURL"},
+					ArtifactBucketARN:    "arn:aws:s3:::mockBucket",
+					ArtifactBucketKeyARN: "mockKMS",
 				}).Return(nil)
 
 				return &envUpgradeOpts{
@@ -345,7 +348,8 @@ func TestEnvUpgradeOpts_Execute(t *testing.T) {
 				mockAppCFN := mocks.NewMockappResourcesGetter(ctrl)
 				mockAppCFN.EXPECT().GetAppResourcesByRegion(&config.Application{Name: "phonetool"}, "us-west-2").
 					Return(&stack.AppRegionalResources{
-						S3Bucket: "mockBucket",
+						S3Bucket:  "mockBucket",
+						KMSKeyARN: "mockKMS",
 					}, nil)
 				mockUploader := mocks.NewMockcustomResourcesUploader(ctrl)
 				mockUploader.EXPECT().UploadEnvironmentCustomResources(gomock.Any()).Return(map[string]string{"mockCustomResource": "mockURL"}, nil)
@@ -360,9 +364,11 @@ func TestEnvUpgradeOpts_Execute(t *testing.T) {
 					App: deploy.AppInformation{
 						Name: "phonetool",
 					},
-					Name:                "test",
-					CFNServiceRoleARN:   "execARN",
-					CustomResourcesURLs: map[string]string{"mockCustomResource": "mockURL"},
+					Name:                 "test",
+					CFNServiceRoleARN:    "execARN",
+					CustomResourcesURLs:  map[string]string{"mockCustomResource": "mockURL"},
+					ArtifactBucketARN:    "arn:aws:s3:::mockBucket",
+					ArtifactBucketKeyARN: "mockKMS",
 				}, "frontend").Return(nil)
 
 				return &envUpgradeOpts{

internal/pkg/cli/interfaces.go
@@ -552,7 +552,7 @@ type svcForceUpdater interface {
 }
 
 type serviceDeployer interface {
-	DeployService(out termprogress.FileWriter, conf cloudformation.StackConfiguration, opts ...awscloudformation.StackOption) error
+	DeployService(out termprogress.FileWriter, conf cloudformation.StackConfiguration, bucketName string, opts ...awscloudformation.StackOption) error
 }
 
 type apprunnerServiceDescriber interface {

commit 2c3ed057418ba4ebfd7f5c2e60d3ce23ccb7f7ee

chore: fix integration test (#3212)
The failure was caused by #3183.

We add to the environment manager role additional permissions to put object into the S3 bucket. In normal workflow, the `Artifact...` data is passed from `cli` (e.g. `env init`) to the template. This is missing from the integ test.

An additional validation for `http.path` tags along. If `path` is missing, the deployment will fail anyway in a later stage. manifest validation could provide a more friendyl error mesage than the cfn error.

By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

internal/pkg/manifest/validate.go
@@ -549,6 +549,16 @@ func (CommandOverride) Validate() error {
 	return nil
 }
 
+// Validate returns nil if RoutingRuleConfigOrBool is configured correctly.
+func (r RoutingRuleConfigOrBool) Validate() error {
+	if aws.BoolValue(r.Enabled) {
+		return &errFieldMustBeSpecified{
+			missingField: "path",
+		}
+	}
+	return r.RoutingRuleConfiguration.Validate()
+}
+
 // Validate returns nil if RoutingRuleConfiguration is configured correctly.
 func (r RoutingRuleConfiguration) Validate() error {
 	var err error
@@ -572,6 +582,11 @@ func (r RoutingRuleConfiguration) Validate() error {
 	if r.ProtocolVersion != nil {
 		if !contains(strings.ToUpper(*r.ProtocolVersion), httpProtocolVersions) {
 			return fmt.Errorf(`"version" field value '%s' must be one of %s`, *r.ProtocolVersion, english.WordSeries(httpProtocolVersions, "or"))
+		}
+	}
+	if r.Path == nil {
+		return &errFieldMustBeSpecified{
+			missingField: "path",
 		}
 	}
 	return nil

internal/pkg/cli/svc_deploy_test.go
@@ -234,6 +234,8 @@ func TestSvcDeployOpts_configureContainerImage(t *testing.T) {
 	mockError := errors.New("mockError")
 	mockManifest := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 image:
   build:
     dockerfile: path/to/Dockerfile
@@ -242,15 +244,20 @@ image:
 `)
 	mockManifestWithBadPlatform := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 platform: linus/abc123
 image:
   build:
     dockerfile: path/to/Dockerfile
     context: path
   port: 80
+
 `)
 	mockManifestWithGoodPlatform := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 platform: linux/amd64
 image:
   build:
@@ -260,18 +267,24 @@ image:
 `)
 	mockMftNoBuild := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 image:
   location: foo/bar
   port: 80
 `)
 	mockMftBuildString := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 image:
   build: path/to/Dockerfile
   port: 80
 `)
 	mockMftNoContext := []byte(`name: serviceA
 type: 'Load Balanced Web Service'
+http:
+  path: "/"
 image:
   build:
     dockerfile: path/to/Dockerfile
@@ -1071,6 +1084,7 @@ func TestSvcDeployOpts_deploySvc(t *testing.T) {
 							},
 							RoutingRule: manifest.RoutingRuleConfigOrBool{
 								RoutingRuleConfiguration: manifest.RoutingRuleConfiguration{
+									Path:  aws.String("/"),
 									Alias: tc.inAliases,
 								},
 							},

internal/pkg/deploy/cloudformation/cc_pipeline_integration_test.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/aws/copilot-cli/internal/pkg/aws/identity"
+	"github.com/aws/copilot-cli/internal/pkg/aws/partitions"
 	"github.com/aws/copilot-cli/internal/pkg/aws/s3"
 	"github.com/aws/copilot-cli/internal/pkg/config"
 	"github.com/aws/copilot-cli/internal/pkg/deploy"
@@ -180,6 +181,11 @@ func TestCCPipelineCreation(t *testing.T) {
 		}))
 		require.NoError(t, err)
 		environmentToDeploy.CustomResourcesURLs = urls
+
+		partition, err := partitions.Region(envRegion.ID()).Partition()
+		require.NoError(t, err)
+		environmentToDeploy.ArtifactBucketARN = s3.FormatARN(partition.ID(), envBucketName)
+		environmentToDeploy.ArtifactBucketKeyARN = regionalResource.KMSKeyARN
 
 		// Deploy the environment in the same tools account but in different
 		// region and wait for it to be complete

internal/pkg/manifest/validate_test.go
@@ -68,6 +68,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 							},
 						},
 					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
+					},
 				},
 			},
 			wantedErrorMsgPrefix: `validate "sidecars[foo]": `,
@@ -79,6 +84,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 					Network: NetworkConfig{
 						vpcConfig{
 							Placement: (*Placement)(aws.String("")),
+						},
+					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
 						},
 					},
 				},
@@ -94,6 +104,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 							{},
 						},
 					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
+					},
 				},
 			},
 			wantedErrorMsgPrefix: `validate "publish": `,
@@ -107,6 +122,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 							Path: "Family",
 						},
 					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
+					},
 				},
 			},
 			wantedErrorMsgPrefix: `validate "taskdef_overrides[0]": `,
@@ -115,6 +135,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 			lbConfig: LoadBalancedWebService{
 				LoadBalancedWebServiceConfig: LoadBalancedWebServiceConfig{
 					ImageConfig: testImageConfig,
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
+					},
 				},
 			},
 			wantedError: fmt.Errorf(`"name" must be specified`),
@@ -126,6 +151,7 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 					ImageConfig: testImageConfig,
 					RoutingRule: RoutingRuleConfigOrBool{
 						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path:            stringP("/"),
 							TargetContainer: aws.String("foo"),
 						},
 					},
@@ -140,6 +166,7 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 					ImageConfig: testImageConfig,
 					RoutingRule: RoutingRuleConfigOrBool{
 						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path:            stringP("/"),
 							TargetContainer: aws.String("mockName"),
 						},
 					},
@@ -166,6 +193,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 							Essential: aws.Bool(false),
 						},
 					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
+					},
 				},
 			},
 			wantedErrorMsgPrefix: `validate container dependencies: `,
@@ -178,6 +210,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 					TaskConfig: TaskConfig{
 						Platform:       PlatformArgsOrString{PlatformString: (*PlatformString)(aws.String("windows/amd64"))},
 						ExecuteCommand: ExecuteCommand{Enable: aws.Bool(true)},
+					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
+						},
 					},
 				},
 			},
@@ -197,6 +234,11 @@ func TestLoadBalancedWebService_Validate(t *testing.T) {
 								Spot:         aws.Int(123),
 								workloadType: LoadBalancedWebServiceType,
 							},
+						},
+					},
+					RoutingRule: RoutingRuleConfigOrBool{
+						RoutingRuleConfiguration: RoutingRuleConfiguration{
+							Path: stringP("/"),
 						},
 					},
 				},
@@ -1002,8 +1044,13 @@ func TestRoutingRule_Validate(t *testing.T) {
 			},
 			wantedErrorMsgPrefix: `"version" field value 'quic' must be one of GRPC, HTTP1 or HTTP2`,
 		},
+		"error if path is missing": {
+			RoutingRule:          RoutingRuleConfiguration{},
+			wantedErrorMsgPrefix: `"path" must be specified`,
+		},
 		"should not error if protocol version is not uppercase": {
 			RoutingRule: RoutingRuleConfiguration{
+				Path:            stringP("/"),
 				ProtocolVersion: aws.String("gRPC"),
 			},
 		},