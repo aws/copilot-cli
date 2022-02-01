3ded76bc98f4fe93febbcdc5ed42513b7cd3be51
Wanxian Yang <wanxiay@amazon.com>
2022-02-01 14:31:07 -0600

    add netwrok load balancer lbws integration test for dev environmnet test case

--- internal/pkg/deploy/cloudformation/stack/lb_network_web_service_integration_test.go
+++ internal/pkg/deploy/cloudformation/stack/lb_network_web_service_integration_test.go
@@ -49,6 +49,11 @@ func TestNetworkLoadBalancedWebService_Template(t *testing.T) {
 			svcStackPath:  "svc-nlb-test.stack.yml",
 			svcParamsPath: "svc-nlb-test.params.json",
 		},
+		"dev env": {
+			envName:       "dev",
+			svcStackPath:  "svc-nlb-dev.stack.yml",
+			svcParamsPath: "svc-nlb-dev.params.json",
+		},
 	}
 	val, exist := os.LookupEnv("TAG")
 	require.NoError(t, os.Setenv("TAG", "cicdtest"))
