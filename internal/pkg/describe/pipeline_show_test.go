efe855432195e92927263dc2643a2d6e35ed8c9c
Wanxian Yang <79273084+Lou1415926@users.noreply.github.com>
2022-04-04 14:29:02 -0700

    chore: do not trim pipeline stack name to 128 characters (#3421)
    This is reverting a part of the changes in #3416 that trim the pipeline stack name to 128 characters by smart-trimming from app name and pipeline name. CFN will error out if the stack name is too long, preventing the initial creation of the stack in the first place. This errorring out is early enough for user to change their pipeline name in order to meet the restriction. 
    
    In addition, the PR updates `pipeline show` and `pipeline status` output.
    
    ### `pipeline show`
    For `pipeline show`, the human string output will show the pipeline name as specified in manifest:
    ```
    About
    
      Name        new-world
      Region      us-east-2
      AccountID   1111111111
      Created At  50 minutes ago
      Updated At  50 minutes ago
    ```
    Its JSON output will show:
    ```
    {"pipelineName":"new-world","name":"pipeline-e2e-try-new-world-Pipeline-11T2LFVLO1XN6","region"...
    ```
    adding a new JSON field `pipelineName` whose value is the manifest pipeline name. This is to prevent breaking changes for folks that've been using the JSON output's `name` filed for automation.
    
    ### `pipeline status`
    `pipeline status` doesn't show pipeline name as human string output, so no changes here.
    
    It's original JSON output is: 
    ```
    {"pipelineName":"pipeline-e2e-try-new-world-Pipeline-11T2LFVLO1XN6"
    ```
    To prevent it from being breaking for users' automation, this PR adds a `name` field whose value is the manifest pipeline name.
    
    Note that this may be confusing because the fields are named differently for `pipeline show` and `pipeline status` JSON.
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/describe/pipeline_show_test.go
+++ internal/pkg/describe/pipeline_show_test.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/aws/copilot-cli/internal/pkg/aws/codepipeline"
+	"github.com/aws/copilot-cli/internal/pkg/deploy"
 	"github.com/aws/copilot-cli/internal/pkg/describe/mocks"
 	"github.com/aws/copilot-cli/internal/pkg/describe/stack"
 	"github.com/dustin/go-humanize"
@@ -22,13 +23,14 @@ type pipelineDescriberMocks struct {
 	pipelineGetter *mocks.MockpipelineGetter
 }
 
+var pipelineResourceName = "pipeline-dinder-badgoose-repo-RANDOMSTRING"
 var pipelineName = "pipeline-dinder-badgoose-repo"
 var mockTime = func() time.Time {
 	t, _ := time.Parse(time.RFC3339, "2020-02-02T15:04:05+00:00")
 	return t
 }
 var mockPipeline = &codepipeline.Pipeline{
-	Name:      pipelineName,
+	Name:      pipelineResourceName,
 	Region:    "us-west-2",
 	AccountID: "1234567890",
 	Stages: []*codepipeline.Stage{
@@ -119,24 +121,32 @@ func TestPipelineDescriber_Describe(t *testing.T) {
 	}{
 		"happy path with resources": {
 			callMocks: func(m pipelineDescriberMocks) {
-				m.pipelineGetter.EXPECT().GetPipeline(pipelineName).Return(mockPipeline, nil)
+				m.pipelineGetter.EXPECT().GetPipeline(pipelineResourceName).Return(mockPipeline, nil)
 				m.cfn.EXPECT().Resources().Return(mockResources, nil)
 			},
 			inShowResource: true,
 			expectedError:  nil,
-			expectedOutput: &Pipeline{*mockPipeline, expectedResources},
+			expectedOutput: &Pipeline{
+				Name:      pipelineName,
+				Pipeline:  *mockPipeline,
+				Resources: expectedResources,
+			},
 		},
 		"happy path without resources": {
 			callMocks: func(m pipelineDescriberMocks) {
-				m.pipelineGetter.EXPECT().GetPipeline(pipelineName).Return(mockPipeline, nil)
+				m.pipelineGetter.EXPECT().GetPipeline(pipelineResourceName).Return(mockPipeline, nil)
 			},
 			inShowResource: false,
 			expectedError:  nil,
-			expectedOutput: &Pipeline{*mockPipeline, nil},
+			expectedOutput: &Pipeline{
+				Name:      pipelineName,
+				Pipeline:  *mockPipeline,
+				Resources: nil,
+			},
 		},
 		"wraps get pipeline error": {
 			callMocks: func(m pipelineDescriberMocks) {
-				m.pipelineGetter.EXPECT().GetPipeline(pipelineName).Return(nil, mockError)
+				m.pipelineGetter.EXPECT().GetPipeline(pipelineResourceName).Return(nil, mockError)
 			},
 			inShowResource: false,
 			expectedError:  fmt.Errorf("get pipeline: %w", mockError),
@@ -144,7 +154,7 @@ func TestPipelineDescriber_Describe(t *testing.T) {
 		},
 		"wraps stack resources error": {
 			callMocks: func(m pipelineDescriberMocks) {
-				m.pipelineGetter.EXPECT().GetPipeline(pipelineName).Return(mockPipeline, nil)
+				m.pipelineGetter.EXPECT().GetPipeline(pipelineResourceName).Return(mockPipeline, nil)
 				m.cfn.EXPECT().Resources().Return(nil, mockError)
 			},
 			inShowResource: true,
@@ -166,8 +176,15 @@ func TestPipelineDescriber_Describe(t *testing.T) {
 			}
 			tc.callMocks(mocks)
 
+			mockDeployedPipeline := deploy.Pipeline{
+				ResourceName: pipelineResourceName,
+				Name:         pipelineName,
+				AppName:      "mockAppName",
+				IsLegacy:     false,
+			}
+
 			describer := &PipelineDescriber{
-				pipelineName:  pipelineName,
+				pipeline:      mockDeployedPipeline,
 				showResources: tc.inShowResource,
 				pipelineSvc:   mockPipelineGetter,
 				cfn:           mockCFN,
@@ -202,7 +219,11 @@ func TestPipelineDescriber_String(t *testing.T) {
 		expectedJSONString  string
 	}{
 		"correct output with resources": {
-			inPipeline: &Pipeline{*mockPipeline, expectedResources},
+			inPipeline: &Pipeline{
+				Name:      pipelineName,
+				Pipeline:  *mockPipeline,
+				Resources: expectedResources,
+			},
 			expectedHumanString: `About
 
   Name        pipeline-dinder-badgoose-repo
@@ -227,10 +248,14 @@ Resources
     AWS::IAM::Role               pipeline-dinder-badgoose-repo-PipelineRole-100SEEQN6CU0F
     AWS::IAM::Policy             pipel-Pipe-EO4QGE10RJ8F
 `,
-			expectedJSONString: "{\"name\":\"pipeline-dinder-badgoose-repo\",\"region\":\"us-west-2\",\"accountId\":\"1234567890\",\"stages\":[{\"name\":\"Source\",\"category\":\"Source\",\"provider\":\"GitHub\",\"details\":\"Repository: badgoose/repo\"},{\"name\":\"Build\",\"category\":\"Build\",\"provider\":\"CodeBuild\",\"details\":\"BuildProject: pipeline-dinder-badgoose-repo-BuildProject\"},{\"name\":\"DeployTo-test\",\"category\":\"Deploy\",\"provider\":\"CloudFormation\",\"details\":\"StackName: dinder-test-test\"}],\"createdAt\":\"2020-02-02T15:04:05Z\",\"updatedAt\":\"2020-02-02T15:04:05Z\",\"resources\":[{\"type\":\"AWS::CodeBuild::Project\",\"physicalID\":\"pipeline-dinder-badgoose-repo-BuildProject\"},{\"type\":\"AWS::IAM::Policy\",\"physicalID\":\"pipel-Buil-1PEASDDL44ID2\"},{\"type\":\"AWS::IAM::Role\",\"physicalID\":\"pipeline-dinder-badgoose-repo-BuildProjectRole-A4V6VSG1XIIJ\"},{\"type\":\"AWS::CodePipeline::Pipeline\",\"physicalID\":\"pipeline-dinder-badgoose-repo\"},{\"type\":\"AWS::IAM::Role\",\"physicalID\":\"pipeline-dinder-badgoose-repo-PipelineRole-100SEEQN6CU0F\"},{\"type\":\"AWS::IAM::Policy\",\"physicalID\":\"pipel-Pipe-EO4QGE10RJ8F\"}]}\n",
+			expectedJSONString: "{\"name\":\"pipeline-dinder-badgoose-repo\",\"pipelineName\":\"pipeline-dinder-badgoose-repo-RANDOMSTRING\",\"region\":\"us-west-2\",\"accountId\":\"1234567890\",\"stages\":[{\"name\":\"Source\",\"category\":\"Source\",\"provider\":\"GitHub\",\"details\":\"Repository: badgoose/repo\"},{\"name\":\"Build\",\"category\":\"Build\",\"provider\":\"CodeBuild\",\"details\":\"BuildProject: pipeline-dinder-badgoose-repo-BuildProject\"},{\"name\":\"DeployTo-test\",\"category\":\"Deploy\",\"provider\":\"CloudFormation\",\"details\":\"StackName: dinder-test-test\"}],\"createdAt\":\"2020-02-02T15:04:05Z\",\"updatedAt\":\"2020-02-02T15:04:05Z\",\"resources\":[{\"type\":\"AWS::CodeBuild::Project\",\"physicalID\":\"pipeline-dinder-badgoose-repo-BuildProject\"},{\"type\":\"AWS::IAM::Policy\",\"physicalID\":\"pipel-Buil-1PEASDDL44ID2\"},{\"type\":\"AWS::IAM::Role\",\"physicalID\":\"pipeline-dinder-badgoose-repo-BuildProjectRole-A4V6VSG1XIIJ\"},{\"type\":\"AWS::CodePipeline::Pipeline\",\"physicalID\":\"pipeline-dinder-badgoose-repo\"},{\"type\":\"AWS::IAM::Role\",\"physicalID\":\"pipeline-dinder-badgoose-repo-PipelineRole-100SEEQN6CU0F\"},{\"type\":\"AWS::IAM::Policy\",\"physicalID\":\"pipel-Pipe-EO4QGE10RJ8F\"}]}\n",
 		},
 		"correct output without resources": {
-			inPipeline: &Pipeline{*mockPipeline, nil},
+			inPipeline: &Pipeline{
+				Name:      pipelineName,
+				Pipeline:  *mockPipeline,
+				Resources: nil,
+			},
 			expectedHumanString: `About
 
   Name        pipeline-dinder-badgoose-repo
@@ -247,7 +272,7 @@ Stages
   Build          Build     CodeBuild       BuildProject: pipeline-dinder-badgoose-repo-BuildProject
   DeployTo-test  Deploy    CloudFormation  StackName: dinder-test-test
 `,
-			expectedJSONString: "{\"name\":\"pipeline-dinder-badgoose-repo\",\"region\":\"us-west-2\",\"accountId\":\"1234567890\",\"stages\":[{\"name\":\"Source\",\"category\":\"Source\",\"provider\":\"GitHub\",\"details\":\"Repository: badgoose/repo\"},{\"name\":\"Build\",\"category\":\"Build\",\"provider\":\"CodeBuild\",\"details\":\"BuildProject: pipeline-dinder-badgoose-repo-BuildProject\"},{\"name\":\"DeployTo-test\",\"category\":\"Deploy\",\"provider\":\"CloudFormation\",\"details\":\"StackName: dinder-test-test\"}],\"createdAt\":\"2020-02-02T15:04:05Z\",\"updatedAt\":\"2020-02-02T15:04:05Z\"}\n",
+			expectedJSONString: "{\"name\":\"pipeline-dinder-badgoose-repo\",\"pipelineName\":\"pipeline-dinder-badgoose-repo-RANDOMSTRING\",\"region\":\"us-west-2\",\"accountId\":\"1234567890\",\"stages\":[{\"name\":\"Source\",\"category\":\"Source\",\"provider\":\"GitHub\",\"details\":\"Repository: badgoose/repo\"},{\"name\":\"Build\",\"category\":\"Build\",\"provider\":\"CodeBuild\",\"details\":\"BuildProject: pipeline-dinder-badgoose-repo-BuildProject\"},{\"name\":\"DeployTo-test\",\"category\":\"Deploy\",\"provider\":\"CloudFormation\",\"details\":\"StackName: dinder-test-test\"}],\"createdAt\":\"2020-02-02T15:04:05Z\",\"updatedAt\":\"2020-02-02T15:04:05Z\"}\n",
 		},
 	}
 	for _, tc := range testCases {
