8f9467669dac9478c975e8a14484c8b5e719a343
Janice <jwh@amazon.com>
2022-04-25 13:56:43 -0700

    chore: update buildspec

--- e2e/multi-pipeline/pipeline_suite_test.go
+++ e2e/multi-pipeline/pipeline_suite_test.go
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package pipeline_test
+package multi_pipeline_test
 
 import (
 	"fmt"
