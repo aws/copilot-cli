2bd2dbdf2b1f1c31cb1654678084e985bb5f0e45
Wanxian Yang <wanxiay@amazon.com>
2022-05-10 14:05:27 -0700

    remove unused func for static check

--- internal/pkg/cli/deploy/env.go
+++ internal/pkg/cli/deploy/env.go
@@ -7,40 +7,15 @@ import (
 	"fmt"
 
 	"github.com/aws/copilot-cli/internal/pkg/aws/s3"
-	"github.com/aws/copilot-cli/internal/pkg/aws/sessions"
 	"github.com/aws/copilot-cli/internal/pkg/config"
-	deploycfn "github.com/aws/copilot-cli/internal/pkg/deploy/cloudformation"
 	"github.com/aws/copilot-cli/internal/pkg/deploy/cloudformation/stack"
 	"github.com/aws/copilot-cli/internal/pkg/manifest"
-	"github.com/aws/copilot-cli/internal/pkg/template"
 	"github.com/aws/copilot-cli/internal/pkg/term/log"
 )
 
 type appResourcesGetter interface {
 	GetAppResourcesByRegion(app *config.Application, region string) (*stack.AppRegionalResources, error)
 	GetRegionalAppResources(app *config.Application) ([]*stack.AppRegionalResources, error)
-}
-
-type envDeployerInput struct {
-	app          *config.Application
-	env          *config.Environment
-	sessProvider *sessions.Provider
-}
-
-func newEnvDeployer(in *envDeployerInput) (*envDeployer, error) {
-	defaultSess, err := in.sessProvider.Default()
-	if err != nil {
-		return nil, fmt.Errorf("default session: %w", err)
-	}
-	envSess, err := in.sessProvider.FromRole(in.env.ManagerRoleARN, in.env.Region)
-	d := &envDeployer{
-		appCFN: deploycfn.New(defaultSess),
-
-		uploader: template.New(),
-		s3:       s3.New(envSess),
-	}
-	return d, nil
-
 }
 
 type envDeployer struct {
