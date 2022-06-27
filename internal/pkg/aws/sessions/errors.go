f5b9b90b79c08cf9bd247398edf64e8d8fb49bf0
Parag Bhingre <pbhingre@amazon.com>
2022-06-27 10:57:55 -0700

    make error message more human friendly

--- internal/pkg/aws/sessions/errors.go
+++ internal/pkg/aws/sessions/errors.go
@@ -37,11 +37,23 @@ func (e *errCredProviderTimeout) Error() string {
 // RecommendActions returns recommended actions to be taken after the error.
 // Implements main.actionRecommender interface.
 func (e *errCredProviderTimeout) RecommendActions() string {
-	return fmt.Sprintf(`It looks like your %s chain is missing or misconfigured:
+	var msg string
+	if e.profile == "default" {
+		msg = `It looks like your credential settings are misconfigured or missing:
 https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials
 - We recommend including your credentials in the shared credentials file.
-- Alternatively, you can also set credentials throguh 
+- Alternatively, you can also set credentials through
 	* Environment Variables
 	* EC2 Instance Metadata (credentials only)
-More information: https://aws.github.io/copilot-cli/docs/credentials/`, color.HighlightCode(e.profile))
+More information: https://aws.github.io/copilot-cli/docs/credentials/`
+	} else {
+		msg = `It looks like your profile [%s] is misconfigured or missing:
+https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html#specifying-credentials
+- We recommend including your credentials in the shared credentials file.
+- Alternatively, you can also set credentials through 
+	* Environment Variables
+	* EC2 Instance Metadata (credentials only)
+More information: https://aws.github.io/copilot-cli/docs/credentials/`
+	}
+	return fmt.Sprintf(msg, color.HighlightCode(e.profile))
 }
