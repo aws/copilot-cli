54bbf4312b734e35ab91466b0d037eecb5f692a2
Austin Ely <austiely@amazon.com>
2022-07-13 14:03:41 -0400

    chore: update cloudwatchlogs to allow limiting log streams fetched and custom prefixes

--- internal/pkg/aws/cloudwatchlogs/cloudwatchlogs.go
+++ internal/pkg/aws/cloudwatchlogs/cloudwatchlogs.go
@@ -109,8 +109,7 @@ func (c *CloudWatchLogs) logStreams(logGroup string, logStreamLimit int, logStre
 func (c *CloudWatchLogs) LogEvents(opts LogEventsOpts) (*LogEventsOutput, error) {
 	var events []*Event
 	in := initGetLogEventsInput(opts)
-
-	logStreams, err := c.logStreams(opts.LogGroup, opts.LogStreamLimit, opts.LogStreamPrefixFilters...)
+	logStreams, err := c.logStreams(opts.LogGroup, opts.LogStreams...)
 	if err != nil {
 		return nil, err
 	}
