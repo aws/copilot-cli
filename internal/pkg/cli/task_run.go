30482491a8428e29b4ff17bb229a0ab13c59bed0
Ryan Linard <rclinard@amazon.com>
2022-07-26 10:35:58 -0700

    fix(task_run): Avoid unintuitive error with --default and --env-file

--- internal/pkg/cli/task_run.go
+++ internal/pkg/cli/task_run.go
@@ -602,6 +602,7 @@ func (o *runTaskOpts) Ask() error {
 	if o.generateCommandTarget != "" {
 		return nil
 	}
+
 	if o.shouldPromptForAppEnv() {
 		if err := o.askAppName(); err != nil {
 			return err
@@ -609,7 +610,12 @@ func (o *runTaskOpts) Ask() error {
 		if err := o.askEnvName(); err != nil {
 			return err
 		}
+	} else if o.envFile != "" {
+		if err := o.askAppName(); err != nil {
+			return fmt.Errorf("no S3 bucket available to hold environment file %s specified in --%s: %w", o.envFile, envFileFlag, err)
+		}
 	}
+
 	if len(o.secrets) > 0 {
 		if err := o.confirmSecretsAccess(); err != nil {
 			return err
