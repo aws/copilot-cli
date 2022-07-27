b103d983e7e51edc40999b2438c2eb35b5c84f04
Daniel Randall <10566468+dannyrandall@users.noreply.github.com>
2022-07-27 16:40:35 -0700

    add license

--- internal/pkg/cli/delete/delete.go
+++ internal/pkg/cli/delete/delete.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 // package delete contains common functions for deleting resources
 // created through copilot.
 package delete
