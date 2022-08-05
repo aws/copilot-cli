e5ae596feb2a230e164cc01aee8a149938f3b7bf
Wanxian Yang <wanxiay@amazon.com>
2022-08-05 10:03:51 -0700

    set up make regression-dryrun

--- scripts/dryrun-creds.sh
+++ scripts/dryrun-creds.sh
@@ -1,19 +1,26 @@
 #! /usr/bin/env python3
 import subprocess
+import sys
 
 cmd = subprocess.run(['aws', 'configure', 'list-profiles'], stdout=subprocess.PIPE)
 profiles = cmd.stdout.decode('utf-8').split('\n')
 
-for e2eprofile in ['e2etestenv', 'e2eprodenv']:
-  if e2eprofile in profiles:
+if sys.argv[1] == "e2e":
+  requiredProfiles = ['e2etestenv', 'e2eprodenv']
+elif sys.argv[1] == "regression":
+  requiredProfiles = ['regression']
+
+for profile in requiredProfiles:
+  print(profile)
+  if profile in profiles:
     continue
-  print(f'Profile [{e2eprofile}] is required but does not exist. Copying your [default] access key id and secret key...')
+  print(f'Profile [{profile}] is required but does not exist. Copying your [default] access key id and secret key...')
   cmd = subprocess.run(['aws', 'configure', 'get', 'default.region'], stdout=subprocess.PIPE)
   region = cmd.stdout.decode('utf-8').strip()
   cmd = subprocess.run(['aws', 'configure', 'get', 'default.aws_access_key_id'], stdout=subprocess.PIPE)
   access_key = cmd.stdout.decode('utf-8').strip()
   cmd = subprocess.run(['aws', 'configure', 'get', 'default.aws_secret_access_key'], stdout=subprocess.PIPE)
   secret = cmd.stdout.decode('utf-8').strip()
-  subprocess.run(['aws', 'configure', 'set', 'region', region, '--profile', e2eprofile])
-  subprocess.run(['aws', 'configure', 'set', 'aws_access_key_id', access_key, '--profile', e2eprofile])
-  subprocess.run(['aws', 'configure', 'set', 'aws_secret_access_key', secret, '--profile', e2eprofile])
+  subprocess.run(['aws', 'configure', 'set', 'region', region, '--profile', profile])
+  subprocess.run(['aws', 'configure', 'set', 'aws_access_key_id', access_key, '--profile', profile])
+  subprocess.run(['aws', 'configure', 'set', 'aws_secret_access_key', secret, '--profile', profile])
