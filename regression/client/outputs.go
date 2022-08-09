080b506cb79e41a987d071f03820b1e6961e581d
Wanxian Yang <wanxiay@amazon.com>
2022-08-09 11:26:55 -0700

    export structs that are returned from exported functions

--- regression/client/outputs.go
+++ regression/client/outputs.go
@@ -4,89 +4,80 @@
 package client
 
 import (
-	"encoding/json"
+       "encoding/json"
 )
 
-type appShowOutput struct {
-	Name string `json:"name"`
-	URI  string `json:"uri"`
+type wkldDescription struct {
+       Name    string `json:"name"`
+       Type    string `json:"type"`
+       AppName string `json:"app"`
 }
 
-// ToAppShowOutput unmarshal a JSON string to an appShowOutput struct.
-func ToAppShowOutput(jsonInput string) (*appShowOutput, error) {
-	var output appShowOutput
-	return &output, json.Unmarshal([]byte(jsonInput), &output)
+// SvcListOutput contains summaries of services.
+type SvcListOutput struct {
+       Services []wkldDescription `json:"services"`
 }
 
-// WkldDescription describes a workload.
-type WkldDescription struct {
-	Name    string `json:"name"`
-	Type    string `json:"type"`
-	AppName string `json:"app"`
+// ToSvcListOutput unmarshal a JSON string to a SvcListOutput struct.
+func ToSvcListOutput(jsonInput string) (*SvcListOutput, error) {
+       var output SvcListOutput
+       return &output, json.Unmarshal([]byte(jsonInput), &output)
 }
 
-type svcListOutput struct {
-	Services []WkldDescription `json:"services"`
-}
-
-// ToSvcListOutput unmarshal a JSON string to a svcListOutput struct.
-func ToSvcListOutput(jsonInput string) (*svcListOutput, error) {
-	var output svcListOutput
-	return &output, json.Unmarshal([]byte(jsonInput), &output)
-}
-
-type svcShowOutput struct {
-	SvcName            string                            `json:"service"`
-	Type               string                            `json:"type"`
-	AppName            string                            `json:"application"`
-	Configs            []svcShowConfigurations           `json:"configurations"`
-	ServiceDiscoveries []svcShowServiceDiscoveries       `json:"serviceDiscovery"`
-	Routes             []svcShowRoutes                   `json:"routes"`
-	Variables          []svcShowVariables                `json:"variables"`
-	Resources          map[string][]*svcShowResourceInfo `json:"resources"`
+// SvcShowOutput contains detailed information about a service.
+type SvcShowOutput struct {
+       SvcName            string                            `json:"service"`
+       Type               string                            `json:"type"`
+       AppName            string                            `json:"application"`
+       Configs            []svcShowConfigurations           `json:"configurations"`
+       ServiceDiscoveries []svcShowServiceDiscoveries       `json:"serviceDiscovery"`
+       Routes             []svcShowRoutes                   `json:"routes"`
+       Variables          []svcShowVariables                `json:"variables"`
+       Resources          map[string][]*svcShowResourceInfo `json:"resources"`
 }
 
 type svcShowConfigurations struct {
-	Environment string `json:"environment"`
-	Port        string `json:"port"`
-	Tasks       string `json:"tasks"`
-	CPU         string `json:"cpu"`
-	Memory      string `json:"memory"`
+       Environment string `json:"environment"`
+       Port        string `json:"port"`
+       Tasks       string `json:"tasks"`
+       CPU         string `json:"cpu"`
+       Memory      string `json:"memory"`
 }
 
 type svcShowRoutes struct {
-	Environment string `json:"environment"`
-	URL         string `json:"url"`
+       Environment string `json:"environment"`
+       URL         string `json:"url"`
 }
 
 type svcShowServiceDiscoveries struct {
-	Environment []string `json:"environment"`
-	Namespace   string   `json:"namespace"`
+       Environment []string `json:"environment"`
+       Namespace   string   `json:"namespace"`
 }
 
 type svcShowVariables struct {
-	Environment string `json:"environment"`
-	Name        string `json:"name"`
-	Value       string `json:"value"`
+       Environment string `json:"environment"`
+       Name        string `json:"name"`
+       Value       string `json:"value"`
 }
 
 type svcShowResourceInfo struct {
-	Type       string `json:"type"`
-	PhysicalID string `json:"physicalID"`
+       Type       string `json:"type"`
+       PhysicalID string `json:"physicalID"`
 }
 
-// ToSvcShowOutput unmarshal a JSON string to a svcShowOutput struct.
-func ToSvcShowOutput(jsonInput string) (*svcShowOutput, error) {
-	var output svcShowOutput
-	return &output, json.Unmarshal([]byte(jsonInput), &output)
+// ToSvcShowOutput unmarshal a JSON string to a SvcShowOutput struct.
+func ToSvcShowOutput(jsonInput string) (*SvcShowOutput, error) {
+       var output SvcShowOutput
+       return &output, json.Unmarshal([]byte(jsonInput), &output)
 }
 
-type jobListOutput struct {
-	Jobs []WkldDescription `json:"jobs"`
+// JobListOutput contains summaries of jobs. 
+type JobListOutput struct {
+       Jobs []wkldDescription `json:"jobs"`
 }
 
-// ToJobListOutput unmarshal a JSON string to a jobListOutput struct.
-func ToJobListOutput(jsonInput string) (*jobListOutput, error) {
-	var output jobListOutput
-	return &output, json.Unmarshal([]byte(jsonInput), &output)
+// ToJobListOutput unmarshal a JSON string to a JobListOutput struct.
+func ToJobListOutput(jsonInput string) (*JobListOutput, error) {
+       var output JobListOutput
+       return &output, json.Unmarshal([]byte(jsonInput), &output)
 }
