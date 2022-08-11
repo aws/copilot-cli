8bd8e6a08b82cff0e7c38c9968b907c56c89cf84
Daniel Randall <10566468+dannyrandall@users.noreply.github.com>
2022-08-11 12:48:48 -0700

    fix test and add template comment

--- internal/pkg/deploy/upload/customresource/customresource_test.go
+++ internal/pkg/deploy/upload/customresource/customresource_test.go
@@ -322,7 +322,7 @@ func TestEnv(t *testing.T) {
 		"CustomDomainFunction":          "manual/scripts/custom-resources/customdomainfunction/01baf83827dca2ff7df3cdf24f6ad354b3fa4f9b7cda39b5bf91de378f81c791.zip",
 		"DNSDelegationFunction":         "manual/scripts/custom-resources/dnsdelegationfunction/17ec5f580cdb9c1d7c6b5b91decee031592547629a6bfed7cd33b9229f61ab19.zip",
 		"CertificateReplicatorFunction": "manual/scripts/custom-resources/certificatereplicatorfunction/647f83437e4736ddf2915784e13d023a7d342d162ffb42a9eec3d7c842072030.zip",
-		"UniqueJSONValuesFunction":      "manual/scripts/custom-resources/uniquejsonvaluesfunction/22a287579888f9abaf4fc1ef09dbf8b895dd1b7191673c46311264371872c4b8.zip",
+		"UniqueJSONValuesFunction":      "manual/scripts/custom-resources/uniquejsonvaluesfunction/68c7ace14491d82ac4bb5ad81b3371743d669a26638f419265c18e9bdfca8dd1.zip",
 	}
 
 	// WHEN
