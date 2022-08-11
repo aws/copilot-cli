296a0c631854b80d5b59b01d2f91910517ed83f5
Ryan Linard <rclinard@amazon.com>
2022-08-11 13:05:43 -0700

    chore: add license headers

--- internal/pkg/docker/dockercompose/unsupported.go
+++ internal/pkg/docker/dockercompose/unsupported.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package dockercompose
 
 // NOTE: This file is currently unused, it will be used in the next PR.
