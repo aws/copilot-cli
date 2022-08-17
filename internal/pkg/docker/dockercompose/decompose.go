92794f7924616fdcb31b24827384e20627202fac
Ryan Linard <rclinard@amazon.com>
2022-08-17 14:09:14 -0700

    chore: break off some functions from DecomposeService

--- internal/pkg/docker/dockercompose/decompose.go
+++ internal/pkg/docker/dockercompose/decompose.go
@@ -18,47 +18,14 @@ func DecomposeService(content []byte, svcName string) (*manifest.BackendServiceC
 		return nil, nil, fmt.Errorf("parse compose yaml: %w", err)
 	}
 
-	// scan over fields in the parsed yaml to find unsupported fields instead of using
-	// boilerplate code or complex reflection
-	svcs, ok := config["services"]
-	if !ok || svcs == nil {
-		return nil, nil, fmt.Errorf("compose file has no services")
+	service, err := serviceConfig(config, svcName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("get service config: %w", err)
 	}
 
-	services, ok := svcs.(map[string]interface{})
-	if !ok {
-		return nil, nil, fmt.Errorf("\"services\" top-level element was not a map, was: %v", svcs)
-	}
-
-	svc, ok := services[svcName]
-	if !ok {
-		var validNames []string
-		for svc := range services {
-			validNames = append(validNames, svc)
-		}
-		sort.Strings(validNames)
-		return nil, nil, fmt.Errorf("no service named \"%s\" in this Compose file, valid services are: %v", svcName, validNames)
-	}
-
-	service, ok := svc.(map[string]interface{})
-	if !ok {
-		return nil, nil, fmt.Errorf("\"services.%s\" element was not a map", svcName)
-	}
-
-	var ignored, fatal []string
-
-	for key := range service {
-		if _, ok := ignoredServiceKeys[key]; ok {
-			ignored = append(ignored, key)
-		} else if _, ok := fatalServiceKeys[key]; ok {
-			fatal = append(fatal, key)
-		}
-	}
-
-	if len(fatal) != 0 {
-		// sort so we have consistent (testable) error messages
-		sort.Strings(fatal)
-		return nil, nil, fmt.Errorf("\"services.%s\" relies on fatally-unsupported Compose keys: %v", svcName, fatal)
+	ignored, err := unsupportedServiceKeys(service, svcName)
+	if err != nil {
+		return nil, nil, fmt.Errorf("get unsupported service keys: %w", err)
 	}
 
 	project, err := loader.Load(compose.ConfigDetails{
@@ -69,7 +36,6 @@ func DecomposeService(content []byte, svcName string) (*manifest.BackendServiceC
 			},
 		},
 	})
-
 	if err != nil {
 		return nil, nil, fmt.Errorf("load Compose project: %w", err)
 	}
@@ -94,4 +60,55 @@ func DecomposeService(content []byte, svcName string) (*manifest.BackendServiceC
 
 	sort.Strings(ignored)
 	return backendSvc, ignored, nil
+}
+
+// serviceConfig extracts the map corresponding to a Compose service from the parsed Compose yaml
+func serviceConfig(config map[string]interface{}, svcName string) (map[string]interface{}, error) {
+	svcs, ok := config["services"]
+	if !ok || svcs == nil {
+		return nil, fmt.Errorf("compose file has no services")
+	}
+
+	services, ok := svcs.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("\"services\" top-level element was not a map, was: %v", svcs)
+	}
+
+	svc, ok := services[svcName]
+	if !ok {
+		var validNames []string
+		for svc := range services {
+			validNames = append(validNames, svc)
+		}
+		sort.Strings(validNames)
+		return nil, fmt.Errorf("no service named \"%s\" in this Compose file, valid services are: %v", svcName, validNames)
+	}
+
+	service, ok := svc.(map[string]interface{})
+	if !ok {
+		return nil, fmt.Errorf("\"services.%s\" element was not a map", svcName)
+	}
+
+	return service, nil
+}
+
+// unsupportedServiceKeys scans over fields in the parsed yaml to find unsupported keys
+func unsupportedServiceKeys(service map[string]interface{}, svcName string) (IgnoredKeys, error) {
+	var ignored, fatal []string
+
+	for key := range service {
+		if _, ok := ignoredServiceKeys[key]; ok {
+			ignored = append(ignored, key)
+		} else if _, ok := fatalServiceKeys[key]; ok {
+			fatal = append(fatal, key)
+		}
+	}
+
+	if len(fatal) != 0 {
+		// sort so we have consistent (testable) error messages
+		sort.Strings(fatal)
+		return nil, fmt.Errorf("\"services.%s\" relies on fatally-unsupported Compose keys: %v", svcName, fatal)
+	}
+
+	return ignored, nil
 }
