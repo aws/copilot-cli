1bf6eddf6dd841e3bfac3c554153df25e1cf6eb5
Janice <jwh@amazon.com>
2022-09-14 11:31:49 -0700

    chore: restore spaces

--- internal/pkg/cli/env_init.go
+++ internal/pkg/cli/env_init.go
@@ -285,6 +285,7 @@ func (o *initEnvOpts) Execute() error {
 	// If the call fails because the role already exists, nothing to do.
 	// If the call fails because the user doesn't have permissions, then the role must be created outside of Copilot.
 	_ = o.iam.CreateECSServiceLinkedRole()
+	
 	// 4. Add the stack set instance to the app stackset.
 	if err := o.addToStackset(&deploycfn.AddEnvToAppOpts{
 		App:          app,
@@ -299,6 +300,7 @@ func (o *initEnvOpts) Execute() error {
 	if err := o.deployEnv(app); err != nil {
 		return err
 	}
+
 	// 6. Store the environment in SSM with information about the deployed bootstrap roles.
 	env, err := o.envDeployer.GetEnvironment(o.appName, o.name)
 	if err != nil {
