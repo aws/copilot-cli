bc61a1c3c73a9effad3274600268ac656b1c4d6f
Janice <jwh@amazon.com>
2022-09-30 15:04:00 -0700

    chore: switch require.True to require.Equal

--- internal/pkg/template/permissions_boundary_integration_test.go
+++ internal/pkg/template/permissions_boundary_integration_test.go
@@ -7,7 +7,6 @@ package template
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/stretchr/testify/require"
 	"io/fs"
 	"os"
@@ -19,11 +18,10 @@ func TestPermissions_Boundary(t *testing.T) {
 	t.Run("every CloudFormation template must contain conditional permissions boundary field for all IAM roles", func(t *testing.T) {
 		err := filepath.WalkDir("templates", func(path string, di fs.DirEntry, err error) error {
 			contents, _ := os.ReadFile(path)
-			IAMRoles := bytes.Count(contents, []byte("AWS::IAM::Role"))
-			permissionsBoundaryFields := bytes.Count(contents, []byte("PermissionsBoundary:"))
+			roleCount := bytes.Count(contents, []byte("AWS::IAM::Role"))
+			pbFieldCount := bytes.Count(contents, []byte("PermissionsBoundary:"))
 
-			msg := fmt.Sprintf("number of IAM roles (%d) does not equal number of permissions boundary fields (%d) in file '%s'", IAMRoles, permissionsBoundaryFields, path)
-			require.True(t, IAMRoles == permissionsBoundaryFields, msg)
+			require.Equal(t, roleCount, pbFieldCount, "number of IAM roles does not equal number of permissions boundary fields in file '%s'", path)
 			return nil
 		})
 		require.NoError(t, err, "should walk templates dir for template files")
