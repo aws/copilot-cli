0ff5b3705d0b92ca9f463aadf0cf2c0d3ad49140
Hidekazu Karino <hidekazu.karino.j@gmail.com>
2022-10-11 16:43:55 +0900

    fix: storageRDSServerlessVersionFlagDescription to lowercase

--- internal/pkg/cli/flag.go
+++ internal/pkg/cli/flag.go
@@ -280,7 +280,7 @@ Must be of the format '<keyName>:<dataType>'.`
 	storageLSIConfigFlagDescription = `Optional. Attribute to use as an alternate sort key. May be specified up to 5 times.
 Must be of the format '<keyName>:<dataType>'.`
 	storageRDSServerlessVersionFlagDescription = `Aurora Serverless version.
-Must be of either "V1" or "V2"`
+Must be either "v1" or "v2".`
 	storageRDSEngineFlagDescription = `The database engine used in the cluster.
 Must be either "MySQL" or "PostgreSQL".`
 	storageRDSInitialDBFlagDescription      = "The initial database to create in the cluster."
