be19cfaf5ec36e732569813da99c3adbdb663fc0
Wanxian Yang <wanxiay@amazon.com>
2022-12-13 11:07:17 -0800

    add comment for exported var

--- internal/pkg/deploy/env.go
+++ internal/pkg/deploy/env.go
@@ -14,7 +14,8 @@ const (
 	// LegacyEnvTemplateVersion is the version associated with the environment template before we started versioning.
 	LegacyEnvTemplateVersion = "v0.0.0"
 	// LatestEnvTemplateVersion is the latest version number available for environment templates.
-	LatestEnvTemplateVersion    = "v1.13.0"
+	LatestEnvTemplateVersion = "v1.13.0"
+	// EnvTemplateVersionBootstrap is the version of an environment template that contains only bootstrap resources.
 	EnvTemplateVersionBootstrap = "bootstrap"
 )
 
