0b9299cacf7c43203bd0f74a827b1b4ef09875c5
Efe Karakus <efekarakus@gmail.com>
2022-12-20 16:48:20 -0800

    chore(templates): add scaffold files for CDK overrides/ (#4291)
    Related #4208 
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

---
+++ internal/pkg/template/templates/overrides/cdk/stack.ts
@@ -0,0 +1,28 @@
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
+{{- range $resourceType := .Resources.Types }}
+import { {{$resourceType.ImportName}} as {{$resourceType.ImportShortRename}} } from 'aws-cdk-lib';
+{{- end }}
+
+export class TransformedStack extends cdk.Stack {
+    constructor (scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+        this.template = new cdk.cloudformation_include.CfnInclude(this, 'Template', {
+            templateFile: path.join('.build', 'in.yml'),
+        });
+        this.appName = this.template.getParameter('AppName').valueAsString;
+        this.envName = this.template.getParameter('EnvName').valueAsString;
+
+        {{- range $resource := .Resources }}
+        this.transform{{camelCase $resource.LogicalID}}();
+        {{- end }}
+    }
+
+    {{- range $resource := .Resources}}
+    // TODO: implement me.
+    transform{{camelCase $resource.LogicalID}}() {
+        const {{camelCase $resource.LogicalID}} = this.template.getResource("{{$resource.LogicalID}}") as {{$resource.Type.ImportShortRename}}.{{$resource.Type.L1ConstructName}};
+        throw new error("not implemented");
+    }
+    {{- end }}
+}
