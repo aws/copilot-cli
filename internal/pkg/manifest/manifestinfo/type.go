06193af714c78f61020f32751e6096a06fb26a9d
penghaoh <penghaoh@amazon.com>
2023-02-09 11:16:48 -0800

    Add unit test

--- internal/pkg/manifest/manifestinfo/type.go
+++ internal/pkg/manifest/manifestinfo/type.go
@@ -1,6 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+// Package manifestinfo provides access to information embedded in a manifest.
 package manifestinfo
 
 const (
