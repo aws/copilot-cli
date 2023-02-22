854fa51aaac1b94882744c679bf3bb94b0b4f000
Efe Karakus <efekarakus@gmail.com>
2023-02-22 14:25:12 -0800

    refactor(selector): expose Prompter interface (#4538)
    The interface is an input in our constructors, making it public.
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/term/selector/ec2.go
+++ internal/pkg/term/selector/ec2.go
@@ -20,12 +20,12 @@ type VPCSubnetLister interface {
 
 // EC2Select is a selector for Ec2 resources.
 type EC2Select struct {
-	prompt prompter
+	prompt Prompter
 	ec2Svc VPCSubnetLister
 }
 
 // NewEC2Select returns a new selector that chooses Ec2 resources.
-func NewEC2Select(prompt prompter, ec2Client VPCSubnetLister) *EC2Select {
+func NewEC2Select(prompt Prompter, ec2Client VPCSubnetLister) *EC2Select {
 	return &EC2Select{
 		prompt: prompt,
 		ec2Svc: ec2Client,
