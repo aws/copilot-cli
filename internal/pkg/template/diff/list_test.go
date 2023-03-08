a4b8e3d4268de86044f2de282a011d6f53881a1c
Wanxian Yang <wanxiay@amazon.com>
2023-03-08 15:53:21 -0800

    udpate test

--- internal/pkg/template/diff/list_test.go
+++ internal/pkg/template/diff/list_test.go
@@ -11,44 +11,66 @@ import (
 )
 
 func Test_longestCommonSubsequence(t *testing.T) {
-	testCases := map[string]struct {
+	testCases := []struct {
 		inA    []string
 		inB    []string
 		wanted []string
 	}{
-		"1": {
+		{
 			inA:    []string{"a", "b", "c"},
 			inB:    []string{"a", "b", "c"},
 			wanted: []string{"a", "b", "c"},
 		},
-		"2": {
+		{
 			inA:    []string{"a", "b", "c"},
 			inB:    []string{"c"},
 			wanted: []string{"c"},
 		},
-		"3": {
+		{
 			inA:    []string{"a", "b", "c"},
 			inB:    []string{"a", "B", "c"},
 			wanted: []string{"a", "c"},
 		},
-		"4": {
+		{
 			inA:    []string{"a", "b", "c"},
 			inB:    []string{"a", "B", "C"},
 			wanted: []string{"a"},
 		},
-		"5": {
+		{
 			inA:    []string{"a", "c", "b", "b", "d"},
 			inB:    []string{"a", "B", "b", "c", "c", "d"},
 			wanted: []string{"a", "b", "d"}, // NOTE: a, c, d is also correct
 		},
-		"6": {
+		{
 			inA:    []string{"a", "b", "B", "B", "c", "d", "D", "d", "e", "f"},
 			inB:    []string{"a", "B", "C", "d", "d", "e", "f"},
 			wanted: []string{"a", "B", "d", "d", "e", "f"},
 		},
+		{
+			inB: []string{},
+		},
+		{
+			inA:    []string{"a"},
+			wanted: nil,
+		},
+		{
+			inA:    []string{"a"},
+			inB:    []string{"a"},
+			wanted: []string{"a"},
+		},
+		{
+			inA:    []string{"a"},
+			inB:    []string{"b"},
+			wanted: nil,
+		},
+		{
+			inA:    []string{"a", "b", "c", "c"},
+			inB:    []string{"c"},
+			wanted: []string{"c"},
+		},
 	}
 	for idx, tc := range testCases {
-		t.Run(fmt.Sprintf("casess %s", idx), func(t *testing.T) {
+		t.Run(fmt.Sprintf("casess %v", idx), func(t *testing.T) {
 			require.Equal(t, tc.wanted, longestCommonSubsequence(tc.inA, tc.inB))
 		})
 	}
