7c4f5da1f8373f83175814147fe9090947494810
Wanxian Yang <wanxiay@amazon.com>
2023-03-13 09:31:40 -0700

    addr fb: add comment"

--- internal/pkg/template/diff/iter.go
+++ internal/pkg/template/diff/iter.go
@@ -47,10 +47,13 @@ func (i *inspector) inspect() action {
 		case fromDone && toDone:
 			action = done
 		case toDone:
+			// Ex: "a,d,e" -> "a". When the inspector moves to "d" in "from", both common and "to" are done, and "d,e" are considered deleted.
 			action = deletion
 		case fromDone:
+			// Ex: "a" -> "a,d,e". When the inspector moves to "d" in "to", both common and "from" are done, and "d,e" are considered to be inserted.
 			action = insertion
 		default:
+			// Ex: "a,b" -> "a,c". When the inspector moves to index 1 of both list, common is done, and b is modified into c.
 			action = modification
 		}
 		i.currAction = action
@@ -63,8 +66,10 @@ func (i *inspector) inspect() action {
 	case i.from.index != commonIdx.inA && i.to.index != commonIdx.inB:
 		action = modification
 	case i.from.index != commonIdx.inA:
+		// Ex: "a,b,c" -> "c,1,2". When the inspector is at (a,c /(b,c), only "c" is common, a,b are considered deleted.
 		action = deletion
 	default:
+		// Ex: "a,b,c" -> "1,2,a". When the inspector is at (a,1) and (a,2), only "a" is common, "1,2" are considered inserted.
 		action = insertion
 	}
 	i.currAction = action
