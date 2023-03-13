eda0f814832fd689ab7e4448f09655a6eeddc3b8
Wanxian Yang <wanxiay@amazon.com>
2023-03-13 09:35:31 -0700

    addr fb: comment about lcs eq

--- internal/pkg/template/diff/lcs.go
+++ internal/pkg/template/diff/lcs.go
@@ -18,6 +18,7 @@ type lcsIndex struct {
 //   One LCS is ["a","c","d"].
 //   "a" is input_a[0] and input_b[0], "c" is in input_a[1] and input_b[3], "d" is input_a[4] and input_b[5].
 //   Therefore, the output will be [0,1,4], [0,3,5]
+// Note that the parameter function `eq` is guaranteed to be called on every combination of a's and b's items.
 func longestCommonSubsequence[T any](a []T, b []T, eq eqFunc) []lcsIndex {
 	if len(a) == 0 || len(b) == 0 {
 		return nil
