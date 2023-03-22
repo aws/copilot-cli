3b2a9e9b44c8a62980f0bb83f1a98e8bef64f4c4
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-03-22 16:17:36 -0700

    modified check for overrides

--- internal/pkg/override/override.go
+++ internal/pkg/override/override.go
@@ -67,35 +67,20 @@ func (i Info) IsYAMLPatch() bool {
 // If path is a YAML patch document, then IsYAMLPatch evaluates to true.
 // If path is a directory that contains a cdk.json file, then IsCDK evaluates to true.
 func Lookup(path string, fs afero.Fs) (Info, error) {
-	stat, err := fs.Stat(path)
-	if err != nil {
-		return Info{}, &ErrNotExist{parent: err}
-	}
-
-	if !stat.IsDir() {
-		return lookupYAMLPatch(path, fs)
-	}
-
 	files, err := afero.ReadDir(fs, path)
-	if err != nil {
+	switch {
+	case err != nil:
 		return Info{}, fmt.Errorf("read directory %q: %w", path, err)
-	}
-	var info Info
-	switch n := len(files); n {
-	case 0:
+	case len(files) == 0:
 		return Info{}, fmt.Errorf(`directory at %q is empty`, path)
-	case 1:
-		info, err = lookupYAMLPatch(filepath.Join(path, files[0].Name()), fs)
-		if err != nil {
-			return Info{}, fmt.Errorf("look up YAML patch document when directory contains a single file: %w", err)
-		}
-	default:
-		info, err = lookupCDK(path, fs)
-		if err != nil {
-			return Info{}, fmt.Errorf("look up CDK project for directories with multiple files: %w", err)
-		}
 	}
-	return info, nil
+
+	info, err := lookupYAMLPatch(path, fs)
+	if err == nil { // return yaml info if no error
+		return info, nil
+	}
+
+	return lookupCDK(path, fs)
 }
 
 type extension string
@@ -103,14 +88,12 @@ type extension string
 func (ext extension) isYAML() bool { return ext == ".yml" || ext == ".yaml" }
 
 func lookupYAMLPatch(path string, fs afero.Fs) (Info, error) {
-	if ext := extension(filepath.Ext(path)); !ext.isYAML() {
-		return Info{}, fmt.Errorf(`YAML patch documents require a ".yml" or ".yaml" extension: %q has a %q extension`, path, ext)
+	fpath := filepath.Join(path, "cfn.patches.yml")
+	content, err := afero.ReadFile(fs, fpath)
+	if err != nil {
+		return Info{}, fmt.Errorf("read file at %q: %w", fpath, err)
 	}
 
-	content, err := afero.ReadFile(fs, path)
-	if err != nil {
-		return Info{}, fmt.Errorf("read file at %q: %w", path, err)
-	}
 	type yamlPatch struct {
 		Op   string `yaml:"op"`
 		Path string `yaml:"path"`
