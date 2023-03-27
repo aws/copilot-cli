581e0847f1bc5c7da31db844afd97b3fb6bb9654
Adithya Kolla <71282729+KollaAdithya@users.noreply.github.com>
2023-03-27 13:15:48 -0700

    chore: revert "refactor: Login to docker client only once for all images" (#4693)
    Reverts aws/copilot-cli#4653
    With this previous change Login to ECR is performed even when we run `copilot svc override` because the Login happens in the workload constructor and the `docker login` command prints `Login Succeeded`.
    ![image](https://user-images.githubusercontent.com/71282729/228046416-2c457184-8e85-4bf7-98fe-762c4d96d000.png)

--- internal/pkg/cli/mocks/mock_interfaces.go
+++ internal/pkg/cli/mocks/mock_interfaces.go
@@ -1615,44 +1615,6 @@ func (mr *MockrepositoryURIGetterMockRecorder) URI() *gomock.Call {
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "URI", reflect.TypeOf((*MockrepositoryURIGetter)(nil).URI))
 }
 
-// MockdockerLogin is a mock of dockerLogin interface.
-type MockdockerLogin struct {
-	ctrl     *gomock.Controller
-	recorder *MockdockerLoginMockRecorder
-}
-
-// MockdockerLoginMockRecorder is the mock recorder for MockdockerLogin.
-type MockdockerLoginMockRecorder struct {
-	mock *MockdockerLogin
-}
-
-// NewMockdockerLogin creates a new mock instance.
-func NewMockdockerLogin(ctrl *gomock.Controller) *MockdockerLogin {
-	mock := &MockdockerLogin{ctrl: ctrl}
-	mock.recorder = &MockdockerLoginMockRecorder{mock}
-	return mock
-}
-
-// EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockdockerLogin) EXPECT() *MockdockerLoginMockRecorder {
-	return m.recorder
-}
-
-// Login mocks base method.
-func (m *MockdockerLogin) Login(docker repository.ContainerLoginBuildPusher) (string, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Login", docker)
-	ret0, _ := ret[0].(string)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// Login indicates an expected call of Login.
-func (mr *MockdockerLoginMockRecorder) Login(docker interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockdockerLogin)(nil).Login), docker)
-}
-
 // MockrepositoryService is a mock of repositoryService interface.
 type MockrepositoryService struct {
 	ctrl     *gomock.Controller
@@ -1689,21 +1651,6 @@ func (m *MockrepositoryService) BuildAndPush(docker repository.ContainerLoginBui
 func (mr *MockrepositoryServiceMockRecorder) BuildAndPush(docker, args interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "BuildAndPush", reflect.TypeOf((*MockrepositoryService)(nil).BuildAndPush), docker, args)
-}
-
-// Login mocks base method.
-func (m *MockrepositoryService) Login(docker repository.ContainerLoginBuildPusher) (string, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Login", docker)
-	ret0, _ := ret[0].(string)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// Login indicates an expected call of Login.
-func (mr *MockrepositoryServiceMockRecorder) Login(docker interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockrepositoryService)(nil).Login), docker)
 }
 
 // URI mocks base method.
