4f7e33de9c4daafc6828f5dd0cf298032439031d
Janice Huang <60631893+huanjani@users.noreply.github.com>
2023-03-28 15:46:35 -0700

    Revert "chore(e2e): use unique CloudFront domain name (#4698)"
    This reverts commit 036c09c55bc61e141b023c79777651a580018221.

--- e2e/cloudfront/cloudfront_suite_test.go
+++ e2e/cloudfront/cloudfront_suite_test.go
@@ -25,7 +25,7 @@ var s3Client *s3.S3
 var s3Manager *s3manager.Uploader
 var staticPath string
 
-var domainName = fmt.Sprintf("%d.copilot-e2e-tests.ecs.aws.dev", time.Now().Unix())
+const domainName = "copilot-e2e-tests.ecs.aws.dev"
 
 func TestCloudFront(t *testing.T) {
 	RegisterFailHandler(Fail)
