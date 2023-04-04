dc1bea962ac9920984ab961a1d69e17e7b736bde
Adithya Kolla <kolladit@amazon.com>
2023-04-04 12:33:06 -0700

    remove license header

--- internal/pkg/term/syncbuffer/syncbuffer.go
+++ internal/pkg/term/syncbuffer/syncbuffer.go
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 // Package syncbuffer provides a synchronized buffer to store and access logs from multiple goroutines.
 package syncbuffer
 
