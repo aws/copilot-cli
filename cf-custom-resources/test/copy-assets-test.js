69029f0d78af18dc2d198255f8b44727f2014c01
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-04-19 14:40:24 -0700

    copy assets tests

---
+++ cf-custom-resources/test/copy-assets-test.js
@@ -0,0 +1,66 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+"use strict";
+
+const aws = require("aws-sdk-mock");
+const lambdaTester = require("lambda-tester").noVersionCheck();
+const sinon = require("sinon");
+const copyAssets = require("../lib/copy-assets");
+
+describe("copy assets", () => {
+  const origConsole = console;
+
+  afterEach(() => {
+    aws.restore();
+  });
+
+  afterAll(() => {
+    console = origConsole;
+  });
+
+  test("happy path", () => {
+    const fake = sinon.fake.resolves({});
+    aws.mock("S3", "copyObject", fake);
+
+    return lambdaTester(copyAssets.handler)
+      .event({
+        srcBucket: "mockSrcBucket",
+        destBucket: "mockDestBucket",
+        mapping: {
+          path: "mockPath",
+          destPath: "mockDestPath",
+        }
+      })
+      .expectResolve(result => {
+        sinon.assert.calledWith(fake, {
+          CopySource: "mockSrcBucket/mockPath",
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+      });
+  });
+
+  test("s3 error", () => {
+    const fake = sinon.fake.rejects("some error");
+    aws.mock("S3", "copyObject", fake);
+
+    return lambdaTester(copyAssets.handler)
+      .event({
+        srcBucket: "mockSrcBucket",
+        destBucket: "mockDestBucket",
+        mapping: {
+          path: "mockPath",
+          destPath: "mockDestPath",
+        }
+      })
+      .expectReject(err => {
+        sinon.assert.calledWith(fake, {
+          CopySource: "mockSrcBucket/mockPath",
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+        expect(err).toEqual(new Error("some error"));
+      });
+  });
+});
