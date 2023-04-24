afd64eee76fc759e1eced14277b7282f453cd86f
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-04-24 15:15:06 -0700

    chore: static site custom resources (#4779)
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

---
+++ cf-custom-resources/lib/copy-assets.js
@@ -0,0 +1,21 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+"use strict";
+
+const aws = require("aws-sdk");
+
+/**
+ * Main handler, invoked by Lambda
+ */
+exports.handler = async function (event, context, callback) {
+	const s3 = new aws.S3();
+
+	const params = {
+		CopySource: event.srcBucket + "/" + event.mapping.path,
+		Bucket: event.destBucket,
+		Key: event.mapping.destPath
+	};
+
+	await s3.copyObject(params).promise();
+};
