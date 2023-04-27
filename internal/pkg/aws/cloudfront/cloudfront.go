944fa808b2a28ed900a58799b24f6040ca05fe8f
penghaoh <penghaoh@amazon.com>
2023-04-27 16:00:30 -0700

    Fix linter

--- internal/pkg/aws/cloudfront/cloudfront.go
+++ internal/pkg/aws/cloudfront/cloudfront.go
@@ -6,7 +6,7 @@ const (
 	// CertRegion is the only AWS region accepted by CloudFront while attaching certificates to a distribution.
 	CertRegion = "us-east-1"
 
-	// Regex validation format for S3 bucket as CloudFront origin domain
+	// S3BucketOriginDomainFormat is the Regex validation format for S3 bucket as CloudFront origin domain
 	// See https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesDomainName
 	S3BucketOriginDomainFormat = `.+\.s3.*\.\w+-\w+-\d+\.amazonaws\.com`
 )
