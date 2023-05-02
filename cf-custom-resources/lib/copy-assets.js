ec3db9698e5687185ed65d5ee043c18624dbd9da
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-05-02 16:07:25 -0700

    add object lifecycle, check if object exists before copy

--- cf-custom-resources/lib/copy-assets.js
+++ cf-custom-resources/lib/copy-assets.js
@@ -11,11 +11,28 @@ const aws = require("aws-sdk");
 exports.handler = async function (event, context, callback) {
 	const s3 = new aws.S3();
 
-	const params = {
+	try {
+		// TODO make sure this returns an error that will get caught in access denied conditons
+		await s3.headObject({
+			Bucket: event.destBucket,
+			Key: event.mapping.destPath
+		}).promise();
+
+		// if headObject is successful, the file already exists
+		// in destBucket. no need to copy, so we're done!
+		return;
+	} catch (err) {
+		// if the object was not found (404),
+		// then we'll copy and create the object.
+		// if there was some other error, we'll throw that.
+		if (err.statusCode !== 404) {
+			throw err;
+		}
+	}
+
+	await s3.copyObject({
 		CopySource: event.srcBucket + "/" + event.mapping.path,
 		Bucket: event.destBucket,
 		Key: event.mapping.destPath
-	};
-
-	await s3.copyObject(params).promise();
+	}).promise();
 };
--- internal/pkg/deploy/app.go
+++ internal/pkg/deploy/app.go
@@ -29,7 +29,7 @@ const (
 	// LegacyAppTemplateVersion is the version associated with the application template before we started versioning.
 	LegacyAppTemplateVersion = "v0.0.0"
 	// LatestAppTemplateVersion is the latest version number available for application templates.
-	LatestAppTemplateVersion = "v1.1.0"
+	LatestAppTemplateVersion = "v1.2.0"
 	// AliasLeastAppTemplateVersion is the least version number available for HTTPS alias.
 	AliasLeastAppTemplateVersion = "v1.0.0"
 )
