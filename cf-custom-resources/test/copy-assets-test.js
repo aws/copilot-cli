41377f8853a75ca372a561bd6a27e1ef4b1c9c4f
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-05-03 16:42:47 -0700

    updated tests

--- cf-custom-resources/test/copy-assets-test.js
+++ cf-custom-resources/test/copy-assets-test.js
@@ -13,9 +13,14 @@ describe("copy assets", () => {
     aws.restore();
   });
 
-  test("happy path", () => {
-    const fake = sinon.fake.resolves({});
-    aws.mock("S3", "copyObject", fake);
+  test("copies file if file doesn't exist", () => {
+    const err = new Error();
+    err.statusCode = 404;
+    const headObjectFake = sinon.fake.rejects(err);
+    aws.mock("S3", "headObject", headObjectFake);
+
+    const copyObjectFake = sinon.fake.resolves({});
+    aws.mock("S3", "copyObject", copyObjectFake);
 
     return lambdaTester(handler.handler)
       .event({
@@ -27,7 +32,12 @@ describe("copy assets", () => {
         }
       })
       .expectResolve(result => {
-        sinon.assert.calledWith(fake, {
+        sinon.assert.calledWith(headObjectFake, {
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+
+        sinon.assert.calledWith(copyObjectFake, {
           CopySource: "mockSrcBucket/mockPath",
           Bucket: "mockDestBucket",
           Key: "mockDestPath"
@@ -35,9 +45,40 @@ describe("copy assets", () => {
       });
   });
 
-  test("s3 error", () => {
-    const fake = sinon.fake.rejects("some error");
-    aws.mock("S3", "copyObject", fake);
+  test("skips copy if file already exists", () => {
+    const headObjectFake = sinon.fake.resolves({});
+    aws.mock("S3", "headObject", headObjectFake);
+
+    const copyObjectFake = sinon.fake.resolves({});
+    aws.mock("S3", "copyObject", copyObjectFake);
+
+    return lambdaTester(handler.handler)
+      .event({
+        srcBucket: "mockSrcBucket",
+        destBucket: "mockDestBucket",
+        mapping: {
+          path: "mockPath",
+          destPath: "mockDestPath",
+        }
+      })
+      .expectResolve(result => {
+        sinon.assert.calledWith(headObjectFake, {
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+
+        sinon.assert.notCalled(copyObjectFake);
+      });
+  });
+
+  test("errors on failure checking if file exists", () => {
+    const err = new Error("headObject error");
+    err.statusCode = 403;
+    const headObjectFake = sinon.fake.rejects(err);
+    aws.mock("S3", "headObject", headObjectFake);
+
+    const copyObjectFake = sinon.fake.resolves({});
+    aws.mock("S3", "copyObject", copyObjectFake);
 
     return lambdaTester(handler.handler)
       .event({
@@ -49,12 +90,48 @@ describe("copy assets", () => {
         }
       })
       .expectReject(err => {
-        sinon.assert.calledWith(fake, {
+        sinon.assert.calledWith(headObjectFake, {
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+
+        sinon.assert.notCalled(copyObjectFake);
+
+        expect(err).toEqual(new Error("headObject error"));
+      });
+  });
+
+  test("errors on failure to copy object", () => {
+    const err = new Error();
+    err.statusCode = 404;
+    const headObjectFake = sinon.fake.rejects(err);
+    aws.mock("S3", "headObject", headObjectFake);
+
+    const copyObjectFake = sinon.fake.rejects("copyObject error");
+    aws.mock("S3", "copyObject", copyObjectFake);
+
+    return lambdaTester(handler.handler)
+      .event({
+        srcBucket: "mockSrcBucket",
+        destBucket: "mockDestBucket",
+        mapping: {
+          path: "mockPath",
+          destPath: "mockDestPath",
+        }
+      })
+      .expectReject(err => {
+        sinon.assert.calledWith(headObjectFake, {
+          Bucket: "mockDestBucket",
+          Key: "mockDestPath"
+        });
+
+        sinon.assert.calledWith(copyObjectFake, {
           CopySource: "mockSrcBucket/mockPath",
           Bucket: "mockDestBucket",
           Key: "mockDestPath"
         });
-        expect(err).toEqual(new Error("some error"));
+
+        expect(err).toEqual(new Error("copyObject error"));
       });
   });
 });
