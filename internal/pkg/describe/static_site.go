b6a6ce160ec038abb514f0cb16ac1ac355a20791
Penghao He <penghaoh@amazon.com>
2023-05-18 10:49:51 -0700

    chore: implement svc show for static site (#4876)
    based on https://github.com/aws/copilot-cli/pull/4870
    
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/describe/static_site.go
+++ internal/pkg/describe/static_site.go
@@ -4,8 +4,15 @@
 package describe
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"strings"
+	"text/tabwriter"
 
+	"github.com/aws/copilot-cli/internal/pkg/describe/stack"
+	"github.com/aws/copilot-cli/internal/pkg/manifest/manifestinfo"
+	"github.com/aws/copilot-cli/internal/pkg/term/color"
 	"github.com/dustin/go-humanize/english"
 )
 
@@ -19,6 +26,8 @@ type StaticSiteDescriber struct {
 	app string
 	svc string
 
+	enableResources        bool
+	store                  DeployedEnvServicesLister
 	initWkldStackDescriber func(string) (workloadDescriber, error)
 	wkldDescribers         map[string]workloadDescriber
 }
@@ -26,9 +35,11 @@ type StaticSiteDescriber struct {
 // NewStaticSiteDescriber instantiates a static site service describer.
 func NewStaticSiteDescriber(opt NewServiceConfig) (*StaticSiteDescriber, error) {
 	describer := &StaticSiteDescriber{
-		app:            opt.App,
-		svc:            opt.Svc,
-		wkldDescribers: make(map[string]workloadDescriber),
+		app:             opt.App,
+		svc:             opt.Svc,
+		enableResources: opt.EnableResources,
+		store:           opt.DeployStore,
+		wkldDescribers:  make(map[string]workloadDescriber),
 	}
 	describer.initWkldStackDescriber = func(env string) (workloadDescriber, error) {
 		if describer, ok := describer.wkldDescribers[env]; ok {
@@ -66,4 +77,107 @@ func (d *StaticSiteDescriber) URI(envName string) (URI, error) {
 		URI:        english.OxfordWordSeries(uris, "or"),
 		AccessType: URIAccessTypeInternet,
 	}, nil
+}
+
+// Describe returns info of a static site.
+func (d *StaticSiteDescriber) Describe() (HumanJSONStringer, error) {
+	environments, err := d.store.ListEnvironmentsDeployedTo(d.app, d.svc)
+	if err != nil {
+		return nil, fmt.Errorf("list deployed environments for service %q: %w", d.svc, err)
+	}
+	var routes []*WebServiceRoute
+	for _, env := range environments {
+		uri, err := d.URI(env)
+		if err != nil {
+			return nil, fmt.Errorf("retrieve service URI: %w", err)
+		}
+		if uri.AccessType == URIAccessTypeInternet {
+			routes = append(routes, &WebServiceRoute{
+				Environment: env,
+				URL:         uri.URI,
+			})
+		}
+	}
+	resources := make(map[string][]*stack.Resource)
+	if d.enableResources {
+		for _, env := range environments {
+			svcDescr, err := d.initWkldStackDescriber(env)
+			if err != nil {
+				return nil, err
+			}
+			stackResources, err := svcDescr.StackResources()
+			if err != nil {
+				return nil, fmt.Errorf("retrieve service resources: %w", err)
+			}
+			resources[env] = stackResources
+		}
+	}
+	return &staticSiteDesc{
+		Service:   d.svc,
+		Type:      manifestinfo.StaticSiteType,
+		App:       d.app,
+		Routes:    routes,
+		Resources: resources,
+
+		environments: environments,
+	}, nil
+}
+
+// Manifest returns the contents of the manifest used to deploy a static site stack.
+// If the Manifest metadata doesn't exist in the stack template, then returns ErrManifestNotFoundInTemplate.
+func (d *StaticSiteDescriber) Manifest(env string) ([]byte, error) {
+	cfn, err := d.initWkldStackDescriber(env)
+	if err != nil {
+		return nil, err
+	}
+	return cfn.Manifest()
+}
+
+// staticSiteDesc contains serialized parameters for a static site.
+type staticSiteDesc struct {
+	Service   string               `json:"service"`
+	Type      string               `json:"type"`
+	App       string               `json:"application"`
+	Routes    []*WebServiceRoute   `json:"routes"`
+	Resources deployedSvcResources `json:"resources,omitempty"`
+
+	environments []string `json:"-"`
+}
+
+// JSONString returns the stringified backendService struct with json format.
+func (w *staticSiteDesc) JSONString() (string, error) {
+	b, err := json.Marshal(w)
+	if err != nil {
+		return "", fmt.Errorf("marshal static site description: %w", err)
+	}
+	return fmt.Sprintf("%s\n", b), nil
+}
+
+// HumanString returns the stringified backendService struct with human readable format.
+func (w *staticSiteDesc) HumanString() string {
+	var b bytes.Buffer
+	writer := tabwriter.NewWriter(&b, minCellWidth, tabWidth, cellPaddingWidth, paddingChar, noAdditionalFormatting)
+	fmt.Fprint(writer, color.Bold.Sprint("About\n\n"))
+	writer.Flush()
+	fmt.Fprintf(writer, "  %s\t%s\n", "Application", w.App)
+	fmt.Fprintf(writer, "  %s\t%s\n", "Name", w.Service)
+	fmt.Fprintf(writer, "  %s\t%s\n", "Type", w.Type)
+	if len(w.Routes) > 0 {
+		fmt.Fprint(writer, color.Bold.Sprint("\nRoutes\n\n"))
+		writer.Flush()
+		headers := []string{"Environment", "URL"}
+		fmt.Fprintf(writer, "  %s\n", strings.Join(headers, "\t"))
+		fmt.Fprintf(writer, "  %s\n", strings.Join(underline(headers), "\t"))
+		for _, route := range w.Routes {
+			fmt.Fprintf(writer, "  %s\t%s\n", route.Environment, route.URL)
+		}
+	}
+	if len(w.Resources) != 0 {
+		fmt.Fprint(writer, color.Bold.Sprint("\nResources\n"))
+		writer.Flush()
+
+		w.Resources.humanStringByEnv(writer, w.environments)
+	}
+	writer.Flush()
+	return b.String()
 }
