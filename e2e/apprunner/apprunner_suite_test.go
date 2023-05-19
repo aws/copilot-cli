6a74fccd9d4b20c81d6b9604ce39767dec7365e8
Wanxian Yang <79273084+Lou1415926@users.noreply.github.com>
2023-05-19 16:11:45 -0500

    chore(e2e): apprunner test suite should refer to secrets by the correct name (#4902)
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- e2e/apprunner/apprunner_suite_test.go
+++ e2e/apprunner/apprunner_suite_test.go
@@ -5,6 +5,7 @@ package apprunner_test
 
 import (
 	"fmt"
+	"os"
 	"testing"
 	"time"
 
@@ -39,10 +40,15 @@ var _ = BeforeSuite(func() {
 	var err error
 	cli, err = client.NewCLI()
 	Expect(err).NotTo(HaveOccurred())
-	Expect(err).NotTo(HaveOccurred())
+
 	appName = fmt.Sprintf("e2e-apprunner-%d", time.Now().Unix())
 	ssmName = fmt.Sprintf("%s-%s", appName, "ssm")
+	err = os.Setenv("SSM_NAME", ssmName)
+	Expect(err).NotTo(HaveOccurred())
 	secretName = fmt.Sprintf("%s-%s", appName, "secretsmanager")
+	err = os.Setenv("SECRETS_MANAGER_NAME", secretName)
+	Expect(err).NotTo(HaveOccurred())
+
 	err = command.Run("aws", []string{"ssm", "put-parameter", "--name", ssmName, "--overwrite", "--value", "abcd1234", "--type", "String"})
 	Expect(err).NotTo(HaveOccurred())
 	err = command.Run("aws", []string{"ssm", "add-tags-to-resource", "--resource-type", "Parameter", "--resource-id", ssmName, "--tags", "[{\"Key\":\"copilot-application\",\"Value\":\"" + appName + "\"},{\"Key\":\"copilot-environment\", \"Value\":\"" + envName + "\"}]"})
