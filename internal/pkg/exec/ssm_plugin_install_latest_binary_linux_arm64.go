1b90889b4b23028e543bbd4379b536fcd7cc1be2
Janice Huang <60631893+huanjani@users.noreply.github.com>
2023-05-22 16:21:19 -0700

    chore(exec): add darwin ARM binary for SSM plugin (#4916)
    Fixes #4891.
    
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/exec/ssm_plugin_install_latest_binary_linux_arm64.go
+++ internal/pkg/exec/ssm_plugin_install_latest_binary_linux_arm64.go
@@ -1,7 +1,6 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-// Package exec provides an interface to execute certain commands.
 package exec
 
 const (
