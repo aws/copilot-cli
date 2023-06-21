7abda7603bb00a2afde12067846c5987cc38854f
Austin Ely <austiely@amazon.com>
2023-06-21 14:06:12 -0400

    chore: finish RemoveEnvFromApp function

--- internal/pkg/deploy/cloudformation/env.go
+++ internal/pkg/deploy/cloudformation/env.go
@@ -78,12 +78,9 @@ func newRenderEnvironmentInput(cfnStack *cloudformation.Stack) *executeAndRender
 func (cf CloudFormation) DeleteEnvironment(appName, envName, cfnExecRoleARN string) error {
 	stackName := stack.NameForEnv(appName, envName)
 	description := fmt.Sprintf("Delete environment stack %s", stackName)
-	if err := cf.deleteAndRenderStack(stackName, description, func() error {
+	return cf.deleteAndRenderStack(stackName, description, func() error {
 		return cf.cfnClient.DeleteAndWaitWithRoleARN(stackName, cfnExecRoleARN)
-	}); err != nil {
-		return err
-	}
-	return cf.RemoveEnvFromApp(&RemoveEnvFromAppOpts{})
+	})
 }
 
 // GetEnvironment returns the Environment metadata from the CloudFormation stack.
