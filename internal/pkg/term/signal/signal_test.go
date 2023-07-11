ffaa878125cc9be7db0b076b7a5f56e1ebae8471
Adithya Kolla <kolladit@amazon.com>
2023-07-11 14:56:20 -0700

    change expected to gotSig

--- internal/pkg/term/signal/signal_test.go
+++ internal/pkg/term/signal/signal_test.go
@@ -38,8 +38,8 @@ func TestSignal(t *testing.T) {
 				if err := syscall.Kill(syscall.Getpid(), wantedSignal.(syscall.Signal)); err != nil {
 					require.Error(t, err)
 				}
-				if expectedSignal := <-signalCh; expectedSignal != wantedSignal {
-					t.Errorf("wanted signal is %v, Got signal %v", wantedSignal, expectedSignal)
+				if gotSignal := <-signalCh; gotSignal != wantedSignal {
+					t.Errorf("wanted signal is %v, Got signal %v", wantedSignal, gotSignal)
 				}
 			}
 			sig.StopCatchSignals()
