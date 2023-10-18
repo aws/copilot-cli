23bd8e3abafdd74359533897e50a6963c8c6ea43
Aiden Carpenter <72675057+CaptainCarpensir@users.noreply.github.com>
2023-10-18 10:21:21 -0700

    chore: continue to delete application parameters when there is no stackset (#5382)
    Wraps a stackset error when the stackset is not found, and skips s3 bucket deletion step when error is encountered in `copilot app delete`
    
    
    Address [this gitter message](https://app.gitter.im/#/room/#aws_copilot-cli:gitter.im/$gXF4Pft2TYjYFHDs2VP6Q2HjkxOPTRJ2ZUytmsTzE2g)
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/cli/app_delete.go
+++ internal/pkg/cli/app_delete.go
@@ -314,6 +314,10 @@ func (o *deleteAppOpts) emptyS3Bucket() error {
 	}
 	appResources, err := o.cfn.GetRegionalAppResources(app)
 	if err != nil {
+		// ErrStackSetNotFound means application Parameter Store params are dangling, return nil to continue for deletion.
+		if isStackSetNotExistsErr(err) {
+			return nil
+		}
 		return fmt.Errorf("get regional application resources for %s: %w", app.Name, err)
 	}
 	o.spinner.Start(deleteAppCleanResourcesStartMsg)
