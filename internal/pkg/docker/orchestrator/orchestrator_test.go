f9176d746cdd29d5adc412d6e044646e8f70c960
Danny Randall <10566468+dannyrandall@users.noreply.github.com>
2023-11-03 13:20:13 -0700

    such coverage

--- internal/pkg/docker/orchestrator/orchestrator_test.go
+++ internal/pkg/docker/orchestrator/orchestrator_test.go
@@ -9,6 +9,8 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"slices"
+	"strconv"
 	"strings"
 	"sync"
 	"testing"
@@ -23,6 +25,14 @@ func TestOrchestrator(t *testing.T) {
 		return dockerengine.RunLogOptions{
 			Output: io.Discard,
 		}
+	}
+	generateHosts := func(n int) []Host {
+		hosts := make([]Host, n)
+		for i := 0; i < n; i++ {
+			hosts[i].Host = strconv.Itoa(i)
+			hosts[i].Port = strconv.Itoa(i)
+		}
+		return hosts
 	}
 
 	type test func(*testing.T, *Orchestrator)
@@ -175,7 +185,7 @@ func TestOrchestrator(t *testing.T) {
 			},
 			errs: []string{},
 		},
-		"proxy setup connection returns error": {
+		"proxy setup, connection returns error": {
 			logOptions: noLogs,
 			test: func(t *testing.T) (test, DockerEngine) {
 				de := &dockerenginetest.Double{
@@ -218,6 +228,269 @@ func TestOrchestrator(t *testing.T) {
 			},
 			stopAfterNErrs: 1,
 			errs:           []string{`setup proxy connections: setup proxy connection for "remote-foo": proxy to remote-foo:80: some error`},
+		},
+		"proxy setup, connection error writing lines": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							w.(*io.PipeWriter).CloseWithError(errors.New("some error"))
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/16")
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, Host{
+						Host: "remote-foo",
+						Port: "80",
+					}))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: setup proxy connection for "remote-foo": process output for proxy to remote-foo:80: some error`},
+		},
+		"proxy setup, subnet out of space": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/24") // 172.20.0.0 - 172.20.0.255
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, generateHosts(257)...))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: no more addresses in network`},
+		},
+		"proxy setup, run out of ipv4 space": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("255.255.255.254/31") // 255.255.255.254 - 255.255.255.255
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, generateHosts(3)...))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: get next ip: ip overflow`},
+		},
+		"proxy setup, ip tables error": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						} else if cmd == "iptables" {
+							return errors.New("some error")
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/16")
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, Host{
+						Host: "remote-foo",
+						Port: "80",
+					}))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: modify iptables: some error`},
+		},
+		"proxy setup, ip tables save error": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						} else if cmd == "iptables-save" {
+							return errors.New("some error")
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/16")
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, Host{
+						Host: "remote-foo",
+						Port: "80",
+					}))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: save iptables: some error`},
+		},
+		"proxy setup, /etc/hosts error": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						} else if cmd == "/bin/bash" {
+							return errors.New("some error")
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/16")
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, Host{
+						Host: "remote-foo",
+						Port: "80",
+					}))
+				}, de
+			},
+			stopAfterNErrs: 1,
+			errs:           []string{`setup proxy connections: update /etc/hosts: some error`},
 		},
 		"proxy success": {
 			logOptions: noLogs,
@@ -267,6 +540,61 @@ func TestOrchestrator(t *testing.T) {
 					}))
 
 					<-waitUntilRun
+				}, de
+			},
+		},
+		"proxy success, subnet overflow": {
+			logOptions: noLogs,
+			test: func(t *testing.T) (test, DockerEngine) {
+				waitUntilRun := make(chan struct{})
+				var gotHosts []string
+
+				de := &dockerenginetest.Double{
+					IsContainerRunningFn: func(ctx context.Context, name string) (bool, error) {
+						return true, nil
+					},
+					ExecFn: func(ctx context.Context, ctr string, w io.Writer, cmd string, args ...string) error {
+						if cmd == "aws" {
+							defer w.(io.WriteCloser).Close()
+							fmt.Fprintf(w, "Port 61972 opened for sessionId mySessionId\n")
+						} else if cmd == "/bin/bash" {
+							gotHosts = append(gotHosts, args[1])
+						}
+						return nil
+					},
+					RunFn: func(ctx context.Context, opts *dockerengine.RunOptions) error {
+						if opts.ContainerName == "prefix-foo" {
+							close(waitUntilRun)
+						}
+						return nil
+					},
+				}
+				return func(t *testing.T, o *Orchestrator) {
+					_, ipNet, err := net.ParseCIDR("172.20.0.0/23") // 172.20.0.0 - 172.20.1.255
+					require.NoError(t, err)
+
+					o.RunTask(Task{
+						PauseSecrets: map[string]string{
+							"A_SECRET": "very secret",
+						},
+						Containers: map[string]ContainerDefinition{
+							"foo": {
+								Ports: map[string]string{
+									"8080": "80",
+								},
+							},
+							"bar": {
+								Ports: map[string]string{
+									"9000": "90",
+								},
+							},
+						},
+					}, RunTaskWithProxy("ecs:cluster_task_ctr", *ipNet, generateHosts(257)...))
+
+					<-waitUntilRun
+					require.True(t, slices.ContainsFunc(gotHosts, func(s string) bool {
+						return strings.HasPrefix(s, "echo 172.20.1.0")
+					}))
 				}, de
 			},
 		},
@@ -334,7 +662,6 @@ func TestOrchestrator(t *testing.T) {
 			wg.Add(2)
 
 			stopCh := make(chan struct{})
-
 			errs := o.Start()
 
 			go func() {
@@ -358,9 +685,7 @@ func TestOrchestrator(t *testing.T) {
 				defer wg.Done()
 
 				test(t, o)
-
 				<-stopCh
-				fmt.Printf("calling stop\n")
 				o.Stop()
 			}()
 
