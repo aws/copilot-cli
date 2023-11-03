28c34b77e6c65ee941136a7047fbe9598feed0e2
Aiden Carpenter <amcarp@amazon.com>
2023-11-03 14:31:20 -0700

    make integration test non-blocking on failure

--- internal/pkg/cli/file/watch_integration_test.go
+++ internal/pkg/cli/file/watch_integration_test.go
@@ -10,6 +10,7 @@ import (
 	"io/fs"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/aws/copilot-cli/internal/pkg/cli/file"
 	"github.com/fsnotify/fsnotify"
@@ -34,10 +35,6 @@ func TestRecursiveWatcher(t *testing.T) {
 		{
 			Name: fmt.Sprintf("%s/watch/subdir/testfile", tmp),
 			Op:   fsnotify.Chmod,
-		},
-		{
-			Name: fmt.Sprintf("%s/watch/subdir/testfile", tmp),
-			Op:   fsnotify.Write,
 		},
 		{
 			Name: fmt.Sprintf("%s/watch/subdir/testfile", tmp),
@@ -73,7 +70,7 @@ func TestRecursiveWatcher(t *testing.T) {
 		err := os.MkdirAll(fmt.Sprintf("%s/watch/subdir", tmp), 0755)
 		require.NoError(t, err)
 
-		watcher, err = file.NewRecursiveWatcher(10)
+		watcher, err = file.NewRecursiveWatcher(uint(len(eventsExpected)))
 		require.NoError(t, err)
 	})
 
@@ -85,37 +82,45 @@ func TestRecursiveWatcher(t *testing.T) {
 		eventsCh := watcher.Events()
 		errorsCh := watcher.Errors()
 
+		expectEvents := func(t *testing.T, n int) []fsnotify.Event {
+			receivedEvents := []fsnotify.Event{}
+			for i := 0; i < n; i++ {
+				select {
+				case e := <-eventsCh:
+					receivedEvents = append(receivedEvents, e)
+				case <-time.After(time.Second):
+				}
+			}
+			return receivedEvents
+		}
+
 		// WATCH
 		file, err := os.Create(fmt.Sprintf("%s/watch/subdir/testfile", tmp))
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 1)...)
 
 		err = os.Chmod(fmt.Sprintf("%s/watch/subdir/testfile", tmp), 0755)
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 1)...)
 
 		err = os.WriteFile(fmt.Sprintf("%s/watch/subdir/testfile", tmp), []byte("write to file"), fs.ModeAppend)
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 2)...)
 
 		err = file.Close()
 		require.NoError(t, err)
 
 		err = os.Rename(fmt.Sprintf("%s/watch/subdir", tmp), fmt.Sprintf("%s/watch/subdir2", tmp))
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
-		eventsActual = append(eventsActual, <-eventsCh)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 3)...)
 
 		err = os.Rename(fmt.Sprintf("%s/watch/subdir2/testfile", tmp), fmt.Sprintf("%s/watch/subdir2/testfile2", tmp))
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 2)...)
 
 		err = os.Remove(fmt.Sprintf("%s/watch/subdir2/testfile2", tmp))
 		require.NoError(t, err)
-		eventsActual = append(eventsActual, <-eventsCh)
+		eventsActual = append(eventsActual, expectEvents(t, 1)...)
 
 		// CLOSE
 		err = watcher.Close()
