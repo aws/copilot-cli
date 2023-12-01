db6ae4d3af0b70d31033a31b5cc4ee53af736f61
AdithyaKolla <adityakolla9@gmail.com>
2023-12-01 13:40:29 -0800

    cleanup code

--- internal/pkg/deploy/pipeline.go
+++ internal/pkg/deploy/pipeline.go
@@ -8,19 +8,19 @@ package deploy
 import (
 	"errors"
 	"fmt"
+	"gopkg.in/yaml.v3"
 	"path"
 	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
 
-	"gopkg.in/yaml.v3"
-
 	"github.com/aws/aws-sdk-go/aws/arn"
 	"github.com/aws/copilot-cli/internal/pkg/config"
 	"github.com/aws/copilot-cli/internal/pkg/graph"
 	"github.com/aws/copilot-cli/internal/pkg/manifest"
 )
+
 
 const (
 	fmtInvalidRepo           = "unable to parse the repository from the URL %+v"
@@ -781,7 +781,7 @@ func (stg *PipelineStage) buildActionsGraph(rankables []actionGraphNode) *graph.
 	for _, r := range rankables {
 		names = append(names, r.name)
 	}
-	digraph := graph.New[string](names...)
+	digraph := graph.New(names...)
 
 	for _, r := range rankables {
 		if r.depends_on == nil {
