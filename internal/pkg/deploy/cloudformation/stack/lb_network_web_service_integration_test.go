29105f03f9bed57688117f7b1c3ab2962c4fb8e7
Bence Hornák <bence.hornak@gmail.com>
2023-12-04 09:37:24 +0100

    test: add dummy ArtifactKey params

--- internal/pkg/deploy/cloudformation/stack/lb_network_web_service_integration_test.go
+++ internal/pkg/deploy/cloudformation/stack/lb_network_web_service_integration_test.go
@@ -106,6 +106,7 @@ func TestNetworkLoadBalancedWebService_Template(t *testing.T) {
 			EnvManifest:        envConfig,
 			Manifest:           v,
 			ArtifactBucketName: "bucket",
+			ArtifactKey:        "arn:aws:kms:us-west-2:111122223333:key/1234abcd-12ab-34cd-56ef-1234567890ab",
 			RuntimeConfig: stack.RuntimeConfig{
 				ServiceDiscoveryEndpoint: svcDiscoveryEndpointName,
 				AccountID:                "123456789123",
