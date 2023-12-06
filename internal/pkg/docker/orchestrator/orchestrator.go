90b23b0a59b36d035e3d86e4345bb15890960fa9
Aiden Carpenter <72675057+CaptainCarpensir@users.noreply.github.com>
2023-12-06 13:28:19 -0800

    chore: prevent orchestrator stopTask from returning false errors (#5531)
    Made a mistake in #5489 that causes an error message to falsely appear when stopping `run local` because it would call `docker inspect` when it only needed to call `docker ps`. This introduces another function that only runs the latter function, rather than running both, fixing the issue.
    
    
    
    By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the Apache 2.0 License.

--- internal/pkg/docker/orchestrator/orchestrator.go
+++ internal/pkg/docker/orchestrator/orchestrator.go
@@ -48,6 +48,7 @@ type logOptionsFunc func(name string, ctr ContainerDefinition) dockerengine.RunL
 // DockerEngine is used by Orchestrator to manage containers.
 type DockerEngine interface {
 	Run(context.Context, *dockerengine.RunOptions) error
+	DoesContainerExist(context.Context, string) (bool, error)
 	IsContainerRunning(context.Context, string) (bool, error)
 	Stop(context.Context, string) error
 	Build(ctx context.Context, args *dockerengine.BuildArguments, w io.Writer) error
@@ -398,13 +399,13 @@ func (o *Orchestrator) stopTask(ctx context.Context, task Task) error {
 
 			// ensure that container is fully stopped before stopTask finishes blocking
 			for {
-				running, err := o.docker.IsContainerRunning(ctx, o.containerID(name))
+				exists, err := o.docker.DoesContainerExist(ctx, o.containerID(name))
 				if err != nil {
 					errCh <- fmt.Errorf("polling container %q for removal: %w", name, err)
 					return
 				}
 
-				if running {
+				if exists {
 					select {
 					case <-time.After(1 * time.Second):
 						continue
