1a690f2edfa02472e2dcf75e30d791ccd339a598
AdithyaKolla <adityakolla9@gmail.com>
2023-12-15 14:40:32 -0800

    address @Lou1415926 fb

--- internal/pkg/docker/orchestrator/orchestrator.go
+++ internal/pkg/docker/orchestrator/orchestrator.go
@@ -485,15 +485,15 @@ func (o *Orchestrator) waitForContainerToStart(ctx context.Context, id string, i
 	}
 }
 
-func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, containerName string, a *runTaskAction) error {
+func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, name string, a *runTaskAction) error {
 	var deps []string
-	for depName, state := range a.task.Containers[containerName].DependsOn {
+	for depName, state := range a.task.Containers[name].DependsOn {
 		deps = append(deps, fmt.Sprintf("%s->%s", depName, state))
 	}
 	logMsg := strings.Join(deps, ", ")
-	fmt.Printf("Waiting for container %q dependencies: [%s]\n", containerName, color.Emphasize(logMsg))
+	fmt.Printf("Waiting for container %q dependencies: [%s]\n", name, color.Emphasize(logMsg))
 	eg, ctx := errgroup.WithContext(ctx)
-	for name, state := range a.task.Containers[containerName].DependsOn {
+	for name, state := range a.task.Containers[name].DependsOn {
 		name, state := name, state
 		eg.Go(func() error {
 			ctrId := o.containerID(name)
@@ -513,7 +513,7 @@ func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, contain
 						return err
 					}
 					if isEssential {
-						log.Successf("Successfully container %s is running\n", ctrId)
+						log.Successf("Successfully started container %s\n", ctrId)
 						return nil
 					}
 					fmt.Printf("non-essential container %q started successfully\n", ctrId)
@@ -522,10 +522,10 @@ func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, contain
 					healthy, err := o.docker.IsContainerHealthy(ctx, ctrId)
 					if err != nil {
 						if !isEssential {
-							fmt.Printf("non-essential container %q failed to be %q: %v\n", ctrId, state, err)
+							fmt.Printf("non-essential container %q is not healthy: %v\n", ctrId, err)
 							return nil
 						}
-						return fmt.Errorf("essential container %q failed to be %q: %w", ctrId, state, err)
+						return fmt.Errorf("essential container %q is not healthy: %w", ctrId, err)
 					}
 					if healthy {
 						log.Successf("Successfully dependency container %q reached %q\n", ctrId, state)
@@ -534,10 +534,10 @@ func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, contain
 				case ctrStateComplete:
 					exitCode, err := o.docker.IsContainerCompleteOrSuccess(ctx, ctrId)
 					if err != nil {
-						return fmt.Errorf("dependency container %q failed to be %q: %w", ctrId, state, err)
+						return fmt.Errorf("wait for container %q to complete: %w", ctrId, err)
 					}
 					if exitCode != -1 {
-						log.Successf("Successfully dependency container %q exited with code: %d\n", ctrId, exitCode)
+						log.Successf("%q's dependency container %q exited with code: %d\n", name, ctrId, exitCode)
 						return nil
 					}
 				case ctrStateSuccess:
@@ -546,10 +546,10 @@ func (o *Orchestrator) waitForContainerDependencies(ctx context.Context, contain
 						return fmt.Errorf("dependency container %q failed to be %q: %w", ctrId, state, err)
 					}
 					if exitCode > 0 {
-						return fmt.Errorf("dependency container %q exited with code %d instead of %d exit code", ctrId, exitCode, 0)
+						return fmt.Errorf("dependency container %q exited with non-zero exit code %d", ctrId, exitCode)
 					}
 					if exitCode == 0 {
-						log.Successf("Successfully dependency container %q reached %q\n", ctrId, state)
+						log.Successf("%q's dependency container %q exited with code: %d\n", name, ctrId, exitCode)
 						return nil
 					}
 				}
