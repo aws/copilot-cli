2c8a212529bfc624cca850066b1a023bdd485161
Aiden Carpenter <amcarp@amazon.com>
2023-12-18 14:00:38 -0800

    generalize tests

--- internal/pkg/cli/file/watch_integration_test.go
+++ internal/pkg/cli/file/watch_integration_test.go
@@ -6,7 +6,6 @@
 package file_test
 
 import (
-	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
@@ -22,45 +21,45 @@ func TestRecursiveWatcher(t *testing.T) {
 	var (
 		watcher        *file.RecursiveWatcher
 		tmp            string
-		eventsExpected map[fsnotify.Event]struct{}
-		eventsActual   map[fsnotify.Event]struct{}
+		eventsExpected []fsnotify.Event
+		eventsActual   []fsnotify.Event
 	)
 
 	tmp = os.TempDir()
-	eventsActual = make(map[fsnotify.Event]struct{})
-	eventsExpected = map[fsnotify.Event]struct{}{
+	eventsActual = []fsnotify.Event{}
+	eventsExpected = []fsnotify.Event{
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir/testfile")),
 			Op:   fsnotify.Create,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir/testfile")),
 			Op:   fsnotify.Chmod,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir/testfile")),
 			Op:   fsnotify.Write,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir")),
 			Op:   fsnotify.Rename,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir2")),
 			Op:   fsnotify.Create,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir2/testfile")),
 			Op:   fsnotify.Rename,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir2/testfile2")),
 			Op:   fsnotify.Create,
-		}: {},
+		},
 		{
 			Name: filepath.ToSlash(filepath.Join(tmp, "watch/subdir2/testfile2")),
 			Op:   fsnotify.Remove,
-		}: {},
+		},
 	}
 
 	t.Run("Setup Watcher", func(t *testing.T) {
@@ -73,60 +72,62 @@ func TestRecursiveWatcher(t *testing.T) {
 
 	t.Run("Watch", func(t *testing.T) {
 		// SETUP
-		err := watcher.Add(fmt.Sprintf("%s/watch", tmp))
+		err := watcher.Add(filepath.ToSlash(filepath.Join(tmp, "watch")))
 		require.NoError(t, err)
 
 		eventsCh := watcher.Events()
 		errorsCh := watcher.Errors()
 
-		expectAndPopulateEvents := func(t *testing.T, n int, events map[fsnotify.Event]struct{}) {
-			for i := 0; i < n; i++ {
+		go func() {
+			for {
 				select {
 				case e := <-eventsCh:
-					events[e] = struct{}{}
-				case <-time.After(time.Second):
+					if e.Op != 0 {
+						eventsActual = append(eventsActual, e)
+					}
+				case <-errorsCh:
+					return
 				}
 			}
-		}
+		}()
 
 		// WATCH
 		file, err := os.Create(filepath.Join(tmp, "watch/subdir/testfile"))
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 1, eventsActual)
 
 		err = os.Chmod(filepath.Join(tmp, "watch/subdir/testfile"), 0755)
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 1, eventsActual)
 
 		err = os.WriteFile(filepath.Join(tmp, "watch/subdir/testfile"), []byte("write to file"), fs.ModeAppend)
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 2, eventsActual)
 
 		err = file.Close()
 		require.NoError(t, err)
 
 		err = os.Rename(filepath.Join(tmp, "watch/subdir"), filepath.Join(tmp, "watch/subdir2"))
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 3, eventsActual)
 
 		err = os.Rename(filepath.Join(tmp, "watch/subdir2/testfile"), filepath.Join(tmp, "watch/subdir2/testfile2"))
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 2, eventsActual)
 
 		err = os.Remove(filepath.Join(tmp, "watch/subdir2/testfile2"))
 		require.NoError(t, err)
-		expectAndPopulateEvents(t, 1, eventsActual)
+
+		// Wait for events to propagate
+		time.Sleep(time.Second)
 
 		// CLOSE
 		err = watcher.Close()
 		require.NoError(t, err)
 		require.Empty(t, errorsCh)
 
-		require.Equal(t, eventsExpected, eventsActual)
+		for _, expectedEvent := range eventsExpected {
+			require.Contains(t, eventsActual, expectedEvent)
+		}
 	})
 
 	t.Run("Clean", func(t *testing.T) {
-		err := os.RemoveAll(fmt.Sprintf("%s/watch", tmp))
+		err := os.RemoveAll(filepath.Join(tmp, "watch"))
 		require.NoError(t, err)
 	})
 }
